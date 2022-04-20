ae7b95630b343fb7263bf1017a59c93065fb8694
David Uhlmann <dib542@gmail.com>
2022-04-20 18:17:01 +1000

    2 - add support for ie11 (#6)
    * Add support for IE11:
        - see: https://github.com/facebook/create-react-app/tree/v5.0.1/packages/react-app-polyfill
    
    * Add IE flexbox height fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+// Import polyfills for IE11
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
