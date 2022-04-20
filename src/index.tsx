a77581d9eca78cbdcb1230f5d084f387c28806cf
David <dib542@gmail.com>
2022-04-21 02:47:32 +1000

    Remove unused CSS

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
