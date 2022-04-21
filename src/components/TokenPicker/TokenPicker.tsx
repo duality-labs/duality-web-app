1675dd8c9cfb864b3f96e77229a13cc12bf9d763
David <dib542@gmail.com>
2022-04-22 01:17:59 +1000

    Add mock data into TokenPicker

--- src/components/TokenPicker/TokenPicker.tsx
+++ src/components/TokenPicker/TokenPicker.tsx
@@ -1,3 +1,14 @@
+import { useTokens } from './mockHooks';
+
 export default function TokenPicker() {
-  return <div className="token-picker"></div>;
+  const { data: tokens = [] } = useTokens();
+  return (
+    <div className="token-picker">
+      <ul>
+        {tokens.map((token) => {
+          return <li key={token}>{token}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
---
+++ src/components/TokenPicker/mockHooks.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const seconds = 1000;
+const requestTime = 2 * seconds;
+const tokens = ['ETH', 'USDC'];
+
+export function useTokens() {
+  const [data, setData] = useState(undefined as string[]|undefined);
+  const [verifying, setVerifying] = useState(true);
+
+  // return mock data after requestTime has passed
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setVerifying(false);
+      setData(tokens);
+    }, requestTime);
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+
+  return { data, isVerifying: verifying };
+}
