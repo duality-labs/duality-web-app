1732d140c36fcce514f53366ab773e0eefc9ec92
David <dib542@gmail.com>
2022-04-21 23:39:30 +1000

    Split out Header into a separate component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo/logo.svg';
+
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link to="/">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Duality</h1>
+        </Link>
+        <Link to="/swap">Swap</Link>
+        <Link to="/pool">Pool</Link>
+      </nav>
+    </header>
+  );
+}
