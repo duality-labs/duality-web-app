0201143113c516bb8bfc163f533c4fa0d36f0448
David <dib542@gmail.com>
2022-04-22 00:14:32 +1000

    Fix styles to similar before Tailwind's reset CSS changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,9 @@ import logo from '../../assets/logo/logo.svg';
 export default function Header() {
   return (
     <header className="flex flex-row items-center justify-start w-full text-xl">
-      <nav className="w-full p-4 shadow shadow-white/10 text-slate-50">
-        <Link className="ml-3" to="/">
-          <img src={logo} className="logo h-6 mr-3" alt="logo" />
+      <nav className="w-full p-4 inline-flex items-end shadow shadow-white/10 text-slate-50">
+        <Link className="ml-3 inline-flex items-center" to="/">
+          <img src={logo} className="logo inline h-6 mr-3" alt="logo" />
           <h1 className="inline text-3xl">Duality</h1>
         </Link>
         <Link className="ml-3" to="/swap">
