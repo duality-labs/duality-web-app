0f9cf6a438448ea8857b640d6327be8b8aa0dabd
David <dib542@gmail.com>
2022-04-22 00:08:48 +1000

    Replace Header CSS with Tailwind classes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,20 @@ import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo/logo.svg';
 
-import './Header.scss';
-
 export default function Header() {
   return (
-    <header className="flex flex-row content-center justify-start">
-      <nav>
-        <Link to="/">
-          <img src={logo} className="logo" alt="logo" />
-          <h1>Duality</h1>
+    <header className="flex flex-row items-center justify-start w-full text-xl">
+      <nav className="w-full p-4 shadow shadow-white/10 text-slate-50">
+        <Link className="ml-3" to="/">
+          <img src={logo} className="logo h-6 mr-3" alt="logo" />
+          <h1 className="inline text-3xl">Duality</h1>
         </Link>
-        <Link to="/swap">Swap</Link>
-        <Link to="/pool">Pool</Link>
+        <Link className="ml-3" to="/swap">
+          Swap
+        </Link>
+        <Link className="ml-3" to="/pool">
+          Pool
+        </Link>
       </nav>
     </header>
   );
