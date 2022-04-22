4573bb6567b9c81f0112e73b6b816524a82de4d5
David <dib542@gmail.com>
2022-04-23 05:25:43 +1000

    Put second TokenPicker on Swap Page

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -3,9 +3,11 @@ import TokenPicker from '../../components/TokenPicker';
 
 export default function Swap() {
   const [tokenA, setTokenA] = useState(undefined as string | undefined);
+  const [tokenB, setTokenB] = useState(undefined as string | undefined);
   return (
     <div className="swap-page">
       <TokenPicker value={tokenA} onChange={setTokenA} />
+      <TokenPicker value={tokenB} onChange={setTokenB} />
     </div>
   );
 }
