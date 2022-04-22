3b3e73220880d4cf7a3f6fe0a878913fcd9c3514
David <dib542@gmail.com>
2022-04-23 05:23:39 +1000

    Put TokenPicker content in Dropdown content

--- src/components/TokenPicker/TokenPicker.tsx
+++ src/components/TokenPicker/TokenPicker.tsx
@@ -1,3 +1,5 @@
+import Dropdown from '../Dropdown';
+
 import { useTokens } from './mockHooks';
 
 export default function TokenPicker({
@@ -9,21 +11,29 @@ export default function TokenPicker({
 }) {
   const { data: tokens = [] } = useTokens();
   return (
-    <div className="token-picker w-60 max-w-full">
-      <label className="block p-2">Select a token</label>
-      <ul className="token-picker-list border-t border-slate-500 py-2">
-        {tokens?.map((token) => (
-          <li
-            key={token}
-            className={`py-1 px-2 hover:bg-slate-600 cursor-pointer${
-              value === token ? ' bg-slate-700' : ''
-            }`}
-            onClick={() => onChange(token)}
-          >
-            {token}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Dropdown
+      overlay={
+        <div className="token-picker">
+          <label className="block p-2">Select a token</label>
+          <ul className="token-picker-list border-t border-slate-500 py-2">
+            {tokens?.map((token) => (
+              <li
+                key={token}
+                className={`py-1 px-2 hover:bg-slate-600 cursor-pointer${
+                  value === token ? ' bg-slate-700' : ''
+                }`}
+                onClick={() => onChange(token)}
+              >
+                {token}
+              </li>
+            ))}
+          </ul>
+        </div>
+      }
+    >
+      <button className="py-1 px-3 border border-slate-200 rounded-lg">
+        {value ?? 'Choose Token'}
+      </button>
+    </Dropdown>
   );
 }
