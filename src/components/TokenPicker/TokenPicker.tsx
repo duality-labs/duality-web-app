65fe92573e241237396e19c3d577dd5c00b39586
David <dib542@gmail.com>
2022-04-23 03:30:55 +1000

    Add styling for dropdown parts

--- src/components/TokenPicker/TokenPicker.tsx
+++ src/components/TokenPicker/TokenPicker.tsx
@@ -3,14 +3,18 @@ import { useTokens } from './mockHooks';
 export default function TokenPicker() {
   const { data: tokens = [] } = useTokens();
   return (
-    <div className="token-picker">
-      {tokens.length > 0 && (
-        <ul>
-          {tokens.map((token) => {
-            return <li key={token}>{token}</li>;
-          })}
-        </ul>
-      )}
+    <div className="token-picker w-60 max-w-full">
+      <label className="block p-2">Select a token</label>
+      <ul className="token-picker-list border-t border-slate-500 py-2">
+        {tokens?.map((token) => (
+          <li
+            key={token}
+            className="py-1 px-2 hover:bg-slate-600 cursor-pointer"
+          >
+            {token}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
