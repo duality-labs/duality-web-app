e942e5f093bd595c917dc28e0b2af6c151e210af
David <dib542@gmail.com>
2022-04-23 05:28:40 +1000

    Add swap token selection button

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import TokenPicker from '../../components/TokenPicker';
 
 export default function Swap() {
   const [tokenA, setTokenA] = useState(undefined as string | undefined);
   const [tokenB, setTokenB] = useState(undefined as string | undefined);
+  const swapTokens = useCallback(() => {
+    setTokenA(tokenB);
+    setTokenB(tokenA);
+  }, [tokenA, tokenB]);
   return (
     <div className="swap-page">
       <TokenPicker value={tokenA} onChange={setTokenA} />
+      <button className="mx-2 py-1 px-3" onClick={swapTokens}>
+        {'<->'}
+      </button>
       <TokenPicker value={tokenB} onChange={setTokenB} />
     </div>
   );
