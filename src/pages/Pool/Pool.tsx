bc61a55e327c1c1427c5df69807e38088d172cb1
David <dib542@gmail.com>
2022-04-26 04:34:50 +1000

    Add Basic parts to Pool page

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -1,3 +1,74 @@
+import { useEffect, useState, useCallback } from 'react';
+
+import TokenPicker from '../../components/TokenPicker';
+import TokenInputGroup from '../../components/TokenInputGroup';
+import {
+  useTokens,
+  useExchangeRate,
+  useDotCounter,
+} from '../../components/TokenPicker/mockHooks';
+
+import './Pool.scss';
+
 export default function Pool() {
-  return <div className="pool-page">Add Liquidity</div>;
+  const [tokenA, setTokenA] = useState(undefined as string | undefined);
+  const [tokenB, setTokenB] = useState(undefined as string | undefined);
+  const swapTokens = useCallback(() => {
+    setTokenA(tokenB);
+    setTokenB(tokenA);
+  }, [tokenA, tokenB]);
+  const { isValidating: isValidatingRate } = useExchangeRate(tokenA || '', 0);
+  const { data: tokenList = [], isValidating: isValidaingTokens } = useTokens();
+  const dotCount = useDotCounter(0.25e3);
+
+  // set token A to be first token in list if not already populated
+  useEffect(() => {
+    if (tokenList.length > 0 && !tokenA) {
+      setTokenA(tokenList[0]);
+    }
+  }, [tokenA, tokenList]);
+
+  return (
+    <div className="pool-page">
+      <TokenPicker
+        value={tokenA}
+        onChange={setTokenA}
+        tokenList={tokenList}
+        exclusion={tokenB}
+      />
+      <button className="mx-2 py-1 px-3" onClick={swapTokens}>
+        {'<->'}
+      </button>
+      <TokenPicker
+        value={tokenB}
+        onChange={setTokenB}
+        tokenList={tokenList}
+        exclusion={tokenA}
+      />
+
+      <TokenInputGroup
+        readOnly
+        tokenList={tokenList}
+        token={tokenA}
+        value="0"
+        exclusion={tokenB}
+      ></TokenInputGroup>
+      <TokenInputGroup
+        readOnly
+        tokenList={tokenList}
+        token={tokenB}
+        value="0"
+        exclusion={tokenA}
+      ></TokenInputGroup>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-auto block"
+        onClick={() => swapTokens()}
+      >
+        Swap
+      </button>
+      {((isValidaingTokens || isValidatingRate) && '.'.repeat(dotCount)) || (
+        <i className="text-transparent">.</i>
+      )}
+    </div>
+  );
 }
