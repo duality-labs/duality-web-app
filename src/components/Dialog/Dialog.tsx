ae05f7861e510ca68187793d1c2310fc30a45a65
David <dib542@gmail.com>
2022-04-30 22:25:15 +1000

    Allow custom title

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -2,7 +2,11 @@ import { createPortal } from 'react-dom';
 import { useA11yDialog } from 'react-a11y-dialog';
 import { generateId } from '../../utils/id';
 
-export default function Dialog() {
+interface IDialog {
+  title: string;
+}
+
+export default function Dialog({ title }: IDialog) {
   // `instance` is the `a11y-dialog` instance.
   // `attr` is an object with the following keys:
   // - `container`: the dialog container
@@ -10,19 +14,18 @@ export default function Dialog() {
   // - `dialog`: the actual dialog box
   // - `title`: the dialog mandatory title
   // - `closeButton`:  the dialog close button
-  const [instance, attr] =
-    useA11yDialog({
-      // The required HTML `id` attribute of the dialog element, internally used
-      // a11y-dialog to manipulate the dialog.
-      id: `dialog-${generateId()}`,
-      // The optional `role` attribute of the dialog element, either `dialog`
-      // (default) or `alertdialog` to make it a modal (preventing closing on
-      // click outside of ESC key).
-      role: 'dialog',
-      // The required dialog title, mandatory in the document
-      // to provide context to assistive technology.
-      title: 'My dialog',
-    });
+  const [instance, attr] = useA11yDialog({
+    // The required HTML `id` attribute of the dialog element, internally used
+    // a11y-dialog to manipulate the dialog.
+    id: `dialog-${generateId()}`,
+    // The optional `role` attribute of the dialog element, either `dialog`
+    // (default) or `alertdialog` to make it a modal (preventing closing on
+    // click outside of ESC key).
+    role: 'dialog',
+    // The required dialog title, mandatory in the document
+    // to provide context to assistive technology.
+    title,
+  });
 
   const dialogPortal = createPortal(
     // @ts-expect-error: react-a11y type for container.ref is not incorrect
@@ -31,7 +34,7 @@ export default function Dialog() {
       <div {...attr.overlay} className="dialog-overlay" />
       <div {...attr.dialog} className="dialog-content">
         <p {...attr.title} className="dialog-title">
-          Your dialog title
+          {title}
         </p>
 
         <p>Your dialog content</p>
