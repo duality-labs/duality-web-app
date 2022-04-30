78b1e108311324c9c1e319b39292e9d4581fd230
David <dib542@gmail.com>
2022-05-01 00:41:43 +1000

    Add basic @reach/dialog Dialog component

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,28 @@
+import { DialogContent, DialogOverlay } from '@reach/dialog';
+import React from 'react';
+
+import '@reach/dialog/styles.css';
+
+interface DialogProps {
+  isOpen: boolean;
+  onDismiss: () => void;
+  initialFocusRef?: React.RefObject<HTMLInputElement | HTMLButtonElement>;
+  children?: React.ReactNode;
+}
+
+export default function Dialog({
+  isOpen,
+  onDismiss,
+  initialFocusRef,
+  children,
+}: DialogProps) {
+  return (
+    <>
+      {isOpen && (
+        <DialogOverlay onDismiss={onDismiss} initialFocusRef={initialFocusRef}>
+          <DialogContent aria-label="dialog content">{children}</DialogContent>
+        </DialogOverlay>
+      )}
+    </>
+  );
+}
