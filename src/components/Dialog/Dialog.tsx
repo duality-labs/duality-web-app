0b9f496c49366329154c63009de47da3936b465d
David <dib542@gmail.com>
2022-05-02 18:50:38 +1000

    Allow a header with a close button

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -8,6 +8,7 @@ interface DialogProps {
   isOpen: boolean;
   onDismiss: () => void;
   initialFocusRef?: React.RefObject<HTMLInputElement | HTMLButtonElement>;
+  header?: React.ReactNode | React.ReactNode[];
   className?: string;
   children?: React.ReactNode;
 }
@@ -16,6 +17,7 @@ export default function Dialog({
   isOpen,
   onDismiss,
   initialFocusRef,
+  header,
   className = '',
   children,
 }: DialogProps) {
@@ -28,6 +30,29 @@ export default function Dialog({
           initialFocusRef={initialFocusRef}
         >
           <DialogContent className="dialog-content" aria-label="dialog content">
+            <div className="dialog-header-row">
+              <div className="dialog-header">{header}</div>
+              <button
+                className="dialog-header-close-button"
+                onClick={onDismiss}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-x"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18"></line>
+                  <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </button>
+            </div>
             {children}
           </DialogContent>
         </DialogOverlay>
