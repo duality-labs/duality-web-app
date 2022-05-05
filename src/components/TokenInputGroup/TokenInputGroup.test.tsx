41a1da71bacf35db5ca02df16532dea3102ad22d
nickzoum <zoumuoz@gmail.com>
2022-05-06 00:02:06 +0300

    Tests for cleanInput function (TokenPicker)

---
+++ src/components/TokenInputGroup/TokenInputGroup.test.tsx
@@ -0,0 +1,42 @@
+import { cleanInput } from './utils';
+
+interface InputProperties {
+  selectionStart: number;
+  selectionEnd: number;
+  value: string;
+}
+
+test.concurrent.each([['0|', '0|']])(
+  'converts "%s" to "%s"',
+  function (originalValue, expectedValue) {
+    const { value, selectionStart, selectionEnd } = parseValue(originalValue);
+    const dom = createInput(value, selectionStart, selectionEnd);
+    const expected = parseValue(expectedValue);
+    cleanInput(dom);
+    expect(dom.value).toBe(expected.value);
+    expect(dom.selectionStart).toBe(expected.selectionStart);
+    expect(dom.selectionEnd).toBe(expected.selectionEnd);
+  }
+);
+
+function createInput(
+  value: string,
+  start: number,
+  end?: number
+): HTMLInputElement {
+  const input = document.createElement('input');
+  input.value = value;
+  input.selectionStart = start;
+  input.selectionStart = end ?? start;
+  return input;
+}
+
+function parseValue(text: string): InputProperties {
+  const start = text.indexOf('|');
+  const end = text.lastIndexOf('|');
+  return {
+    value: text.replace(/\|/g, ''),
+    selectionStart: start === -1 ? text.length : start,
+    selectionEnd: start === -1 ? text.length : start === end ? start : end - 1,
+  };
+}
