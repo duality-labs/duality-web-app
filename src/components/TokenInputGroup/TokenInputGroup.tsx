a3106867976c5fde320c1395ef8d3b29c8302913
nickzoum <zoumuoz@gmail.com>
2022-05-05 05:49:36 +0300

    numeric input data cleaning

--- src/components/TokenInputGroup/TokenInputGroup.tsx
+++ src/components/TokenInputGroup/TokenInputGroup.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TokenPicker from '../TokenPicker';
 
@@ -42,6 +42,7 @@ export default function TokenInputGroup({
         type="text"
         className="form-control"
         value={selectedValue ?? '...'}
+        onInput={onInput}
         onChange={(e) => onInputChange(e.target.value)}
       />
       <TokenPicker
@@ -62,5 +63,107 @@ export default function TokenInputGroup({
     if (newToken === exclusion) return;
     setToken(newToken);
     changeToken(newToken);
+  }
+}
+
+/**
+ * Clear invalid characters from the input
+ * @param {React.FormEvent<HTMLInputElement>} event change event
+ * @returns {void}
+ */
+function onInput(event: React.FormEvent<HTMLInputElement>) {
+  const target = event.currentTarget;
+  const value = target.value;
+  let selectionStart = target.selectionStart ?? value.length;
+  let selectionEnd = target.selectionEnd ?? value.length;
+  let firstDigitFound = false;
+  let pointFound = false;
+  let result = '';
+
+  // special case (0|0)
+  if (value === '00' && selectionStart === 1 && selectionEnd === 1) {
+    target.value = '0';
+    target.selectionStart = 1;
+    target.selectionEnd = 1;
+    return;
+  }
+
+  // remove invalid characters
+  for (let index = 0; index < value.length; index++) {
+    const char = value[index];
+    if (/\d/.test(char)) {
+      firstDigitFound = true;
+      result += char;
+    } else if (char === '.') {
+      if (pointFound) {
+        // Ignore multiple points (0._0)
+        removeChar(index);
+      } else if (firstDigitFound) {
+        // Check for the first point (0_0)
+        result += char;
+        pointFound = true;
+      } else {
+        // Check for the first point if no digit has been registered and add a 0 (_0)
+        result += '0' + char;
+        if (index < selectionStart) selectionStart += 1;
+        if (index < selectionEnd) selectionEnd += 1;
+        pointFound = true;
+      }
+    } else {
+      removeChar(index);
+    }
+  }
+
+  // remove leading zeros
+  result = result.replace(
+    /^(0+)(.?)/,
+    function (_: string, text: string, nextChar: string, index: number) {
+      if (!nextChar || nextChar === '.') {
+        removeChar(index, text.length - 1);
+        return `0${nextChar}`;
+      } else {
+        removeChar(index, text.length);
+        return nextChar;
+      }
+    }
+  );
+
+  // remove lagging zeros
+  result = result.replace(
+    /(\.)(.*)0+$/,
+    function (
+      fullText: string,
+      point: string,
+      previousText: string,
+      text: string,
+      index: number
+    ) {
+      if (previousText) {
+        removeChar(index, text.length - 1);
+        return `${point}${previousText}`;
+      } else {
+        removeChar(index + fullText.length - text.length + 1, text.length - 1);
+        return `${point}${previousText}${0}`;
+      }
+    }
+  );
+
+  // special case, fully empty text
+  if (!value) {
+    result = '0';
+    selectionStart = 0;
+    selectionEnd = 0;
+  }
+
+  target.value = result;
+  target.selectionStart = selectionStart;
+  target.selectionEnd = selectionEnd;
+
+  function removeChar(index: number, gap = 1) {
+    if (!gap) return;
+    for (let i = gap; i >= 1; i--) {
+      if (index <= selectionStart - i) selectionStart -= 1;
+      if (index <= selectionEnd - i) selectionEnd -= 1;
+    }
   }
 }
