7ec56b3c89b7a565d32341c0db64e6500f6b428b
Nick Z <nzoumis@outlook.com>
2022-05-10 03:31:48 +0300

    convert svg to html for proper css rotation

--- src/components/Orbs/Orbs.tsx
+++ src/components/Orbs/Orbs.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import './Orbs.scss';
 
@@ -6,29 +6,19 @@ interface OrbsProps {
   rate: number;
 }
 
-const xmlLink = 'http://www.w3.org/2000/svg',
-  xmlLinkX = 'http://www.w3.org/1999/xlink';
-
 export default function Orbs({ rate }: OrbsProps) {
   const [fullHeight, setfullHeight] = useState(0);
   const [fullWidth, setFullWidth] = useState(0);
   const bodyRef = useRef<HTMLDivElement>(null);
   const safeRate =
-    rate === 0 || rate === Infinity ? 1 : Math.min(Math.max(0.1, rate), 10);
+    !rate || rate === Infinity ? 1 : Math.min(Math.max(0.2, rate), 5);
   const baseOrbRadius = 50;
   const radiusA = baseOrbRadius * safeRate;
   const radiusB = baseOrbRadius / safeRate;
-  const dx = fullWidth / 3,
-    xCenter = fullWidth / 2;
-  const dy = (fullHeight * 3) / 7,
-    yCenter = fullHeight / 2;
-  const x0 = xCenter - dx,
-    x1 = xCenter + dx;
-  const y0 = yCenter - dy,
-    y1 = yCenter + dy;
-  const circlePath = `M ${x0}, ${yCenter}
-        C ${x0}, ${y1}  ${x1}, ${y1} ${x1}, ${yCenter}
-        C ${x1}, ${y0}  ${x0}, ${y0} ${x0}, ${yCenter}`;
+  const dx = fullWidth / 3;
+  const xCenter = fullWidth / 2;
+  const windowRatio = fullHeight / fullWidth;
+  const reverseRatio = 1 / windowRatio;
 
   const updateDimensions = useCallback(function () {
     setfullHeight(bodyRef.current?.offsetHeight || 0);
@@ -42,23 +32,42 @@ export default function Orbs({ rate }: OrbsProps) {
 
   return (
     <div className="orbs-bg" ref={bodyRef}>
-      <svg
-        width={fullWidth}
-        height={fullHeight}
-        xmlns={xmlLink}
-        xmlnsXlink={xmlLinkX}
+      <div
+        className="y-shrink"
+        style={{ '--scale-ratio': windowRatio } as React.CSSProperties}
       >
-        <path
-          d={circlePath}
-          className="circle circle-a"
-          style={{ strokeWidth: radiusA }}
-        />
-        <path
-          d={circlePath}
-          className="circle circle-b"
-          style={{ strokeWidth: radiusB }}
-        />
-      </svg>
+        <div className="rotate-slow">
+          <div
+            className="orb-container counter-rotate-slow"
+            style={{
+              width: radiusA * 2,
+              height: radiusA * 2,
+              top: xCenter - radiusA,
+              left: xCenter - dx - radiusA,
+            }}
+          >
+            <div
+              className="orb y-expand"
+              style={{ '--scale-ratio': reverseRatio } as React.CSSProperties}
+            ></div>
+          </div>
+
+          <div
+            className="orb-container counter-rotate-slow"
+            style={{
+              width: radiusB * 2,
+              height: radiusB * 2,
+              top: xCenter - radiusB,
+              left: xCenter + dx - radiusB,
+            }}
+          >
+            <div
+              className="orb y-expand"
+              style={{ '--scale-ratio': reverseRatio } as React.CSSProperties}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
