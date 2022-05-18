eb2360eff57fa9915ba346a0c1dd9b7fd2b41bf0
David <dib542@gmail.com>
2022-05-19 00:14:16 +1000

    Add release docs

--- release.config.js
+++ release.config.js
@@ -1,9 +1,15 @@
+/**
+ * config for Semantic Release workflow
+ * docs: https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration
+ **/
+
 module.exports = {
   branches: ["main"],
   dryRun: false,
   plugins: [
     ["@semantic-release/commit-analyzer", {
       preset: "angular",
+      // see default rules: https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
       releaseRules: [
         { type: "feat", release: "minor" },
         { type: "fix", release: "patch" },
