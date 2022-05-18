b882393f40ba5e54f59090ff40e7fa366447b70b
David <dib542@gmail.com>
2022-05-19 00:12:32 +1000

    Format as js not json

--- release.config.js
+++ release.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "branches": ["main"],
-  "dryRun": false,
-  "plugins": [
+  branches: ["main"],
+  dryRun: false,
+  plugins: [
     ["@semantic-release/commit-analyzer", {
-      "preset": "angular",
-      "releaseRules": [
-        { "type": "feat", "release": "minor" },
-        { "type": "fix", "release": "patch" },
-        { "type": "perf", "release": "patch" }
-      ]
+      preset: "angular",
+      releaseRules: [
+        { type: "feat", release: "minor" },
+        { type: "fix", release: "patch" },
+        { type: "perf", release: "patch" },
+      ],
     }],
-    "@semantic-release/github"
-  ]
+    "@semantic-release/github",
+  ],
 }
