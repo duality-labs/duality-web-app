7b60e5049709cd33f30a1ce646988b5d3fef3380
Nick Z <nzoumis@outlook.com>
2022-05-25 04:59:14 +0300

    basic interfaces for the swap hooks

---
+++ src/pages/Swap/hooks/index.d.ts
@@ -0,0 +1,29 @@
+export interface PairRequest {
+  /** address of altered token */
+  address0?: string;
+  /** address of other token */
+  address1?: string;
+  /** value of altered token */
+  value0?: string;
+}
+
+export interface PairResult {
+  /** address of token 0 */
+  address0: string;
+  /** address of token 1 */
+  address1: string;
+  /** value for token 1 */
+  value0: string;
+  /** (estimated) value for token 2 */
+  value1: string;
+  /** (estimated) rate of exchange */
+  rate: string;
+  /** (estimated) gas fee */
+  gas: string;
+}
+
+export interface ErrorMessage {
+  message: string;
+  stack: string;
+  code: number;
+}
