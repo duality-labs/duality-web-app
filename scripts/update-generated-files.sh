88bc2e555d20e37586c10299a4712da4eeec7bc9
David <dib542@gmail.com>
2022-07-05 19:30:14 +1000

    Add potential generated script fix

--- scripts/update-generated-files.sh
+++ scripts/update-generated-files.sh
@@ -14,3 +14,11 @@ cp -r "$DUALITY_CORE_DIRECTORY/vue/src/store/generated/duality/duality.duality/p
 # copy readme info
 cp -r "$DUALITY_CORE_DIRECTORY/vue/src/store/generated/readme.md" \
       "src/generated/readme.md"
+
+# TODO: fix Starports terrible code
+# the follow code should work in some environment?: https://unix.stackexchange.com/questions/26284/how-can-i-use-sed-to-replace-a-multi-line-string#525524
+
+REPLACE_THIS="let client;\n  if (addr) {\n    client = await SigningStargateClient.connectWithSigner(addr, wallet, { registry });\n  }else{\n    client = await SigningStargateClient.offline( wallet, { registry });\n  }"
+REPLACE_WITH="const client = addr\n    ? await SigningStargateClient.connectWithSigner(addr, wallet, { registry })\n    : await SigningStargateClient.offline( wallet, { registry });"
+
+sed -z "s/$REPLACE_THIS/$REPLACE_WITH/" -i src/generated/duality/duality.duality/module/index.ts
