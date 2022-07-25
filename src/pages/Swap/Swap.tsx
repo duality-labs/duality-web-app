ec69ea492f76e98eea44a446fe9edcf957729192
Nick Z <nzoumis@outlook.com>
2022-07-26 02:14:14 +0300

    feat: 85 update swap page logic to cosmjs usage (#93)
    * indexer provider useContext
    
    * basic mock router implementation
    
    * indexer provider use events
    
    * indexer provider: fix name + import style of env vars
    
    * indexer provider: imporve documentation for id helpers
    
    * indexer provider fix context type
    
    * swap throw not implemented exception
    
    * indexer provider remove unnecessary promises
    
    * indexer + router refactor math
    
    * indexer provider undo old state mutation
    
    * only store the base url in the env files
    
    * indexer provider swr + get single pair data
    
    * rename token0 and token1 to A and B where applicable
    
    * clear errors after a successful event
    
    * swap/router: better representation of PairRequest
    
    * reorganize router/indexer files

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -9,7 +9,7 @@ import {
 
 import { PairRequest } from './hooks/index';
 
-import { useIndexer } from './hooks/useIndexer';
+import { useRouter } from './hooks/useRouter';
 import { useSwap } from './hooks/useSwap';
 
 import './Swap.scss';
@@ -25,10 +25,11 @@ export default function Swap() {
     data: rateData,
     isValidating: isValidatingRate,
     error: rateError,
-  } = useIndexer({
-    token0: lastUpdatedA ? tokenA?.address : tokenB?.address,
-    token1: lastUpdatedA ? tokenB?.address : tokenA?.address,
-    value0: lastUpdatedA ? valueA : valueB,
+  } = useRouter({
+    tokenA: tokenA?.address,
+    tokenB: tokenB?.address,
+    valueA: lastUpdatedA ? valueA : undefined,
+    valueB: lastUpdatedA ? undefined : valueB,
   });
   const [swapRequest, setSwapRequest] = useState<PairRequest>();
   const {
@@ -38,8 +39,8 @@ export default function Swap() {
   } = useSwap(swapRequest);
   const dotCount = useDotCounter(0.25e3);
 
-  const valueAConverted = lastUpdatedA ? valueA : rateData?.value1;
-  const valueBConverted = lastUpdatedA ? rateData?.value1 : valueB;
+  const valueAConverted = lastUpdatedA ? valueA : rateData?.valueA;
+  const valueBConverted = lastUpdatedA ? rateData?.valueB : valueB;
 
   const swapTokens = useCallback(
     function () {
@@ -47,7 +48,7 @@ export default function Swap() {
       setTokenB(tokenA);
       setValueA(valueBConverted);
       setValueB(valueAConverted);
-      setLastUpdatedA((a) => !a);
+      setLastUpdatedA((flag) => !flag);
     },
     [tokenA, tokenB, valueAConverted, valueBConverted]
   );
@@ -56,12 +57,13 @@ export default function Swap() {
     function (event?: React.FormEvent<HTMLFormElement>) {
       if (event) event.preventDefault();
       setSwapRequest({
-        token0: tokenA?.address,
-        token1: tokenB?.address,
-        value0: valueAConverted,
+        tokenA: tokenA?.address,
+        tokenB: tokenB?.address,
+        valueA: lastUpdatedA ? valueA : undefined,
+        valueB: lastUpdatedA ? undefined : valueB,
       });
     },
-    [tokenA?.address, tokenB?.address, valueAConverted]
+    [tokenA?.address, tokenB?.address, valueA, valueB, lastUpdatedA]
   );
 
   const onValueAChanged = useCallback((newValue: string) => {
@@ -121,7 +123,7 @@ export default function Swap() {
       <div className="text-red-500">{rateError}</div>
       <div className="text-sky-500">
         {!isValidatingSwap && swapResponse
-          ? `Traded ${swapResponse.value0} ${swapResponse.token0} to ${swapResponse.value1} ${swapResponse.token1}`
+          ? `Traded ${swapResponse.valueA} ${swapResponse.tokenA} to ${swapResponse.valueB} ${swapResponse.tokenB}`
           : ''}
       </div>
       <input
---
+++ src/pages/Swap/hooks/router.ts
@@ -0,0 +1,98 @@
+import { PairMap } from '../../../lib/web3/indexerProvider';
+import { RouterResult } from './index';
+import { BigNumber } from 'bignumber.js';
+
+// mock implementation of router (no hop)
+export function router(
+  state: PairMap,
+  tokenA: string,
+  tokenB: string,
+  value0: string
+): RouterResult {
+  const [token0, token1] = [tokenA, tokenB].sort();
+  const exactPair = Object.values(state).find(
+    (pairInfo) => pairInfo.token0 === token0 && pairInfo.token1 === token1
+  );
+  if (!exactPair) {
+    throw new Error('There are no ticks for the supplied token pair');
+  } else {
+    const sortMultiplier = token1 === tokenA ? -1 : 1;
+    const sortedTicks = Object.values(exactPair.ticks).sort(
+      (tick0, tick1) =>
+        sortMultiplier *
+        tick0.price0
+          .dividedBy(tick0.price1)
+          .comparedTo(tick1.price0.dividedBy(tick1.price1))
+    );
+    return {
+      amountIn: new BigNumber(value0),
+      tokens: [tokenA, tokenB],
+      prices0: [sortedTicks.map((tickInfo) => tickInfo.price0)], // price
+      prices1: [sortedTicks.map((tickInfo) => tickInfo.price1)],
+      fees: [sortedTicks.map((tickInfo) => tickInfo.fee)],
+      reserves0: [sortedTicks.map((tickInfo) => tickInfo.reserves0)], // reserves
+      reserves1: [sortedTicks.map((tickInfo) => tickInfo.reserves1)],
+    };
+  }
+}
+
+export async function routerAsync(
+  state: PairMap,
+  token0: string,
+  token1: string,
+  value0: string
+): Promise<RouterResult> {
+  return await router(state, token0, token1, value0);
+}
+
+/**
+ * Calculates the amountOut using the (amountIn * price0) / (price1) formula
+ * for each tick, until the amountIn amount has been covered
+ * @param data the RouteInput struct
+ * @returns estimated value for amountOut
+ */
+export function calculateOut(data: RouterResult): BigNumber {
+  let amountLeft = data.amountIn;
+  let amountOut = new BigNumber(0);
+  for (let pairIndex = 0; pairIndex < data.tokens.length - 1; pairIndex++) {
+    const tokens = [data.tokens[pairIndex], data.tokens[pairIndex + 1]].sort();
+    for (
+      let tickIndex = 0;
+      tickIndex < data.prices0[pairIndex].length;
+      tickIndex++
+    ) {
+      const isSameOrder = tokens[0] === data.tokens[pairIndex];
+      const priceIn = isSameOrder
+        ? data.prices0[pairIndex][tickIndex]
+        : data.prices1[pairIndex][tickIndex];
+      const priceOut = isSameOrder
+        ? data.prices1[pairIndex][tickIndex]
+        : data.prices0[pairIndex][tickIndex];
+      const reservesOut = isSameOrder
+        ? data.reserves1[pairIndex][tickIndex]
+        : data.reserves0[pairIndex][tickIndex];
+      const maxOut = amountLeft.multipliedBy(priceIn).dividedBy(priceOut);
+
+      if (reservesOut.isLessThan(maxOut)) {
+        const amountInTraded = reservesOut
+          .multipliedBy(priceOut)
+          .dividedBy(priceIn);
+        amountLeft = amountLeft.minus(amountInTraded);
+        amountOut = amountOut.plus(reservesOut);
+        if (amountLeft.isEqualTo(0)) return amountOut;
+        if (amountLeft.isLessThan(0))
+          throw new Error(
+            'Error while calculating amount out (negative amount)'
+          );
+      } else {
+        return amountOut.plus(maxOut);
+      }
+    }
+  }
+  return amountOut;
+}
+
+// mock implementation of fee calculation
+export function calculateFee(data: RouterResult): BigNumber {
+  return data.fees[0][0];
+}
