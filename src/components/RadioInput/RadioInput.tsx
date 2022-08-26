d9bf412cf45939a330bbd85d2c78d9cd6593342f
Nick Z <nzoumis@outlook.com>
2022-08-26 16:21:29 +0300

    feat: 116 radio input wrapper component (#117)
    * basic radio input component
    
    * ids and names for elements + option to the selection by key
    
    * option to set the max column count
    
    * pool example usage (revert before merge)
    
    * Change RadioInput to accept list of renderOptions (from:@nickzoum):
    
        - instead of manipulating children components
    
    * Simplify setting RadioInput children from list
    
    * Simplify RadioInput setting selectedIndex
    
    * Optimise RadioInput label re-rendering
    
    * Allow RadioInput to accept custom OptionComponents
    
    * Export OptionProps interface for custom OptionComponent components
    
        - including example usage
    
    * Remove RadioInput usage example
    
    Co-authored-by: David Uhlmann <dib542@gmail.com>

---
+++ src/components/RadioInput/RadioInput.tsx
@@ -0,0 +1,68 @@
+import { Fragment, useId, useMemo } from 'react';
+import './RadioInput.scss';
+
+export interface OptionProps<T> {
+  option: T;
+  id: string;
+  index: number;
+}
+interface RadioInputProps<T> {
+  OptionComponent?: React.ElementType<OptionProps<T>>;
+  onChange?: (value: T, index: number) => void;
+  list: Array<T>;
+  maxColumnCount?: number;
+  value?: T;
+  index?: number;
+  name?: string;
+}
+
+function DefaultOptionComponent<T>({ option }: OptionProps<T>) {
+  return <span>{`${option}`}</span>;
+}
+
+export default function RadioInput<T>({
+  OptionComponent = DefaultOptionComponent,
+  onChange,
+  list,
+  maxColumnCount,
+  value,
+  name,
+}: RadioInputProps<T>) {
+  const selectedIndex = value !== undefined ? list.indexOf(value) : -1;
+  const groupID = useId();
+  const groupName = name || groupID;
+  const labelStyle = useMemo(
+    () => ({
+      // set column width style
+      flexBasis: maxColumnCount ? `${100 / maxColumnCount}%` : undefined,
+    }),
+    [maxColumnCount]
+  );
+
+  return (
+    <div className="radio-input-group">
+      {list.map((option, index) => {
+        const id = `${groupName}-${index}`;
+
+        return (
+          <Fragment key={id}>
+            <input
+              type="radio"
+              name={groupName}
+              id={id}
+              checked={index === selectedIndex}
+              onChange={() => onChange?.(option, index)}
+            ></input>
+            <label
+              htmlFor={id}
+              className="button button-primary"
+              style={labelStyle}
+            >
+              <OptionComponent option={option} id={id} index={index} />
+            </label>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
