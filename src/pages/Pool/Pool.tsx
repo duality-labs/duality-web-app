e804a6d5d11ac777ee44a83264454d14d5786fca
David Uhlmann <dib542@gmail.com>
2022-09-10 10:13:09 +1000

    Convert LiquiditySelector numbers to BigNumber for improved accuracy

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -132,9 +132,9 @@ export default function Pool() {
           tokenB,
           new BigNumber(feeType.fee),
           userTicks.map(([price, amount0, amount1]) => [
-            new BigNumber(price),
-            new BigNumber(amount0).dividedBy(denomRatio),
-            new BigNumber(amount1).dividedBy(denomRatio),
+            price,
+            amount0.dividedBy(denomRatio),
+            amount1.dividedBy(denomRatio),
           ])
         );
       }
