bcf33df830e3d57c1d7add904fba9416f7ce6bcd
David Uhlmann <dib542@gmail.com>
2022-09-15 14:18:56 +1000

    Create useOnContinualPress hook

---
+++ src/components/hooks/useOnContinualPress.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect, useCallback } from 'react';
+
+export default function useOnContinualPress(
+  onTimeout: () => void,
+  { delay = Infinity, interval = 50 } = {}
+) {
+  // set active state and handlers
+  const [active, setActive] = useState(false);
+  const stop = useCallback(() => setActive(false), []);
+  const start = useCallback(() => {
+    if (delay < Infinity) {
+      const timeout = setTimeout(() => setActive(true), delay);
+      return () => clearTimeout(timeout);
+    }
+  }, [delay]);
+
+  useEffect(() => {
+    if (active) {
+      const timeout = setInterval(() => onTimeout(), interval);
+      return () => clearInterval(timeout);
+    }
+  }, [active, interval, onTimeout]);
+
+  return [start, stop];
+}
