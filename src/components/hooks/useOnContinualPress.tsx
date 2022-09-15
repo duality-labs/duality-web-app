cdbc9674bbf9201717e3ab7a1dc16c0f839e238f
David Uhlmann <dib542@gmail.com>
2022-09-15 14:31:09 +1000

    Redo delay and interval options delay of 0 by default

--- src/components/hooks/useOnContinualPress.tsx
+++ src/components/hooks/useOnContinualPress.tsx
@@ -2,18 +2,18 @@ import { useState, useEffect, useCallback } from 'react';
 
 export default function useOnContinualPress(
   onTimeout: () => void,
-  interval = 50,
-  delay = interval
+  delay = 0,
+  interval = 50
 ) {
   // set active state and handlers
   const [active, setActive] = useState(false);
   const stop = useCallback(() => setActive(false), []);
   const start = useCallback(() => {
-    if (delay < Infinity && delay >= interval) {
-      const timeout = setTimeout(() => setActive(true), delay - interval);
+    if (delay < Infinity && delay) {
+      const timeout = setTimeout(() => setActive(true), delay);
       return () => clearTimeout(timeout);
     }
-  }, [delay, interval]);
+  }, [delay]);
 
   useEffect(() => {
     if (active) {
