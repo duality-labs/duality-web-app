2de8fc931ba3f84b18760877db164598d2217465
David Uhlmann <dib542@gmail.com>
2022-09-28 17:25:38 +1000

    Allow removing or not of trailing zeros

--- src/lib/bignumber.utils.ts
+++ src/lib/bignumber.utils.ts
@@ -4,13 +4,19 @@ import BigNumber from 'bignumber.js';
  * Format Price information nicely to a certain amount of significant digits
  * @param value BigNumber
  * @param significantDigits number
+ * @param removeTrailingZeros boolean
  * @returns string
  */
-export function formatPrice(value: BigNumber, significantDigits = 3): string {
-  return value
-    .dp(
-      Math.max(0, value.dp() - value.sd(true) + significantDigits),
-      BigNumber.ROUND_HALF_UP
-    )
-    .toFixed();
+export function formatPrice(
+  value: BigNumber,
+  significantDigits = 3,
+  removeTrailingZeros = true
+): string {
+  const decimalPlaces = Math.max(
+    0,
+    value.dp() - value.sd(true) + significantDigits
+  );
+  return removeTrailingZeros
+    ? value.dp(decimalPlaces, BigNumber.ROUND_HALF_UP).toFixed()
+    : value.toFixed(decimalPlaces, BigNumber.ROUND_HALF_UP);
 }
