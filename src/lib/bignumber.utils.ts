64064a8d1bf50dd8af131c516d9ce96f5326f7e4
David Uhlmann <dib542@gmail.com>
2022-09-28 17:23:01 +1000

    Allow adjusting of significant digits

--- src/lib/bignumber.utils.ts
+++ src/lib/bignumber.utils.ts
@@ -3,10 +3,14 @@ import BigNumber from 'bignumber.js';
 /**
  * Format Price information nicely to a certain amount of significant digits
  * @param value BigNumber
+ * @param significantDigits number
  * @returns string
  */
-export function formatPrice(value: BigNumber): string {
+export function formatPrice(value: BigNumber, significantDigits = 3): string {
   return value
-    .dp(Math.max(0, value.dp() - value.sd(true) + 3), BigNumber.ROUND_HALF_UP)
+    .dp(
+      Math.max(0, value.dp() - value.sd(true) + significantDigits),
+      BigNumber.ROUND_HALF_UP
+    )
     .toFixed();
 }
