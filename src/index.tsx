0ef5123c44da4adde5ab4ecc2207a431d1057050
David Uhlmann <dib542@gmail.com>
2022-10-01 12:17:59 +1000

    style: update base styles to new design (#132)
    * Fix color scheme lightness
    
    * Add some basic flex global styles
    
    * Add CSS reset and normalisation
    
    * Add default paragraph line height
    
    * Add current design font
    
    * Style headings in semibold
    
    * Add global disabled styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import './index.scss';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
