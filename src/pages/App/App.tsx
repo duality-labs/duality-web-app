3dd63f58ab35f4f22f8708e30aec2b5bece93dab
David Uhlmann <dib542@gmail.com>
2022-10-05 07:25:59 +1000

    Add Chakra provider

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,6 +1,7 @@
 import { Web3Provider } from '../../lib/web3/useWeb3';
 import { IndexerProvider } from '../../lib/web3/indexerProvider';
 import { ThemeProvider } from '../../lib/themeProvider';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -14,20 +15,22 @@ function App() {
   return (
     <Web3Provider>
       <IndexerProvider>
-        <ThemeProvider>
-          <BrowserRouter>
-            <Header />
-            <main>
-              <Routes>
-                <Route index element={<Swap />} />
-                <Route path="add-liquidity" element={<Pool />} />
-                <Route path="my-liquidity" element={<div>Coming soon</div>} />
-                <Route path="stake" element={<div>Coming soon</div>} />
-                <Route path="*" element={<div>Not found</div>} />
-              </Routes>
-            </main>
-          </BrowserRouter>
-        </ThemeProvider>
+        <ChakraProvider>
+          <ThemeProvider>
+            <BrowserRouter>
+              <Header />
+              <main>
+                <Routes>
+                  <Route index element={<Swap />} />
+                  <Route path="add-liquidity" element={<Pool />} />
+                  <Route path="my-liquidity" element={<div>Coming soon</div>} />
+                  <Route path="stake" element={<div>Coming soon</div>} />
+                  <Route path="*" element={<div>Not found</div>} />
+                </Routes>
+              </main>
+            </BrowserRouter>
+          </ThemeProvider>
+        </ChakraProvider>
       </IndexerProvider>
     </Web3Provider>
   );
