4fb76485b5422644592db41ddbdd726c1872fe3f
David Uhlmann <dib542@gmail.com>
2022-10-06 05:01:25 +1000

    Show 'Insufficient Balance' on relevant tokens

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -80,9 +80,7 @@ export default function Swap() {
   const hasFormData =
     address && tokenA && tokenB && valueAConvertedNumber.isGreaterThan(0);
   const hasSufficientFunds =
-    (hasFormData &&
-      valueAConvertedNumber.isLessThanOrEqualTo(balanceTokenA || 0)) ||
-    false;
+    valueAConvertedNumber.isLessThanOrEqualTo(balanceTokenA || 0) || false;
 
   const [slippage, setSlippage] = useState(defaultSlippage);
 
@@ -209,6 +207,7 @@ export default function Swap() {
         </div>
         <div className="card-row">
           <TokenInputGroup
+            variant={!hasSufficientFunds && 'error'}
             onValueChanged={onValueAChanged}
             onTokenChanged={setTokenA}
             tokenList={tokenList}
@@ -307,7 +306,7 @@ export default function Swap() {
         )}
         <div className="my-4">
           {address ? (
-            hasSufficientFunds ? (
+            hasFormData && hasSufficientFunds ? (
               <button
                 className="submit-button button-primary"
                 type="submit"
