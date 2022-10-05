870575ce68070c7322ccfc418bbd658ea0ce495b
David Uhlmann <dib542@gmail.com>
2022-10-05 16:38:25 +1000

    Add background to ChakraUI theme

--- src/pages/App/theme.ts
+++ src/pages/App/theme.ts
@@ -1,10 +1,23 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { GlobalStyles } from '@chakra-ui/theme-tools';
+import type { StyleFunctionProps } from '@chakra-ui/styled-system';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: true,
 };
 
-const theme = extendTheme({ config });
+const styles: GlobalStyles = {
+  global: (props: StyleFunctionProps) => ({
+    body: {
+      bg:
+        props.colorMode === 'dark'
+          ? 'fixed #fff conic-gradient(from 120deg at 50% 100%, rgb(78, 177, 232) 0deg, rgba(18, 92, 135, 0.8) 180deg, rgba(0, 0, 0, 0.8) 360deg)'
+          : 'fixed #fff conic-gradient(from 120deg at 50% 100%, rgb(78, 177, 232) 0deg, rgba(78, 177, 232, 0.8) 180deg, rgba(255, 255, 255, 0.8) 360deg)',
+    },
+  }),
+};
+
+const theme = extendTheme({ config, styles });
 
 export default theme;
