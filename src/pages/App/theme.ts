500bdf20a6bded13f02224beefa7db33538ef52f
David Uhlmann <dib542@gmail.com>
2022-10-07 08:09:37 +1000

    Revert "Make background more dark in dark mode"
    This reverts commit 41eebb0b76d9560190773cd291b9220e5e154888.

--- src/pages/App/theme.ts
+++ src/pages/App/theme.ts
@@ -13,7 +13,7 @@ const styles: Theme['styles'] = {
     body: {
       bg:
         props.colorMode === 'dark'
-          ? 'fixed #000 conic-gradient(from 120deg at 50% 100%, rgb(78, 177, 232) 0deg, rgba(18, 92, 135, 0.8) 180deg, rgba(0, 0, 0, 0.8) 360deg)'
+          ? 'fixed #fff conic-gradient(from 120deg at 50% 100%, rgb(78, 177, 232) 0deg, rgba(18, 92, 135, 0.8) 180deg, rgba(0, 0, 0, 0.8) 360deg)'
           : 'fixed #fff conic-gradient(from 120deg at 50% 100%, rgb(78, 177, 232) 0deg, rgba(78, 177, 232, 0.8) 180deg, rgba(255, 255, 255, 0.8) 360deg)',
     },
   }),
