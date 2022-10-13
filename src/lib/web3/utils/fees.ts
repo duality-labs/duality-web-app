4fc06016bf0008963594416c61bd700139813255
David Uhlmann <dib542@gmail.com>
2022-10-14 06:42:31 +1000

    Abstract out fee utils

---
+++ src/lib/web3/utils/fees.ts
@@ -0,0 +1,16 @@
+export interface FeeType {
+  fee: number;
+  label: string;
+  description: string;
+}
+
+export const feeTypes: Array<FeeType> = Object.entries({
+  '0.01%': 'Best for very stable pairs.',
+  '0.05%': 'Best for  stable pairs.',
+  '0.30%': 'Best for most assets.',
+  '1.00%': 'Best for exotic assets.',
+}).map(([label, description]) => ({
+  label,
+  fee: Number(label.replace(/%$/, '')) / 100,
+  description,
+}));
