d4fb826207ad2b91359844c64a9c62b8f134fdc4
David Uhlmann <dib542@gmail.com>
2022-10-21 03:07:58 +1000

    feat: improve radio button group input usage (#161)
    * Allow RadioButtonGroupInput to accept an order Map of values
    
    * Allow RadioButtonGroupInput to accept a customized button class
    
    * Replace button-switch-group with RadioButtonGroupInput on Pool page
    
    * Allow numeric keys to be used as keys for RadioButtonGroupInput
    
    * Reduce number of maximum tabs possible in RadioButtonGroupInput
    
    * Improve number of maximum tabs possible in RadioButtonGroupInput
    
    * Fix RadioButtonGroupInput animation when values change issue
    
    * Reweight RadioButtonGroupInput included indexes:
    
        - weight distance non-linearly
        - increase weighting for extents
    
    * Rearrange RadioButtonGroupInput ellipsis button calculations:
    
        - so that they don't need to be calculated for non-ellipsis buttons
    
    * Protect against array out-of-bounds conditions
    
    * Optimize RadioButtonGroupInput: don't include useless ellipsis buttons
    
    * Remove color override of RadioButtonGroupInput on Pool page

--- src/components/RadioButtonGroupInput/RadioButtonGroupInput.tsx
+++ src/components/RadioButtonGroupInput/RadioButtonGroupInput.tsx
@@ -2,13 +2,14 @@ import {
   ReactNode,
   useCallback,
   useLayoutEffect,
+  useMemo,
   useRef,
   useState,
 } from 'react';
 
 import './RadioButtonGroupInput.scss';
 
-function useSelectedButtonBackgroundMove<T extends string>(
+function useSelectedButtonBackgroundMove<T extends string | number>(
   value: T
 ): [
   (ref: HTMLButtonElement | null) => void,
@@ -42,7 +43,7 @@ function useSelectedButtonBackgroundMove<T extends string>(
       if (movingButton && targetButton) {
         movingButton.style.width = `${targetButton.offsetWidth}px`;
         movingButton.style.left = `${targetButton.offsetLeft}px`;
-        if (newValue) {
+        if (newValue !== undefined) {
           movingButton.classList.add('transition-ready');
         } else {
           movingButton?.classList.remove('transition-ready');
@@ -59,34 +60,84 @@ function useSelectedButtonBackgroundMove<T extends string>(
       lastValue.current = value;
       updateValue(value);
     } else {
-      updateValue();
+      // todo: this has been changed to allow rerendering changed components to cause an animation
+      // this has however caused the animation to start on component creation
+      updateValue(value);
     }
   });
 
   return [movingButtonRef, createRefForValue];
 }
 
-interface Props<T extends string> {
+interface Props<T extends string | number> {
   className?: string;
-  values: { [value in T]: ReactNode } | T[];
+  buttonClassName?: string;
+  values: { [value in T]: ReactNode } | Map<T, ReactNode> | T[];
   value: T;
   onChange: (value: T) => void;
 }
 
-export default function RadioButtonGroupInput<T extends string>({
+export default function RadioButtonGroupInput<T extends string | number>({
   className,
+  buttonClassName,
   values = [],
   value,
   onChange,
 }: Props<T>) {
   const [movingAssetRef, createRefForValue] =
     useSelectedButtonBackgroundMove<T>(value);
-  const entries = Array.isArray(values)
-    ? values.map<[T, string]>((value) => [value, value])
-    : (Object.entries(values).map(([value, description]) => [
-        value,
-        description,
-      ]) as [T, string][]);
+  const entries = useMemo(() => {
+    return Array.isArray(values)
+      ? values.map<[T, string]>((value) => [value, `${value}`])
+      : values instanceof Map
+      ? Array.from(values.entries())
+      : (Object.entries(values).map(([value, description]) => [
+          value,
+          description,
+        ]) as [T, string][]);
+  }, [values]);
+  const selectedIndex = entries.findIndex(
+    ([entryValue]) => entryValue === value
+  );
+  const includedIndexes = useMemo(() => {
+    return (
+      entries
+        .map((_, index, entries) => {
+          // cumulate weightings
+          let result = 0;
+          // weight extents of list
+          if (index === 0 || index === entries.length - 1) {
+            result += 25;
+          }
+          // weight start of list
+          if (index < 5) {
+            result += Math.pow(5 - index, 2);
+          }
+          // weight end of list
+          if (index >= entries.length - 5) {
+            result += Math.pow(5 - (entries.length - 1 - index), 2);
+          }
+          // weight to left near selection
+          if (index >= selectedIndex - 4 && index <= selectedIndex) {
+            result += Math.pow(5 - (selectedIndex - index), 2);
+          }
+          // weight to right near selection
+          if (index >= selectedIndex && index <= selectedIndex + 4) {
+            result += Math.pow(5 - (index - selectedIndex), 2);
+          }
+          return [index, result];
+        })
+        // get most weighted indexes
+        .sort((a, b) => b[1] - a[1])
+        // truncated to top 10
+        .slice(0, 10)
+        // get just the indexes, remove the weightings
+        .map((a) => a[0])
+        // sort indexes in order
+        .sort((a, b) => a - b)
+    );
+  }, [entries, selectedIndex]);
+
   return (
     <div
       className={['radio-button-group-switch', className]
@@ -94,21 +145,60 @@ export default function RadioButtonGroupInput<T extends string>({
         .join(' ')}
     >
       <button
-        className="button moving-background"
+        className={['button moving-background', buttonClassName]
+          .filter(Boolean)
+          .join(' ')}
         disabled
         ref={movingAssetRef}
       />
-      {entries.map(([entryValue, description]) => (
-        <button
-          key={entryValue}
-          type="button"
-          className="button non-moving"
-          ref={createRefForValue(entryValue)}
-          onClick={() => onChange(entryValue)}
-        >
-          {description}
-        </button>
-      ))}
+      {entries.flatMap(([entryValue, description], index, entries) => {
+        const previousIndex = includedIndexes.includes(index - 1);
+        const currentIndex = includedIndexes.includes(index);
+        const nextIndex = includedIndexes.includes(index + 1);
+
+        // include button if required or if excluding it
+        // will not reduce the number of shown buttons
+        if (currentIndex || (previousIndex && nextIndex)) {
+          return (
+            <button
+              key={entryValue}
+              type="button"
+              className={['button non-moving', buttonClassName]
+                .filter(Boolean)
+                .join(' ')}
+              ref={createRefForValue(entryValue)}
+              onClick={() => onChange(entryValue)}
+            >
+              {description}
+            </button>
+          );
+        }
+
+        // button is not included and button before this was also not included (ignore)
+        if (!previousIndex) return [];
+
+        // else calculate value to use for an ellipsis (…) button
+        const nextIncludedIndex = includedIndexes.indexOf(index - 1) + 1;
+        const nextAverageIndex =
+          nextIncludedIndex < includedIndexes.length
+            ? Math.floor((includedIndexes[nextIncludedIndex] + index) / 2)
+            : includedIndexes.length - 1; // don't look past array bounds
+        const nextAverageKey = entries[nextAverageIndex][0];
+
+        return (
+          <button
+            key={nextAverageKey}
+            type="button"
+            className={['button non-moving', buttonClassName]
+              .filter(Boolean)
+              .join(' ')}
+            ref={createRefForValue(nextAverageKey)}
+            onClick={() => onChange(nextAverageKey)}
+          >
+            …
+          </button>
+        );
+      })}
     </div>
   );
 }
