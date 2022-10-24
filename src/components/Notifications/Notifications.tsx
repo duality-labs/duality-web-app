50e23b1480b739e7c4782f5795b5f8ad5531583d
David Uhlmann <dib542@gmail.com>
2022-10-25 00:26:36 +1000

    Add react-hot-toasts Notifications with transistions example:
        - modified to use Sass classes instead of all inline styles

---
+++ src/components/Notifications/Notifications.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react';
+import { useToaster } from 'react-hot-toast/headless';
+
+import './Notifications.scss';
+
+export default function Notifications() {
+  const { toasts, handlers } = useToaster();
+  const { startPause, endPause, calculateOffset, updateHeight } = handlers;
+
+  return (
+    <div
+      className="notifications"
+      onMouseEnter={startPause}
+      onMouseLeave={endPause}
+    >
+      {toasts.map((toast) => {
+        const offset = calculateOffset(toast, {
+          reverseOrder: false,
+          gutter: 8,
+        });
+
+        const ref = (el: HTMLDivElement) => {
+          if (el && typeof toast.height !== 'number') {
+            const height = el.getBoundingClientRect().height;
+            updateHeight(toast.id, height);
+          }
+        };
+
+        return (
+          <div
+            key={toast.id}
+            ref={ref}
+            className="notification"
+            style={{
+              opacity: toast.visible ? 1 : 0,
+              transform: `translateY(${offset}px)`,
+            }}
+            {...toast.ariaProps}
+          >
+            {toast.message as ReactNode}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
