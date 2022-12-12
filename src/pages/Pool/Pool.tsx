787abb413afe7cb101de2e44258b9b6dcd0a39ce
David Uhlmann <dib542@gmail.com>
2022-12-13 06:52:15 +1000

    Remove space between token names in token pair

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -816,9 +816,7 @@ export default function Pool() {
               <div className="chart-header row my-4">
                 <h3 className="h3 text-normal">Liquidity Distribution</h3>
                 <span className="tokens-badge badge-default badge-large font-console">
-                  {tokenA?.symbol}
-                  <span className="mx-2">/</span>
-                  {tokenB?.symbol}
+                  {tokenA?.symbol}/{tokenB?.symbol}
                 </span>
                 <button type="button" className="icon-button" onClick={swapAll}>
                   <FontAwesomeIcon
