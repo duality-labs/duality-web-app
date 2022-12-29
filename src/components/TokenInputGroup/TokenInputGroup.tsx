079ac5507b22ba0815af03cdc7426ebc3696ba5e
David Uhlmann <dib542@gmail.com>
2022-12-30 05:01:25 +1000

    Fix token input "max value" button output with 18 zero decimal places

--- src/components/TokenInputGroup/TokenInputGroup.tsx
+++ src/components/TokenInputGroup/TokenInputGroup.tsx
@@ -110,10 +110,12 @@ export default function TokenInputGroup({
             onClick={() =>
               onValueChanged?.(
                 // allow max value be as long as it needs to be to perfectly fit user's balance
-                new BigNumber(maxValue).toFixed(
-                  maxFractionDigits,
-                  BigNumber.ROUND_DOWN
-                )
+                new BigNumber(maxValue)
+                  .toFixed(maxFractionDigits, BigNumber.ROUND_DOWN)
+                  // replace trailing zeros
+                  .replace(/\.([0-9]*[1-9])?0+$/, '.$1')
+                  // remove trailing decimal point
+                  .replace(/\.$/, '')
               )
             }
           >
