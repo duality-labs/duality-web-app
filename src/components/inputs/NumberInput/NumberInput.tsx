f8be6d1013da3ef54eab5a9fac1baf74166b4546
David Uhlmann <dib542@gmail.com>
2023-01-09 14:39:43 +1000

    Add santized appendString to expected NumberInput pattern

--- src/components/inputs/NumberInput/NumberInput.tsx
+++ src/components/inputs/NumberInput/NumberInput.tsx
@@ -29,6 +29,11 @@ const parseValue = (value: string, appendString = '') => {
     : value;
 };
 
+function sanitizeInputRegex(string: string) {
+  // see https://github.com/sindresorhus/escape-string-regexp/blob/ba9a4473850cb367936417e97f1f2191b7cc67dd/index.js#L8-L10
+  return string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+}
+
 export default function NumberInput({
   className,
   placeholder = '0',
@@ -63,7 +68,7 @@ export default function NumberInput({
       className={['number-input', className].filter(Boolean).join(' ')}
       type="text"
       placeholder={placeholder}
-      pattern="^[0-9]*[.,]?[0-9]*$"
+      pattern={`^[0-9]*[.,]?[0-9]*${sanitizeInputRegex(appendString)}$`}
       value={`${value}${appendString}`}
       // global HTML element attribute options
       inputMode="decimal"
