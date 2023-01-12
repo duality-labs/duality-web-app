8e2b9f9c90f0d7e952893322c2e9b1838a564939
David Uhlmann <dib542@gmail.com>
2023-01-13 05:52:51 +1000

    Fix TS file generation from backend: ensure directory is clear first

--- scripts/update-generated-files.sh
+++ scripts/update-generated-files.sh
@@ -3,6 +3,9 @@
 # use provided source path or a default
 DUALITY_CORE_DIRECTORY="${1:-"../duality"}"
 
+# remove previous files
+rm -r "src/lib/web3/generated/ts-client"
+
 # copy module files
 cp -r "$DUALITY_CORE_DIRECTORY/ts-client" "src/lib/web3/generated"
 
