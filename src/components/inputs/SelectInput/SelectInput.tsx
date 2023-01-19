2554b85fcde8a145d78abdfa902cafd8bc63a5f0
David Uhlmann <dib542@gmail.com>
2023-01-20 01:20:13 +1000

    Allow customizable Components with defaults in SelectInput

--- src/components/inputs/SelectInput/SelectInput.tsx
+++ src/components/inputs/SelectInput/SelectInput.tsx
@@ -2,17 +2,69 @@ import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useCallback, useState } from 'react';
 import RadioInput from '../../RadioInput';
-import { RadioInputProps } from '../../RadioInput/RadioInput';
+import { OptionProps, RadioInputProps } from '../../RadioInput/RadioInput';
 
 import './SelectInput.scss';
 
+type GetString<T> = (item: T | undefined) => string;
+interface SelectedComponentProps<T> {
+  value: T | undefined;
+  getSelectedText: GetString<T>;
+}
+function DefaultSelectedComponent<T>({
+  value,
+  getSelectedText,
+}: SelectedComponentProps<T>) {
+  return <>{getSelectedText(value)}</>;
+}
+
+interface SelectOptionsProps<T> extends OptionProps<T> {
+  getLabel: GetString<T>;
+  getDescription: GetString<T>;
+}
+
+function DefaultOptionComponent<T>({
+  option,
+  getLabel,
+  getDescription,
+}: SelectOptionsProps<T>) {
+  const label = getLabel(option) ?? '';
+  const description = getDescription(option) ?? '';
+  return (
+    <>
+      <div className="label">{label}</div>
+      <div
+        className={['description', !description && 'hide', 'ml-auto']
+          .filter(Boolean)
+          .join(' ')}
+      >
+        {description}
+      </div>
+    </>
+  );
+}
+
 interface SelectInputProps<T> extends RadioInputProps<T> {
-  SelectedComponent: React.ComponentType<{ value: T | undefined }>;
+  SelectedComponent?: React.ComponentType<SelectedComponentProps<T>>;
+  getSelectedText?: GetString<T>;
+  getLabel?: GetString<T>;
+  getDescription?: GetString<T>;
+}
+
+function defaultGetLabelText<T>(item: T) {
+  return (item as unknown as { label: string })?.['label'] || '';
+}
+
+function defaultGetDescriptionText<T>(item: T) {
+  return (item as unknown as { description: string })?.['description'] || '';
 }
 
 export default function SelectInput<T>({
   className,
-  SelectedComponent,
+  SelectedComponent = DefaultSelectedComponent,
+  getLabel = defaultGetLabelText,
+  getDescription = defaultGetDescriptionText,
+  getSelectedText = getLabel,
   list,
   value,
   ...radioInputProps
@@ -27,7 +79,10 @@ export default function SelectInput<T>({
     <div className="select-input">
       <div className="select-input-selection row">
         <div className="col">
-          <SelectedComponent value={selectedItem} />
+          <SelectedComponent
+            value={selectedItem}
+            getSelectedText={getSelectedText}
+          />
         </div>
         <div className="col ml-auto flex-centered">
           <button type="button" onClick={toggleExpand}>
@@ -42,6 +97,17 @@ export default function SelectInput<T>({
           className="select-input-group"
           OptionContainerComponent={({ children }) => (
             <div className="select-input-option">{children}</div>
+          )}
+          // set default OptionComponent to use getters
+          OptionComponent={useCallback(
+            (optionComponentProps: OptionProps<T>) => (
+              <DefaultOptionComponent
+                {...optionComponentProps}
+                getLabel={getLabel}
+                getDescription={getDescription}
+              />
+            ),
+            [getLabel, getDescription]
           )}
           onClick={() => setExpanded(false)}
           // allow overwriting with custom components
