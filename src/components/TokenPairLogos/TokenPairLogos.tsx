3330d84252b6c47cfa26fdf83d9d8369e37fc760
David Uhlmann <dib542@gmail.com>
2023-01-21 20:55:52 +1000

    Allow classNames to be set in TokenPairLogos

--- src/components/TokenPairLogos/TokenPairLogos.tsx
+++ src/components/TokenPairLogos/TokenPairLogos.tsx
@@ -2,10 +2,16 @@ import { Token } from '../TokenPicker/hooks';
 
 import './TokenPairLogos.scss';
 
-function TokenLogo({ token: { symbol, logo_URIs } }: { token: Token }) {
+function TokenLogo({
+  className,
+  token: { symbol, logo_URIs },
+}: {
+  className?: string;
+  token: Token;
+}) {
   return (
     <img
-      className="token-pair-logo"
+      className={['token-pair-logo', className].filter(Boolean).join(' ')}
       alt={`${symbol} logo`}
       src={logo_URIs?.svg ?? logo_URIs?.png}
     />
@@ -13,14 +19,16 @@ function TokenLogo({ token: { symbol, logo_URIs } }: { token: Token }) {
 }
 
 export default function TokenPairLogos({
+  className,
   tokenA,
   tokenB,
 }: {
+  className?: string;
   tokenA: Token;
   tokenB: Token;
 }) {
   return (
-    <div className="token-pair-logos">
+    <div className={['token-pair-logos', className].filter(Boolean).join(' ')}>
       <TokenLogo token={tokenA} />
       <TokenLogo token={tokenB} />
     </div>
