11653bcc575c3ec18686270b6130af924dd9b821
David Uhlmann <dib542@gmail.com>
2023-01-25 01:28:58 +1000

    Remove IE polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
-// Import polyfills for IE11
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
