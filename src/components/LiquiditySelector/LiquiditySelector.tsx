cb15d5a28977bb28194a4d3cf8b919b11496cdad
David Uhlmann <dib542@gmail.com>
2023-02-13 17:30:46 +0700

    Add correction for increased text padding filter effect

--- src/components/LiquiditySelector/LiquiditySelector.tsx
+++ src/components/LiquiditySelector/LiquiditySelector.tsx
@@ -811,7 +811,7 @@ function TicksArea({
         {currentPrice && (
           <text
             filter="url(#text-solid-highlight)"
-            x={(4 + plotX(startTickPrice)).toFixed(3)}
+            x={(2 + 3.5 + plotX(startTickPrice)).toFixed(3)}
             y={6 - containerHeight}
             dy="12"
             textAnchor="end"
@@ -936,7 +936,7 @@ function TicksArea({
         {currentPrice && (
           <text
             filter="url(#text-solid-highlight)"
-            x={(-4 + plotX(endTickPrice)).toFixed(3)}
+            x={(-(2 + 3.5) + plotX(endTickPrice)).toFixed(3)}
             y={6 - containerHeight}
             dy="12"
             textAnchor="start"
