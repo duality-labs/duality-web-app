147e7e8fa8a980bbaaf1d5909d71b84e22e686c1
David Uhlmann <dib542@gmail.com>
2023-03-02 22:33:43 +0700

    refactor: abstract out useShareValueMap hook and related types

--- src/pages/MyLiquidity/MyLiquidity.tsx
+++ src/pages/MyLiquidity/MyLiquidity.tsx
@@ -3,20 +3,17 @@ import { Link } from 'react-router-dom';
 import BigNumber from 'bignumber.js';
 import { Coin } from '@cosmjs/launchpad';
 
-import {
-  useBankBalances,
-  useIndexerData,
-  useShares,
-  TickInfo,
-  hasInvertedOrder,
-} from '../../lib/web3/indexerProvider';
+import { useBankBalances } from '../../lib/web3/indexerProvider';
 import { useWeb3 } from '../../lib/web3/useWeb3';
-import { feeTypes } from '../../lib/web3/utils/fees';
 import { useSimplePrice } from '../../lib/tokenPrices';
+import useShareValueMap, {
+  ShareValue,
+  TickShareValue,
+  TickShareValueMap,
+} from './useShareValueMap';
 
 import {
   Token,
-  useDualityTokens,
   useFilteredTokenList,
   useTokens,
 } from '../../components/TokenPicker/hooks';
@@ -25,28 +22,10 @@ import { formatAmount } from '../../lib/utils/number';
 
 import './MyLiquidity.scss';
 import { getAmountInDenom } from '../../lib/web3/utils/tokens';
-import { calculateShares } from '../../lib/web3/utils/ticks';
-import { IndexedShare } from '../../lib/web3/utils/shares';
 import { EditedTickShareValue, useEditLiquidity } from './useEditLiquidity';
 import TableCard from '../../components/cards/TableCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-
-interface ShareValue {
-  share: IndexedShare;
-  token0: Token;
-  token1: Token;
-  userReserves0?: BigNumber;
-  userReserves1?: BigNumber;
-}
-interface TickShareValue extends ShareValue {
-  feeIndex: number;
-  tick0: TickInfo;
-  tick1: TickInfo;
-}
-interface TickShareValueMap {
-  [pairID: string]: Array<TickShareValue>;
-}
 
 const keplrLogoURI =
   'https://raw.githubusercontent.com/chainapsis/keplr-wallet/master/docs/.vuepress/public/favicon-256.png';
@@ -54,20 +33,6 @@ const keplrLogoURI =
 function matchTokenDenom(denom: string) {
   return (token: Token) =>
     !!token.denom_units.find((unit) => unit.denom === denom);
-}
-
-// this is a function that exists in the backend
-// but is not easily queried from here
-// perhaps the backend could return these values on each Share object
-export function getVirtualTickIndexes(
-  tickIndex: number | string | undefined,
-  feeIndex: number | string | undefined
-): [number, number] | [] {
-  const feePoints = feeTypes[Number(feeIndex)].fee * 10000;
-  const middleIndex = Number(tickIndex);
-  return feePoints && !isNaN(middleIndex)
-    ? [middleIndex + feePoints, middleIndex - feePoints]
-    : [];
 }
 
 type TokenCoin = Coin & {
@@ -79,103 +44,9 @@ export default function MyLiquidity() {
   const { wallet } = useWeb3();
   const { data: balances } = useBankBalances();
 
-  const { data: indexer } = useIndexerData();
-  const { data: shares } = useShares();
-  const dualityTokens = useDualityTokens();
-
   const [, setSelectedTokens] = useState<[Token, Token]>();
 
-  const shareValueMap = useMemo(() => {
-    if (shares && indexer) {
-      return shares.reduce<TickShareValueMap>((result, share) => {
-        const { pairId = '', tickIndex, feeIndex, sharesOwned } = share;
-        // skip this share object if there are no shares owned
-        if (feeIndex === undefined || !(Number(sharesOwned) > 0)) return result;
-        const [tokenA, tokenB] = dualityTokens;
-        const fee = feeTypes[Number(feeIndex)].fee;
-        if (
-          tokenA &&
-          tokenA.address &&
-          tokenB &&
-          tokenB.address &&
-          fee !== undefined
-        ) {
-          const inverted = hasInvertedOrder(
-            pairId,
-            tokenA.address,
-            tokenB.address
-          );
-          const [token0, token1] = inverted
-            ? [tokenB, tokenA]
-            : [tokenA, tokenB];
-          const extendedShare: ShareValue = { share, token0, token1 };
-          const ticks = indexer[pairId]?.ticks || [];
-          const [tickIndex1, tickIndex0] = getVirtualTickIndexes(
-            tickIndex,
-            feeIndex
-          );
-          if (tickIndex0 === undefined || tickIndex1 === undefined) {
-            return result;
-          }
-          const tick0 = ticks.find(
-            (tick) =>
-              tick.feeIndex.isEqualTo(feeIndex) &&
-              tick.tickIndex.isEqualTo(tickIndex0)
-          );
-          const tick1 = ticks.find(
-            (tick) =>
-              tick.feeIndex.isEqualTo(feeIndex) &&
-              tick.tickIndex.isEqualTo(tickIndex1)
-          );
-          const totalShares =
-            tick0 && tick1
-              ? calculateShares({
-                  price: tick0.price,
-                  reserve0: tick0.reserve0,
-                }).plus(
-                  calculateShares({
-                    price: tick1.price,
-                    reserve1: tick1.reserve1,
-                  })
-                )
-              : new BigNumber(0);
-          // add optional tick data from indexer
-          if (tick0 && tick1 && totalShares) {
-            const shareFraction = new BigNumber(sharesOwned ?? 0).dividedBy(
-              totalShares
-            );
-            extendedShare.userReserves0 = shareFraction.multipliedBy(
-              // convert to big tokens
-              getAmountInDenom(
-                tick0.token0,
-                tick0.reserve0,
-                tick0.token0.address,
-                tick0.token0.display
-              ) || '0'
-            );
-            extendedShare.userReserves1 = shareFraction.multipliedBy(
-              // convert to big tokens
-              getAmountInDenom(
-                tick1.token1,
-                tick1.reserve1,
-                tick1.token1.address,
-                tick1.token1.display
-              ) || '0'
-            );
-            // add TickShareValue to TickShareValueMap
-            result[pairId] = result[pairId] || [];
-            result[pairId].push({
-              ...extendedShare,
-              tick0,
-              tick1,
-              feeIndex: Number(feeIndex),
-            });
-          }
-        }
-        return result;
-      }, {});
-    }
-  }, [shares, indexer, dualityTokens]);
+  const shareValueMap = useShareValueMap();
 
   // show connect page
   if (!wallet) {
