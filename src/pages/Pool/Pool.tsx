b7e353b19deebe9a16f59e1381bb4460a372faeb
David Uhlmann <dib542@gmail.com>
2023-03-21 02:53:27 +1000

    style: improve wrapping behavior for screen sizes for Liquidity page:
        - chart is forced to a smaller size than expected at 992px width
        - layout should flow on breakpoint, not a little before or after

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -743,7 +743,7 @@ function Pool() {
             <div className="col flex-centered ml-auto">Transaction Details</div>
           </div>
           <hr className="mt-3 mb-4" />
-          <div className="flex row flow-wrap flow-nowrap-lg">
+          <div className="flex col row-lg gapx-lg">
             <div className="flex col col--left">
               <div className="chart-header row my-4">
                 <TokenPairLogos
@@ -826,16 +826,18 @@ function Pool() {
                   />
                 </div>
               </div>
-              <input
-                className="button-primary text-medium mt-4 p-3"
-                type="submit"
-                disabled={
-                  (isValueAZero && isValueBZero) ||
-                  !hasSufficientFundsA ||
-                  !hasSufficientFundsB
-                }
-                value="Confirm"
-              />
+              <div className="col-lg">
+                <input
+                  className="button-primary text-medium mt-4 p-3"
+                  type="submit"
+                  disabled={
+                    (isValueAZero && isValueBZero) ||
+                    !hasSufficientFundsA ||
+                    !hasSufficientFundsB
+                  }
+                  value="Confirm"
+                />
+              </div>
             </div>
             <div className="flex col col--right">
               <div className="chart-header row flow-wrap my-4">
@@ -1018,6 +1020,18 @@ function Pool() {
                   </div>
                 </div>
               )}
+            </div>
+            <div className="col pt-lg col-lg-hide">
+              <input
+                className="button-primary text-medium mt-4 p-3"
+                type="submit"
+                disabled={
+                  (isValueAZero && isValueBZero) ||
+                  !hasSufficientFundsA ||
+                  !hasSufficientFundsB
+                }
+                value="Confirm"
+              />
             </div>
           </div>
         </div>
