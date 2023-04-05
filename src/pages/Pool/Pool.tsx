78cc1b4d64e8ce75366adce128a756c562cfba32
David Uhlmann <dib542@gmail.com>
2023-04-06 07:29:18 +1000

    fix: improve chart layout flow on Liquidity page (#339)
    * feat: improve wrapping behavior for screen sizes for Liquidity page:
    
        - chart is forced to a smaller size than expected at 992px width
        - layout should flow on breakpoint, not a little before or after
    
    * refactor: abstract out confirm button so it will always be the same:
    
        - across both screen sizes

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -647,6 +647,19 @@ function Pool() {
     'all'
   );
 
+  const confirmButton = (
+    <input
+      className="button-primary text-medium mt-4 p-3"
+      type="submit"
+      disabled={
+        (isValueAZero && isValueBZero) ||
+        !hasSufficientFundsA ||
+        !hasSufficientFundsB
+      }
+      value="Confirm"
+    />
+  );
+
   if (!tokenA || !tokenB || !valuesConfirmed) {
     return (
       <form
@@ -751,7 +764,7 @@ function Pool() {
             <div className="col flex-centered ml-auto">Transaction Details</div>
           </div>
           <hr className="mt-3 mb-4" />
-          <div className="flex row flow-wrap flow-nowrap-lg">
+          <div className="flex col row-lg gapx-lg">
             <div className="flex col col--left">
               <div className="chart-header row my-4">
                 <TokenPairLogos
@@ -834,16 +847,7 @@ function Pool() {
                   />
                 </div>
               </div>
-              <input
-                className="button-primary text-medium mt-4 p-3"
-                type="submit"
-                disabled={
-                  (isValueAZero && isValueBZero) ||
-                  !hasSufficientFundsA ||
-                  !hasSufficientFundsB
-                }
-                value="Confirm"
-              />
+              <div className="col-lg">{confirmButton}</div>
             </div>
             <div className="flex col col--right">
               <div className="chart-header row flow-wrap my-4">
@@ -1028,6 +1032,7 @@ function Pool() {
                 </div>
               )}
             </div>
+            <div className="col pt-lg col-lg-hide">{confirmButton}</div>
           </div>
         </div>
       </div>
