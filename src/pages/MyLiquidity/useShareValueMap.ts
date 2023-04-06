da37f5b7eeae3529bf0d7ded4e0297b7128f9875
David Uhlmann <dib542@gmail.com>
2023-04-06 17:42:46 +1000

    fix: upgrade api to backend v0.1.3 (part 1) (#345)
    * refactor: remove unused extra translation of feeIndex to number type
    
    * fix: ticks on both sides are no longer guaranteed to be defined:
    
        - adapt to possibly undefined ticks on each side of the token pair
    
    * refactor: split out share value types more consistently:
    
        - the tick and reserves values are found together
    
    * refactor: remove unused ticks from TickShareValue
    
    * refactor: combine common types to be only defined once
    
    * refactor: make TickShareValue type stricter:
    
        - at creation time we have enough context to fill all properties
    
    * docs: add notes around why we use indexed data to calculate reserves
    
    * docs: add note of how future usage of indexed data could be reduced

--- src/pages/MyLiquidity/useShareValueMap.ts
+++ src/pages/MyLiquidity/useShareValueMap.ts
@@ -4,7 +4,6 @@ import BigNumber from 'bignumber.js';
 import {
   useIndexerData,
   useShares,
-  TickInfo,
   hasInvertedOrder,
 } from '../../lib/web3/indexerProvider';
 import { feeTypes } from '../../lib/web3/utils/fees';
@@ -19,14 +18,10 @@ export interface ShareValue {
   share: IndexedShare;
   token0: Token;
   token1: Token;
-  userReserves0?: BigNumber;
-  userReserves1?: BigNumber;
 }
 export interface TickShareValue extends ShareValue {
-  // todo: take from useShareValueMap??
-  feeIndex: number;
-  tick0: TickInfo;
-  tick1: TickInfo;
+  userReserves0: BigNumber;
+  userReserves1: BigNumber;
 }
 export interface TickShareValueMap {
   [pairID: string]: Array<TickShareValue>;
@@ -74,7 +69,7 @@ export default function useShareValueMap() {
           const [token0, token1] = inverted
             ? [tokenB, tokenA]
             : [tokenA, tokenB];
-          const extendedShare: ShareValue = { share, token0, token1 };
+          const shareValue: ShareValue = { share, token0, token1 };
           const [tickIndex1, tickIndex0] = getVirtualTickIndexes(
             tickIndex,
             feeIndex
@@ -82,6 +77,15 @@ export default function useShareValueMap() {
           if (tickIndex0 === undefined || tickIndex1 === undefined) {
             return result;
           }
+          // the reason that we fetch the reserve0 and reserve1 context
+          // from the indexed ticks is because these reserves indicated
+          // which tokens the user's shares currently represent.
+          // eg. user has 100 shares: is that currently in token0 or token1?
+          //     if indexed data has 0/2000000 token0/token1 reserves
+          //     then the user's share is best represented in token1 values
+          // todo: this may be better optimized as an estimation using
+          //       only the first page of results of each token in a pair
+          //       when a token pair has many (constantly updating) tick pages
           const tick0 = (indexer[pairId]?.token0Ticks || []).find(
             (tick) =>
               tick.feeIndex.isEqualTo(feeIndex) &&
@@ -92,49 +96,56 @@ export default function useShareValueMap() {
               tick.feeIndex.isEqualTo(feeIndex) &&
               tick.tickIndex.isEqualTo(tickIndex1)
           );
-          const totalShares =
-            tick0 && tick1
-              ? calculateShares({
-                  price: tick0.price,
-                  reserve0: tick0.reserve0,
-                }).plus(
-                  calculateShares({
-                    price: tick1.price,
-                    reserve1: tick1.reserve1,
-                  })
-                )
-              : new BigNumber(0);
+          const tick0Shares =
+            tick0 &&
+            calculateShares({
+              price: tick0.price,
+              reserve0: tick0.reserve0,
+            });
+          const tick1Shares =
+            tick1 &&
+            calculateShares({
+              price: tick1.price,
+              reserve1: tick1.reserve1,
+            });
+          // total shares if found
+          const totalShares = new BigNumber(0)
+            .plus(tick0Shares || 0)
+            .plus(tick1Shares || 0);
+
           // add optional tick data from indexer
-          if (tick0 && tick1 && totalShares) {
+          if (totalShares.isGreaterThan(0)) {
             const shareFraction = new BigNumber(sharesOwned ?? 0).dividedBy(
               totalShares
             );
-            extendedShare.userReserves0 = shareFraction.multipliedBy(
-              // convert to big tokens
-              getAmountInDenom(
-                tick0.token0,
-                tick0.reserve0,
-                tick0.token0.address,
-                tick0.token0.display
-              ) || '0'
-            );
-            extendedShare.userReserves1 = shareFraction.multipliedBy(
-              // convert to big tokens
-              getAmountInDenom(
-                tick1.token1,
-                tick1.reserve1,
-                tick1.token1.address,
-                tick1.token1.display
-              ) || '0'
-            );
+            const extendedShare: TickShareValue = {
+              ...shareValue,
+              userReserves0: tick0
+                ? shareFraction.multipliedBy(
+                    // convert to big tokens
+                    getAmountInDenom(
+                      tick0.token0,
+                      tick0.reserve0,
+                      tick0.token0.address,
+                      tick0.token0.display
+                    ) || '0'
+                  )
+                : new BigNumber(0),
+              userReserves1: tick1
+                ? shareFraction.multipliedBy(
+                    // convert to big tokens
+                    getAmountInDenom(
+                      tick1.token1,
+                      tick1.reserve1,
+                      tick1.token1.address,
+                      tick1.token1.display
+                    ) || '0'
+                  )
+                : new BigNumber(0),
+            };
             // add TickShareValue to TickShareValueMap
             result[pairId] = result[pairId] || [];
-            result[pairId].push({
-              ...extendedShare,
-              tick0,
-              tick1,
-              feeIndex: Number(feeIndex),
-            });
+            result[pairId].push(extendedShare);
           }
         }
         return result;
