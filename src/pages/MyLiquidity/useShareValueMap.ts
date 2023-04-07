cf0206fdebbebc2abb24ee0c1c5b5c80f3e9fd23
David Uhlmann <dib542@gmail.com>
2023-04-08 05:34:06 +1000

    feat: add new Edit Liquidity card

--- src/pages/MyLiquidity/useShareValueMap.ts
+++ src/pages/MyLiquidity/useShareValueMap.ts
@@ -17,6 +17,8 @@ export interface ShareValue {
   token1: Token;
 }
 export interface TickShareValue extends ShareValue {
+  tickIndex0: number;
+  tickIndex1: number;
   userReserves0: BigNumber;
   userReserves1: BigNumber;
 }
@@ -151,6 +153,8 @@ export function useTickShareValues(
           );
           return {
             ...shareValue,
+            tickIndex0,
+            tickIndex1,
             userReserves0: tick0
               ? shareFraction.multipliedBy(
                   // convert to big tokens
