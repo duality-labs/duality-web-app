898bb0e84c7bf2f46c283fc0fb04bd35be6ed73a
David Uhlmann <dib542@gmail.com>
2023-04-08 01:33:20 +1000

    feat: allow advanced page to have consistent vertical position:
        - previously the number of ticks could alter the graph position
          if the table got larger and was vertically centered to the page

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -100,8 +100,8 @@ const restrictPriceRangeValues = (
 
 export default function PoolPage() {
   return (
-    <div className="container">
-      <div className="page">
+    <div className="container row flex py-5">
+      <div className="page col flex">
         <Pool />
       </div>
     </div>
@@ -737,7 +737,7 @@ function Pool() {
     return (
       <form
         className={[
-          'pool-page row flex-centered flow-wrap gap-5',
+          'pool-page m-auto row flex-centered flow-wrap gap-5',
           isValidatingDeposit && 'disabled',
         ]
           .filter(Boolean)
