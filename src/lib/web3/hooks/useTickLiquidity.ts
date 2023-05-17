657697a653625375979054847ddf42a3655857b4
David Uhlmann <dib542@gmail.com>
2023-05-18 07:22:44 +1000

    fix: fetch tickLiquidity using cached SWR hook only:
        - requires pairID and token address to be known before fetching
        - cannot call arbitrarily long list of pairs due to SWR hook usage
          so the aggregated user's value total will need to be redone

--- src/lib/web3/hooks/useTickLiquidity.ts
+++ src/lib/web3/hooks/useTickLiquidity.ts
@@ -7,13 +7,20 @@ import {
   QueryAllTickLiquidityResponseSDKType,
 } from '@duality-labs/dualityjs/types/codegen/duality/dex/query';
 import { useLcdClientPromise } from '../lcdClient';
+import { TickLiquiditySDKType } from '@duality-labs/dualityjs/types/codegen/duality/dex/tick_liquidity';
 
 import { defaultPaginationParams, getNextPaginationKey } from './utils';
+import { feeTypes } from '../utils/fees';
 
-type QueryAllTickLiquidityList =
-  QueryAllTickLiquidityResponseSDKType['tickLiquidity'];
+import { addressableTokenMap as tokenMap } from '../../../components/TokenPicker/hooks';
+import BigNumber from 'bignumber.js';
+import { TickInfo, tickIndexToPrice } from '../utils/ticks';
+import { useOrderedTokenPair } from './useTokenPairs';
+import { TokenAddress } from '../utils/tokens';
+import { getPairID } from '../utils/pairs';
+
 type QueryAllTickLiquidityState = {
-  data: QueryAllTickLiquidityList | undefined;
+  data: Array<TickInfo> | undefined;
   isValidating: SWRResponse['isValidating'];
   error: SWRResponse['error'];
 };
@@ -24,25 +31,31 @@ export default function useTickLiquidity({
   queryClient: queryClientConfig,
 }: {
   swr?: SWRConfiguration;
-  query: QueryAllTickLiquidityRequest;
+  query: QueryAllTickLiquidityRequest | null;
   queryClient?: string;
 }): QueryAllTickLiquidityState {
-  if (!queryConfig?.pairId) {
+  if (queryConfig && !queryConfig?.pairId) {
     throw new Error('Cannot fetch liquidity: no pair ID given');
   }
-  if (!queryConfig?.tokenIn) {
+  if (queryConfig && !queryConfig?.tokenIn) {
     throw new Error('Cannot fetch liquidity: no token ID given');
   }
 
-  const params: QueryAllTickLiquidityRequest = {
-    ...queryConfig,
-    pagination: {
-      ...defaultPaginationParams,
-      ...queryConfig?.pagination,
-    },
-  };
+  const params: QueryAllTickLiquidityRequest | null = !queryConfig
+    ? null
+    : {
+        ...queryConfig,
+        pagination: {
+          ...defaultPaginationParams,
+          ...queryConfig?.pagination,
+        },
+      };
 
   const lcdClientPromise = useLcdClientPromise(queryClientConfig);
+
+  // todo: add a subscription listener here or above here to invalidate the
+  // following request cache keys a refetch new data (recommend passing block ID
+  // into the cache key to only refetch at most once per block)
 
   const {
     data: pages,
@@ -51,15 +64,22 @@ export default function useTickLiquidity({
     size,
     setSize,
   } = useSWRInfinite<QueryAllTickLiquidityResponseSDKType>(
-    getNextPaginationKey<QueryAllTickLiquidityRequest>(
-      // set unique cache key for this client method
-      'dualitylabs.duality.dex.tickLiquidityAll',
-      params
-    ),
-    async ([, params]: [_: string, params: QueryAllTickLiquidityRequest]) => {
-      const client = await lcdClientPromise;
-      return await client.dualitylabs.duality.dex.tickLiquidityAll(params);
-    },
+    !params
+      ? () => ''
+      : getNextPaginationKey<QueryAllTickLiquidityRequest>(
+          // set unique cache key for this client method
+          'dualitylabs.duality.dex.tickLiquidityAll',
+          params
+        ),
+    !params
+      ? null
+      : async ([, params]: [
+          _: string,
+          params: QueryAllTickLiquidityRequest
+        ]) => {
+          const client = await lcdClientPromise;
+          return await client.dualitylabs.duality.dex.tickLiquidityAll(params);
+        },
     { persistSize: true, ...swrConfig }
   );
   // set number of pages to latest total
@@ -77,4 +97,86 @@ export default function useTickLiquidity({
     return liquidity && transformData(liquidity);
   }, [pages]);
   return { data: tradingPairs, isValidating, error };
+}
+
+function transformData(ticks: Array<TickLiquiditySDKType>): Array<TickInfo> {
+  return ticks.map<TickInfo>(function ({
+    poolReserves: {
+      pairId: { token0 = '', token1 = '' } = {},
+      tokenIn,
+      tickIndex: tickIndexString,
+      fee: feeString,
+      reserves: reservesString,
+    } = {},
+  }) {
+    const tickIndex = Number(tickIndexString);
+    const fee = Number(feeString) / 10000 || 0;
+    const feeIndex = feeTypes.findIndex((feeType) => feeType.fee === fee);
+    if (
+      !isNaN(tickIndex) &&
+      tokenIn &&
+      token0 &&
+      token1 &&
+      tokenMap[tokenIn] &&
+      tokenMap[token0] &&
+      tokenMap[token1] &&
+      !isNaN(Number(reservesString)) &&
+      feeIndex >= 0
+    ) {
+      // calculate price from tickIndex, try to keep price values consistent:
+      //   JS rounding may be inconsistent with API's rounding
+      const bigTickIndex = new BigNumber(tickIndex || 0);
+      const bigPrice = tickIndexToPrice(bigTickIndex);
+
+      if (tokenIn === token0) {
+        return {
+          token0: tokenMap[token0],
+          token1: tokenMap[token1],
+          tickIndex: bigTickIndex,
+          price: bigPrice,
+          feeIndex: new BigNumber(feeIndex),
+          fee: new BigNumber(fee),
+          reserve0: new BigNumber(reservesString || 0),
+          reserve1: new BigNumber(0),
+        };
+      } else if (tokenIn === token1) {
+        return {
+          token0: tokenMap[token0],
+          token1: tokenMap[token1],
+          tickIndex: bigTickIndex,
+          price: bigPrice,
+          feeIndex: new BigNumber(feeIndex),
+          fee: new BigNumber(fee),
+          reserve0: new BigNumber(0),
+          reserve1: new BigNumber(reservesString || 0),
+        };
+      }
+    }
+    throw new Error('Unexpected tickLiquidity shape');
+  });
+}
+
+// add convenience method to fetch ticks in a pair
+export function useTokenPairTickLiquidity([tokenA, tokenB]: [
+  TokenAddress?,
+  TokenAddress?
+]): {
+  data: [TickInfo[] | undefined, TickInfo[] | undefined];
+  isValidating: boolean;
+  error: unknown;
+} {
+  const [token0, token1] = useOrderedTokenPair([tokenA, tokenB]) || [];
+  const pairId = token0 && token1 ? getPairID(token0, token1) : null;
+  const token0TicksState = useTickLiquidity({
+    query: pairId && token0 ? { pairId, tokenIn: token0 } : null,
+  });
+  const token1TicksState = useTickLiquidity({
+    query: pairId && token1 ? { pairId, tokenIn: token1 } : null,
+  });
+  return {
+    data: [token0TicksState.data, token1TicksState.data],
+    isValidating:
+      token0TicksState.isValidating && token1TicksState.isValidating,
+    error: token0TicksState.error || token1TicksState.error,
+  };
 }
