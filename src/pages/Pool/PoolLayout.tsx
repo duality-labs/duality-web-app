b6053d57fe81c2c0830c427cb31cd2b8ca53c095
David Uhlmann <dib542@gmail.com>
2023-05-27 13:06:26 +1000

    refactor: simplify Pool page layout and rename CSS classes for clarity

--- src/pages/Pool/PoolLayout.tsx
+++ src/pages/Pool/PoolLayout.tsx
@@ -22,50 +22,38 @@ export default function PoolLayout({
   children?: ReactNode;
 }) {
   return (
-    <div className={[!!disabled && 'disabled'].filter(Boolean).join(' ')}>
-      <div className="pool-page">
-        <div className="row gap-4">
-          <div className="col flex gap-4">
-            <div className="chart-card col">
-              <div className="chart-breadcrumbs row flow-wrap gap-3 mb-5">
-                <Link className="text-light-alt" to="/pools">
-                  Pools
-                </Link>
-                {'>'}
-                <span>
-                  {tokenA.symbol}/{tokenB.symbol}
-                </span>
-              </div>
-              <div className="chart-header row flow-wrap">
-                <div className="col">
-                  <div className="chart-header row my-4">
-                    <TokenPairLogos
-                      className="h3"
-                      tokenA={tokenA}
-                      tokenB={tokenB}
-                    />
-                    <h2 className="h3">
-                      {tokenA.symbol} {tokenB.symbol} Pool
-                    </h2>
-                    <button
-                      type="button"
-                      className="ml-auto icon-button"
-                      onClick={swap}
-                    >
-                      <FontAwesomeIcon
-                        icon={faArrowRightArrowLeft}
-                      ></FontAwesomeIcon>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <hr className="mt-3 mb-4" />
+    <div className="pool-page">
+      <div className="col">
+        <div className="pool-page__breadcrumbs row flow-wrap gap-3 mb-5">
+          <Link className="text-light-alt" to="/pools">
+            Pools
+          </Link>
+          {'>'}
+          <span>
+            {tokenA.symbol}/{tokenB.symbol}
+          </span>
+        </div>
+        <div className="row flow-wrap">
+          <div className="col">
+            <div className="pool-page__header row my-4">
+              <TokenPairLogos className="h3" tokenA={tokenA} tokenB={tokenB} />
+              <h2 className="h3">
+                {tokenA.symbol} {tokenB.symbol} Pool
+              </h2>
+              <button
+                type="button"
+                className="ml-auto icon-button"
+                disabled={disabled}
+                onClick={swap}
+              >
+                <FontAwesomeIcon icon={faArrowRightArrowLeft}></FontAwesomeIcon>
+              </button>
             </div>
           </div>
         </div>
-        {children}
+        <hr className="mt-3 mb-4" />
       </div>
-      <div className="spacer"></div>
+      {children}
     </div>
   );
 }
