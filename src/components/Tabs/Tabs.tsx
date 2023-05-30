1829087801cca91336665300593917d0c09516f1
David Uhlmann <dib542@gmail.com>
2023-05-30 14:04:23 +1000

    fix: Tabs don't need a key anymore if index is used

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,7 +1,6 @@
 import { ReactNode, useState } from 'react';
 
 interface Tab {
-  key: string;
   nav: ReactNode;
   Tab: React.FunctionComponent;
 }
@@ -12,24 +11,23 @@ export default function Tabs({
   tabs: Array<Tab>;
   className?: string;
 }) {
-  const [{ key, Tab }, setTab] = useState<Tab>(tabs[0]);
+  const [tabIndex, setTabIndex] = useState<number>(0);
+  const { Tab } = tabs[tabIndex];
 
   return (
     <div className={['tabs', className].filter(Boolean).join(' ')}>
       <div className="tabs__nav row">
-        <div className="col">
-          {tabs.map((tab) => {
-            return (
-              <button
-                key={tab.key}
-                className={tab.key === key ? 'active' : ''}
-                onClick={() => setTab(tab)}
-              >
-                {tab.nav}
-              </button>
-            );
-          })}
-        </div>
+        {tabs.map((tab, index) => {
+          return (
+            <button
+              key={index}
+              className={index === tabIndex ? 'active' : ''}
+              onClick={() => setTabIndex(index)}
+            >
+              {tab.nav}
+            </button>
+          );
+        })}
       </div>
       <div className="tabs__tab row">
         <div className="col">
