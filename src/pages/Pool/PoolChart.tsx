fcdb73b82e262681a55593cce001e2bcc5a03f96
David Uhlmann <dib542@gmail.com>
2023-06-03 20:10:01 +1000

    feat: add TVL data fetching

--- src/pages/Pool/PoolChart.tsx
+++ src/pages/Pool/PoolChart.tsx
@@ -1,10 +1,11 @@
 import BigNumber from 'bignumber.js';
-import { useQuery } from '@tanstack/react-query';
-import { useMemo, useState } from 'react';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useEffect, useMemo, useState } from 'react';
 
 import BarChart from '../../components/charts/BarChart';
 import ButtonGroup from '../../components/ButtonGroup/ButtonGroup';
 
+import { days, hours, weeks } from '../../lib/utils/time';
 import { Token, getAmountInDenom } from '../../lib/web3/utils/tokens';
 import { formatCurrency } from '../../lib/utils/number';
 import { useSimplePrice } from '../../lib/tokenPrices';
@@ -76,7 +77,7 @@ export default function PoolChart({
       </div>
       {chartKeys[chartIndex] === 'TVL' ? (
         // plot line chart type
-        <PoolBarChart
+        <PoolLineChart
           tokenA={tokenA}
           tokenB={tokenB}
           chartKey={chartKeys[chartIndex]}
@@ -91,6 +92,156 @@ export default function PoolChart({
           timePeriodKey={timePeriodKeys[timePeriodIndex]}
         />
       )}
+    </div>
+  );
+}
+
+type TimeSeriesRow = [unixTime: number, amountA: number, amountB: number];
+type TimeSeriesPage = {
+  shape: Array<string>;
+  data: Array<TimeSeriesRow>;
+  pagination?: {
+    next_key?: string | null;
+  };
+};
+
+function PoolLineChart({
+  chartKey,
+  timePeriodKey,
+  tokenA,
+  tokenB,
+}: {
+  chartKey: typeof chartKeys[number];
+  timePeriodKey: TimePeriodKey;
+  tokenA: Token;
+  tokenB: Token;
+}) {
+  const {
+    data: [tokenAPrice, tokenBPrice],
+  } = useSimplePrice([tokenA, tokenB]);
+
+  const {
+    data: resultData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: [tokenA.address, tokenB.address],
+    queryFn: async ({ pageParam: pageKey }): Promise<TimeSeriesPage> => {
+      const requestPath = (() => {
+        const queryParams = new URLSearchParams();
+        if (pageKey) {
+          queryParams.append('pagination.key', pageKey);
+        } else {
+          // add time restriction
+          const startTimeMs = (() => {
+            const now = Date.now();
+            switch (timePeriodKey) {
+              case '24H':
+                return now - hours * 24;
+              case '1W':
+                return now - weeks * 1;
+              case '1M':
+                return now - weeks * 4;
+              case '1Y':
+                return now - days * 365;
+                return undefined;
+            }
+            return;
+          })();
+          if (startTimeMs) {
+            queryParams.append(
+              'pagination.after',
+              (startTimeMs / 1000).toFixed(0)
+            );
+          }
+        }
+        const query = queryParams ? `?${queryParams}` : '';
+        switch (chartKey) {
+          case 'TVL':
+            return `/timeseries/tvl/${tokenA.address}/${tokenB.address}${query}`;
+          default:
+            return `/timeseries/tvl/${tokenA.address}/${tokenB.address}`;
+        }
+      })();
+      const response = await fetch(`${REACT_APP__INDEXER_API}${requestPath}`);
+      return await response.json();
+    },
+    defaultPageParam: '',
+    getNextPageParam: (lastPage: TimeSeriesPage) => {
+      const nextKey = lastPage?.pagination?.next_key;
+      return nextKey ? nextKey : undefined;
+    },
+  });
+
+  // fetch more data if data has changed but there are still more pages to get
+  useEffect(() => {
+    if (fetchNextPage && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, resultData, hasNextPage]);
+
+  // collect pages together
+  const data = useMemo<TimeSeriesRow[] | undefined>(() => {
+    return resultData?.pages?.flatMap((page) => page.data);
+  }, [resultData?.pages]);
+
+  // map data to chart
+  const chartData = useMemo(() => {
+    return data
+      ?.map(([unixTime, amountA, amountB]) => {
+        // add values here properly with price data
+        return { x: unixTime.toFixed(), y: Number(amountA) + Number(amountB) };
+      })
+      .reverse();
+  }, [data]);
+
+  const [[unixTime, lastAmountA, lastAmountB]] = data ?? [[]];
+
+  return chartData ? (
+    // plot chart
+    <div className="line-chart">
+      <div className="chart__header mt-lg">
+        {tokenAPrice &&
+        tokenBPrice &&
+        Number(lastAmountA) >= 0 &&
+        Number(lastAmountB) >= 0
+          ? formatCurrency(
+              BigNumber.sum(
+                getAmountInDenom(
+                  tokenA,
+                  Number(lastAmountA) * tokenAPrice,
+                  tokenA.address,
+                  tokenA.display
+                ) || 0,
+                getAmountInDenom(
+                  tokenB,
+                  Number(lastAmountB) * tokenBPrice,
+                  tokenB.address,
+                  tokenB.display
+                ) || 0
+              ).toFixed()
+            )
+          : '...'}
+      </div>
+      <div className="chart__subheader">
+        {unixTime ? (
+          new Date(unixTime * 1000).toLocaleDateString(undefined, {
+            dateStyle: 'long',
+          })
+        ) : (
+          <>&nbsp;</>
+        )}
+      </div>
+      <BarChart width={500} height={300} data={chartData} />
+    </div>
+  ) : (
+    // show skeleton
+    <div
+      className="chart--empty mt-6 mb-xl flex flex-centered row"
+      style={{ height: 300 }}
+    >
+      {isFetching ? 'loading...' : 'no data'}
     </div>
   );
 }
