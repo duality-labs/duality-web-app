c7f7ad6da681bb28b9af1c773089cceee3016883
David Uhlmann <dib542@gmail.com>
2023-06-07 07:11:35 +1000

    feat: make all charts bar charts for now

--- src/pages/Pool/PoolChart.tsx
+++ src/pages/Pool/PoolChart.tsx
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { useEffect, useMemo, useState } from 'react';
 
 import BarChart from '../../components/charts/BarChart';
@@ -14,29 +14,10 @@ import './PoolChart.scss';
 
 const { REACT_APP__INDEXER_API = '' } = process.env;
 
-const resolutions = ['hour', 'day', 'week'] as const;
-
 const timePeriodKeys: TimePeriodKey[] = ['24H', '1W', '1M', '1Y', 'ALL'];
 
 type TimePeriodKey = '24H' | '1W' | '1M' | '1Y' | 'ALL';
 type DataResolution = 'second' | 'minute' | 'hour' | 'day' | 'month';
-
-interface DataRow {
-  timestamp: string;
-  tokenA: number;
-  tokenB: number;
-}
-interface DataPeriod {
-  resolution: typeof resolutions[number];
-  data: Array<DataRow>;
-}
-interface DataPeriodsResponse {
-  '24H': DataPeriod;
-  '1W': DataPeriod;
-  '1M': DataPeriod;
-  '1Y': DataPeriod;
-  ALL: DataPeriod;
-}
 
 const chartKeys = ['TVL', 'Volume', 'Fees', 'Volatility'] as const;
 
@@ -78,7 +59,7 @@ export default function PoolChart({
       </div>
       {chartKeys[chartIndex] === 'TVL' ? (
         // plot line chart type
-        <PoolLineChart
+        <PoolBarChart
           tokenA={tokenA}
           tokenB={tokenB}
           chartKey={chartKeys[chartIndex]}
@@ -136,7 +117,7 @@ function getDataResolution(
   }
 }
 
-function PoolLineChart({
+function PoolBarChart({
   chartKey,
   timePeriodKey,
   tokenA,
@@ -287,127 +268,4 @@ function getLastDataValues(data: TimeSeriesRow[] = [], index = 0): number[] {
     }
   }
   return [];
-}
-
-function PoolBarChart({
-  chartKey,
-  timePeriodKey,
-  tokenA,
-  tokenB,
-}: {
-  chartKey: typeof chartKeys[number];
-  timePeriodKey: TimePeriodKey;
-  tokenA: Token;
-  tokenB: Token;
-}) {
-  const {
-    data: [tokenAPrice, tokenBPrice],
-  } = useSimplePrice([tokenA, tokenB]);
-
-  const { data, isFetching } = useQuery({
-    queryKey: [tokenA.address, tokenB.address],
-    queryFn: async (): Promise<DataPeriodsResponse> => {
-      const requestPath = (() => {
-        switch (chartKey) {
-          case 'Volume':
-            return `/timeseries/volume/${tokenA.address}/${tokenB.address}`;
-          default:
-            return `/timeseries/volume/${tokenA.address}/${tokenB.address}`;
-        }
-      })();
-      const response = await fetch(`${REACT_APP__INDEXER_API}${requestPath}`);
-      return await response.json();
-    },
-  });
-
-  const dataPeriod = useMemo<DataPeriod | null>(() => {
-    if (data && timePeriodKey) {
-      return data[timePeriodKey];
-    }
-    return null;
-  }, [data, timePeriodKey]);
-
-  // map data to chart
-  const chartData = useMemo(() => {
-    if (dataPeriod) {
-      const length = (() => {
-        switch (true) {
-          case dataPeriod.resolution === 'hour' && timePeriodKey === '24H':
-            return 24;
-          case dataPeriod.resolution === 'hour' && timePeriodKey === '1W':
-            return 24 * 7;
-          case dataPeriod.resolution === 'day' && timePeriodKey === '1M':
-            return 28;
-          case dataPeriod.resolution === 'week' && timePeriodKey === '1Y':
-            return 52;
-          case dataPeriod.resolution === 'hour':
-            return 24;
-          case dataPeriod.resolution === 'day':
-            return 28;
-          case dataPeriod.resolution === 'week':
-            return 52;
-          default:
-            return 1;
-        }
-      })();
-      return Array.from({ length })
-        .map((_, index) => {
-          const dataRow = dataPeriod.data[index];
-          // todo: find data for expected timestamps
-          if (dataRow) {
-            const { timestamp, tokenA, tokenB }: DataRow = dataRow;
-            // add values here properly with price data
-            return { x: timestamp, y: tokenA + tokenB };
-          }
-          return { x: `empty-${index}`, y: 0 };
-        })
-        .reverse();
-    }
-  }, [dataPeriod, timePeriodKey]);
-
-  const [lastChartData] = dataPeriod?.data ?? [];
-
-  return chartData ? (
-    // plot chart
-    <div className="bar-chart">
-      <div className="chart__header mt-lg">
-        {tokenAPrice && tokenBPrice && lastChartData
-          ? formatCurrency(
-              BigNumber.sum(
-                getAmountInDenom(
-                  tokenA,
-                  lastChartData.tokenA * tokenAPrice,
-                  tokenA.address,
-                  tokenA.display
-                ) || 0,
-                getAmountInDenom(
-                  tokenB,
-                  lastChartData.tokenB * tokenBPrice,
-                  tokenB.address,
-                  tokenB.display
-                ) || 0
-              ).toFixed()
-            )
-          : '...'}
-      </div>
-      <div className="chart__subheader">
-        {lastChartData ? (
-          new Date(lastChartData.timestamp).toLocaleDateString(undefined, {
-            dateStyle: 'long',
-          })
-        ) : (
-          <>&nbsp;</>
-        )}
-      </div>
-      <BarChart width={500} height={300} data={chartData} />
-    </div>
-  ) : (
-    // show skeleton
-    <div
-      className="chart--empty mt-6 mb-xl flex flex-centered row"
-      style={{ height: 300 }}
-    >
-      {isFetching ? 'loading...' : 'no data'}
-    </div>
-  );
 }
