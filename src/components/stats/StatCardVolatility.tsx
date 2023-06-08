a0ad835456b483678dd090fdd67f8bd190ae50a3
David Uhlmann <dib542@gmail.com>
2023-06-08 16:38:08 +1000

    feat: add StatCardVolatility component

---
+++ src/components/stats/StatCardVolatility.tsx
@@ -0,0 +1,34 @@
+import useSWR from 'swr';
+
+import StatCard from '../cards/StatCard';
+
+import { TimeSeriesPage, getLastDataChanges, getLastDataValues } from './utils';
+import { Token } from '../../lib/web3/utils/tokens';
+import { formatPercentage } from '../../lib/utils/number';
+
+const { REACT_APP__INDEXER_API = '' } = process.env;
+
+export default function StatCardVolatility({
+  tokenA,
+  tokenB,
+}: {
+  tokenA: Token;
+  tokenB: Token;
+}) {
+  const { data } = useSWR<TimeSeriesPage>(
+    `${REACT_APP__INDEXER_API}/stats/volatility/${tokenA?.address}/${tokenB?.address}`,
+    async (url) => {
+      const response = await fetch(url);
+      return response.json();
+    }
+  );
+
+  const [volatility] = getLastDataValues(data?.data);
+  const [volatilityDiff] = getLastDataChanges(data?.data);
+
+  return (
+    <StatCard loading={!data} header="Volatility (10d)" change={volatilityDiff}>
+      {volatility !== undefined ? formatPercentage(volatility) : undefined}
+    </StatCard>
+  );
+}
