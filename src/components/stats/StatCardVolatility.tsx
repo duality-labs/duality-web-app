519295f916036b379a9f120be09d97bd899abac6
David Uhlmann <dib542@gmail.com>
2023-06-08 17:59:16 +1000

    feat: allow dev environment to see less strict volatility calculation

--- src/components/stats/StatCardVolatility.tsx
+++ src/components/stats/StatCardVolatility.tsx
@@ -6,7 +6,7 @@ import { TimeSeriesPage, getLastDataChanges, getLastDataValues } from './utils';
 import { Token } from '../../lib/web3/utils/tokens';
 import { formatPercentage } from '../../lib/utils/number';
 
-const { REACT_APP__INDEXER_API = '' } = process.env;
+const { NODE_ENV, REACT_APP__INDEXER_API = '' } = process.env;
 
 export default function StatCardVolatility({
   tokenA,
@@ -16,7 +16,9 @@ export default function StatCardVolatility({
   tokenB: Token;
 }) {
   const { data } = useSWR<TimeSeriesPage>(
-    `${REACT_APP__INDEXER_API}/stats/volatility/${tokenA?.address}/${tokenB?.address}`,
+    `${REACT_APP__INDEXER_API}/stats/volatility/${tokenA?.address}/${
+      tokenB?.address
+    }${NODE_ENV === 'development' ? '?strict=false' : ''}`,
     async (url) => {
       const response = await fetch(url);
       return response.json();
