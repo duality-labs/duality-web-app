ec1e01f633aeb836bed26d552e1ed579e52bb9df
David Uhlmann <dib542@gmail.com>
2023-06-09 05:24:41 +1000

    feat: make chart take up 100% width

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -1,7 +1,10 @@
-import { useMemo } from 'react';
+import { useMemo, useRef, useState } from 'react';
+import useResizeObserver from '@react-hook/resize-observer';
 import { Bar } from '@visx/shape';
 import { Group } from '@visx/group';
 import { scaleBand, scaleLinear } from '@visx/scale';
+
+import './BarChart.scss';
 
 const verticalMargin = 40;
 
@@ -24,7 +27,30 @@ interface ChartProps extends BarChartProps {
   height: number;
 }
 
-export default function BarChart({ data, width, height }: ChartProps) {
+export default function BarChart({ data, height }: Omit<ChartProps, 'width'>) {
+  // find container size to fit
+  const chartContainer = useRef<HTMLDivElement>(null);
+  const [chartSize, setChartSize] = useState({ width: 0, height: 0 });
+
+  // redraw canvas when the screen size changes
+  useResizeObserver(chartContainer, (container) =>
+    setChartSize({
+      width: container.contentRect.width,
+      height: container.contentRect.height,
+    })
+  );
+  return (
+    <div className="visx-chart-area" ref={chartContainer} style={{ height }}>
+      <BarChartContent
+        width={chartSize.width || 0}
+        height={chartSize.height || 0}
+        data={data}
+      />
+    </div>
+  );
+}
+
+function BarChartContent({ data, height, width }: ChartProps) {
   // bounds
   const xMax = width;
   const yMax = height - verticalMargin;
