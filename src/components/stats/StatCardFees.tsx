25e744425834895f3407d32013b748fc49a4190a
David Uhlmann <dib542@gmail.com>
2023-06-08 22:18:41 +1000

    refactor: abstract out stat value fetching

--- src/components/stats/StatCardFees.tsx
+++ src/components/stats/StatCardFees.tsx
@@ -1,13 +1,8 @@
-import useSWR from 'swr';
-
 import StatCard from '../cards/StatCard';
 
-import { TimeSeriesPage, getLastDataChanges, getLastDataValues } from './utils';
-import { useTokenValueTotal } from '../../lib/web3/hooks/useTokens';
+import { useStatFee } from './hooks';
 import { Token } from '../../lib/web3/utils/tokens';
-import { formatCurrency } from '../../lib/utils/number';
-
-const { REACT_APP__INDEXER_API = '' } = process.env;
+import { formatStatTokenValue } from './utils';
 
 export default function StatCardFees({
   tokenA,
@@ -16,26 +11,14 @@ export default function StatCardFees({
   tokenA: Token;
   tokenB: Token;
 }) {
-  const { data } = useSWR<TimeSeriesPage>(
-    `${REACT_APP__INDEXER_API}/stats/volume/${tokenA?.address}/${tokenB?.address}`,
-    async (url) => {
-      const response = await fetch(url);
-      return response.json();
-    }
-  );
-
-  const [, , amountA, amountB] = getLastDataValues(data?.data);
-  const [, , amountDiffA, amountDiffB] = getLastDataChanges(data?.data);
-
-  const valueTotal = useTokenValueTotal([tokenA, amountA], [tokenB, amountB]);
-  const valueDiffTotal = useTokenValueTotal(
-    [tokenA, amountDiffA],
-    [tokenB, amountDiffB]
-  );
-
+  const [valueTotal, valueDiffTotal] = useStatFee(tokenA, tokenB);
   return (
-    <StatCard loading={!data} header="Fees (24H)" change={valueDiffTotal}>
-      {typeof valueTotal === 'number' ? formatCurrency(valueTotal) : undefined}
+    <StatCard
+      loading={valueTotal === undefined}
+      header="Fees (24H)"
+      change={valueDiffTotal}
+    >
+      {formatStatTokenValue(valueTotal)}
     </StatCard>
   );
 }
---
+++ src/components/stats/hooks.ts
@@ -0,0 +1,147 @@
+import useSWR from 'swr';
+import { useMemo } from 'react';
+
+import {
+  TimeSeriesPage,
+  TimeSeriesRow,
+  TokenValuePair,
+  getLastDataChanges,
+  getLastDataValues,
+} from './utils';
+import { Token } from '../../lib/web3/utils/tokens';
+import { useTokenValueTotal } from '../../lib/web3/hooks/useTokens';
+
+const { REACT_APP__INDEXER_API = '' } = process.env;
+
+function useIndexerData(
+  tokenA: Token,
+  tokenB: Token,
+  getIndexerPath: (tokenA: Token, tokenB: Token) => string
+) {
+  return useSWR<TimeSeriesPage>(
+    `${REACT_APP__INDEXER_API}/${getIndexerPath(tokenA, tokenB)}`,
+    async (url) => {
+      const response = await fetch(url);
+      return response.json();
+    }
+  );
+}
+
+export function useTimeSeriesData(
+  tokenA: Token,
+  tokenB: Token,
+  getIndexerPath: (tokenA: Token, tokenB: Token) => string,
+  getValues?: (values: number[]) => number[]
+) {
+  const response = useIndexerData(tokenA, tokenB, getIndexerPath);
+  const { data: pages, error } = response;
+
+  return useMemo<TimeSeriesRow[] | null | undefined>(() => {
+    // return error state as null
+    if (error && !pages) {
+      return null;
+    }
+    // return success state as loading or data
+    return pages && getValues
+      ? pages.data.map(([timeUnix, values]) => [timeUnix, getValues(values)])
+      : pages?.data;
+  }, [error, pages, getValues]);
+}
+
+export function useStatData(
+  tokenA: Token,
+  tokenB: Token,
+  getIndexerPath: (tokenA: Token, tokenB: Token) => string,
+  getValues?: (values: number[]) => number[]
+) {
+  const data = useTimeSeriesData(tokenA, tokenB, getIndexerPath, getValues);
+
+  const values = data && getLastDataValues(data);
+  const valueDiffs = data && getLastDataChanges(data);
+
+  // expect values to exist, if an empty array is found consider the stat to be
+  // in an error state not a loading state
+  return [
+    values && (values.length > 0 ? values : null),
+    valueDiffs && (valueDiffs.length > 0 ? valueDiffs : null),
+  ];
+}
+
+export function useStatTokenValue(
+  tokenA: Token,
+  tokenB: Token,
+  getIndexerPath: (tokenA: Token, tokenB: Token) => string,
+  getValues?: (values: number[]) => number[]
+): TokenValuePair {
+  const [amounts, amountDiffs] = useStatData(
+    tokenA,
+    tokenB,
+    getIndexerPath,
+    getValues
+  );
+
+  const [amountA, amountB] = amounts || [];
+  const [amountDiffA, amountDiffB] = amountDiffs || [];
+
+  const valueTotal = useTokenValueTotal([tokenA, amountA], [tokenB, amountB]);
+  const valueDiffTotal = useTokenValueTotal(
+    [tokenA, amountDiffA],
+    [tokenB, amountDiffB]
+  );
+
+  return [valueTotal, valueDiffTotal];
+}
+
+// TVL
+function getStatTvlPath(tokenA: Token, tokenB: Token) {
+  return `stats/tvl/${tokenA.address}/${tokenB.address}`;
+}
+function getStatTvlValues([amountA, amountB]: number[]): number[] {
+  return [amountA, amountB];
+}
+export function useStatTVL(tokenA: Token, tokenB: Token) {
+  return useStatTokenValue(tokenA, tokenB, getStatTvlPath, getStatTvlValues);
+}
+
+// volume and fees
+function getStatVolumePath(tokenA: Token, tokenB: Token) {
+  return `stats/volume/${tokenA.address}/${tokenB.address}`;
+}
+function getStatVolumeValues([amountA, amountB]: number[]): number[] {
+  return [amountA, amountB];
+}
+export function useStatVolume(tokenA: Token, tokenB: Token) {
+  return useStatTokenValue(
+    tokenA,
+    tokenB,
+    getStatVolumePath,
+    getStatVolumeValues
+  );
+}
+function getStatFeeValues([, , amountA, amountB]: number[]): number[] {
+  return [amountA, amountB];
+}
+export function useStatFee(tokenA: Token, tokenB: Token) {
+  return useStatTokenValue(tokenA, tokenB, getStatVolumePath, getStatFeeValues);
+}
+
+// volatility
+function getStatVolatilityPath(tokenA: Token, tokenB: Token) {
+  return `stats/volatility/${tokenA.address}/${tokenB.address}`;
+}
+function getStatVolatilityValues([value]: number[]): number[] {
+  return [value];
+}
+export function useStatVolatility(
+  tokenA: Token,
+  tokenB: Token
+): TokenValuePair {
+  const [amounts, amountDiffs] = useStatData(
+    tokenA,
+    tokenB,
+    getStatVolatilityPath,
+    getStatVolatilityValues
+  );
+  // return single values from data
+  return [amounts && amounts[0], amountDiffs && amountDiffs[0]];
+}
