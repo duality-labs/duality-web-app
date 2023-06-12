e8f950eeb1bd8b0eadc1be25ea828bf72d6f94fc
David Uhlmann <dib542@gmail.com>
2023-06-12 14:42:44 +1000

    feat: format new LineChart design

--- src/components/charts/TimeSeriesLineChart.tsx
+++ src/components/charts/TimeSeriesLineChart.tsx
@@ -1,21 +1,22 @@
-import { useMemo } from 'react';
-import { Bar } from '@visx/shape';
+import { useCallback, useMemo } from 'react';
+import { Bar, Circle, LinePath } from '@visx/shape';
 import { Group } from '@visx/group';
-import { scaleBand, scaleLinear } from '@visx/scale';
+import { useTooltip } from '@visx/tooltip';
+import { localPoint } from '@visx/event';
+import { scaleLinear } from '@visx/scale';
 
 import { ChartProps } from './Chart';
 import { TimeSeriesRow } from '../stats/utils';
 
+import './TimeSeriesLineChart.scss';
+
 const verticalMargin = 40;
+const horizontalMargin = 10; // leave enough space for point onHover circles
 
 // accessors
-const getX = (d: DataTuple) => d.x;
-const getY = (d: DataTuple) => d.y;
-
-interface DataTuple {
-  x: string;
-  y: number;
-}
+const getX = ([unixTime]: TimeSeriesRow) => unixTime;
+const getY = ([, values]: TimeSeriesRow) =>
+  values.reduce((acc, value) => acc + value, 0);
 
 interface TimeSeriesLineChartProps extends ChartProps<TimeSeriesRow> {
   width: number;
@@ -23,82 +24,126 @@ interface TimeSeriesLineChartProps extends ChartProps<TimeSeriesRow> {
   onHover: (data?: TimeSeriesRow) => void;
 }
 
-function useChartData(data: TimeSeriesRow[] | undefined) {
-  // map data to chart
-  return useMemo(() => {
-    return data
-      ?.map(([unixTime, values]) => {
-        // add values here properly with price data
-        return {
-          x: unixTime.toFixed(),
-          y: values.reduce((acc, value) => acc + value, 0),
-        };
-      })
-      .reverse();
-  }, [data]);
-}
-
 export default function TimeSeriesLineChart({
-  data: timeSeries,
+  data,
   height,
   width,
   onHover,
 }: TimeSeriesLineChartProps) {
   // bounds
-  const xMax = width;
-  const yMax = height - verticalMargin;
+  const innerWidth = width - horizontalMargin;
+  const innerHeight = height - verticalMargin;
 
-  const data = useChartData(timeSeries || undefined);
+  // sort data ascending (data arrives in reverse-chronological order)
+  const sortedData = useMemo(() => {
+    return data
+      .slice(0)
+      .reverse()
+      .sort(([unixTimeA], [unixTimeB]) => unixTimeA - unixTimeB);
+  }, [data]);
 
   // scales, memoize for performance
-  const xScale = useMemo(
-    () =>
-      scaleBand<string>({
-        range: [0, xMax],
-        round: true,
-        domain: (data || []).map(getX),
-        padding: 0.4,
-      }),
-    [xMax, data]
-  );
+  const xScale = useMemo(() => {
+    return scaleLinear<number>({
+      range: [0, innerWidth],
+      round: true,
+      domain:
+        sortedData.length > 0
+          ? [getX(sortedData[0]), getX(sortedData[sortedData.length - 1])]
+          : [],
+    });
+  }, [innerWidth, sortedData]);
   const yScale = useMemo(
     () =>
       scaleLinear<number>({
-        range: [yMax, 0],
+        range: [innerHeight, 0],
         round: true,
-        domain: [0, Math.max(...(data || []).map(getY))],
+        domain: [0, Math.max(...sortedData.map(getY))],
       }),
-    [yMax, data]
+    [innerHeight, sortedData]
+  );
+
+  const { tooltipData, tooltipLeft, tooltipTop, showTooltip, hideTooltip } =
+    useTooltip();
+
+  // tooltip handler
+  const handleTooltip = useCallback(
+    (
+      event: React.TouchEvent<SVGRectElement> | React.MouseEvent<SVGRectElement>
+    ) => {
+      const point = localPoint(event) || { x: 0 };
+      const x = xScale.invert(point.x);
+      const indexToRightOfX = sortedData.findIndex((d) => x <= getX(d));
+      const selectedIndex = (() => {
+        if (indexToRightOfX === undefined) return undefined;
+        switch (true) {
+          // no data to the right of this point: choose last data point
+          case indexToRightOfX < 0:
+            return sortedData.length - 1;
+          // not enough data to compare with: choose first point
+          case indexToRightOfX < 1:
+            return 0;
+          // compare the closest two points
+          default: {
+            const indexToLeftOfX = indexToRightOfX - 1;
+            const xLeft = getX(sortedData[indexToLeftOfX]);
+            const xRight = getX(sortedData[indexToRightOfX]);
+            // test if left gap is smaller
+            if (x - xLeft < xRight - x) {
+              return indexToLeftOfX;
+            } else {
+              return indexToRightOfX;
+            }
+          }
+        }
+      })();
+      const selectedData = sortedData[selectedIndex ?? -1];
+      onHover(selectedData);
+      if (selectedData !== undefined) {
+        showTooltip({
+          tooltipData: selectedData,
+          tooltipLeft: xScale(getX(selectedData)),
+          tooltipTop: yScale(getY(selectedData)),
+        });
+      }
+    },
+    [showTooltip, xScale, yScale, sortedData, onHover]
   );
 
   return width < 10 ? null : (
     <svg className="visx-line-chart" width={width} height={height}>
-      <Group top={verticalMargin / 2}>
-        {timeSeries.reverse().map((data) => {
-          const [unixTime, values] = data;
-          // add values here properly with price data
-          const d = {
-            x: unixTime.toFixed(),
-            y: values.reduce((acc, value) => acc + value, 0),
-          };
-          const x = getX(d);
-          const barWidth = xScale.bandwidth();
-          const barHeight = yMax - (yScale(getY(d)) ?? 0);
-          const barX = xScale(x);
-          const barY = yMax - barHeight;
-          return (
-            <Bar
-              key={`bar-${x}`}
-              x={barX}
-              y={barY}
-              width={barWidth}
-              height={barHeight}
-              fill="rgba(23, 233, 217, .5)"
-              onMouseOver={() => onHover(data)}
-            />
-          );
-        })}
+      <Group top={verticalMargin / 2} left={horizontalMargin / 2}>
+        <LinePath
+          x={(d) => xScale(getX(d)) ?? 0}
+          y={(d) => yScale(getY(d)) ?? 0}
+          data={data}
+          stroke="rgba(23, 233, 217, .5)"
+          strokeWidth={2}
+        />
+        {tooltipData && (
+          <Circle
+            className="visx-tooltip"
+            cx={tooltipLeft}
+            cy={tooltipTop}
+            r={3.5}
+            fill="blue"
+            stroke="white"
+            strokeWidth={1.5}
+            pointerEvents="none"
+          />
+        )}
       </Group>
+      {/* a single invisible Bar over the chart is used to place the tooltip */}
+      <Bar
+        width={width}
+        height={height}
+        fill="transparent"
+        rx={0}
+        onTouchStart={handleTooltip}
+        onTouchMove={handleTooltip}
+        onMouseMove={handleTooltip}
+        onMouseLeave={() => hideTooltip()}
+      />
     </svg>
   );
 }
