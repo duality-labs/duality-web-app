a8c980b670992e473c07f1384aef574a6b26e1db
David Uhlmann <dib542@gmail.com>
2023-06-16 06:45:15 +1000

    fix: prevent dragging of editMode ticks for now

--- src/pages/Pool/PoolManagement.tsx
+++ src/pages/Pool/PoolManagement.tsx
@@ -1198,8 +1198,9 @@ export default function PoolManagement({
                           : setUserTicks
                       }
                       advanced={editMode}
-                      canMoveUp
-                      canMoveDown
+                      // todo: fix dragging of edit mode ticks
+                      canMoveUp={!editMode}
+                      canMoveDown={!editMode}
                       oneSidedLiquidity={isValueAZero || isValueBZero}
                       ControlsComponent={ChartControls}
                     ></LiquiditySelector>
