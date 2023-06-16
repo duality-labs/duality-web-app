b9912902d8868e6f4d0b90a4b7a8f8caf9f855ad
David Uhlmann <dib542@gmail.com>
2023-06-16 13:18:33 +1000

    refactor: abstract Pool Management Tables into TableCard components

--- src/pages/Pool/PoolManagement.tsx
+++ src/pages/Pool/PoolManagement.tsx
@@ -30,6 +30,10 @@ import {
 import { useCurrentPriceFromTicks } from '../../components/LiquiditySelector/useCurrentPriceFromTicks';
 import RadioButtonGroupInput from '../../components/RadioButtonGroupInput/RadioButtonGroupInput';
 import PriceDataDisclaimer from '../../components/PriceDataDisclaimer';
+import {
+  MyEditedPositionTableCard,
+  MyNewPositionTableCard,
+} from './MyPositionTableCard';
 
 import useTokens from '../../lib/web3/hooks/useTokens';
 import { useDeposit } from './useDeposit';
@@ -695,32 +699,8 @@ export default function PoolManagement({
     tokenB?.address
   );
 
-  const [newReserveATotal, newReserveBTotal] = useMemo(() => {
-    return [
-      userTicks.reduce(
-        (acc, tick) => acc.plus(tick.reserveA),
-        new BigNumber(0)
-      ),
-      userTicks.reduce(
-        (acc, tick) => acc.plus(tick.reserveB),
-        new BigNumber(0)
-      ),
-    ];
-  }, [userTicks]);
-
   const pairPoolDepositFilter = usePoolDepositFilterForPair([tokenA, tokenB]);
   const userPositionsContext = useUserPositionsContext(pairPoolDepositFilter);
-
-  const [userReserveATotal, userReserveBTotal] = useMemo(() => {
-    return [
-      userPositionsContext?.reduce((acc, { token0Context }) => {
-        return acc.plus(token0Context?.userReserves || 0);
-      }, new BigNumber(0)),
-      userPositionsContext?.reduce((acc, { token1Context }) => {
-        return acc.plus(token1Context?.userReserves || 0);
-      }, new BigNumber(0)),
-    ];
-  }, [userPositionsContext]);
 
   const [{ isValidating: isValidatingEdit }, sendEditRequest] =
     useEditLiquidity();
@@ -1402,261 +1382,22 @@ export default function PoolManagement({
                 </div>
               </div>
             </div>
-            <div className="page-card">
-              <table className="my-position-table" style={{ width: '100%' }}>
-                <thead>
-                  <tr>
-                    <th style={{ width: '7.5%' }}></th>
-                    <th style={{ width: '20%' }}>Price</th>
-                    <th style={{ width: '20%' }}>Percent</th>
-                    <th style={{ width: '20%' }}>
-                      {tokenA.display.toUpperCase()}
-                    </th>
-                    <th style={{ width: '20%' }}>
-                      {tokenB.display.toUpperCase()}
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {isValueAZero && isValueBZero ? (
-                    editedUserPosition ? (
-                      editedUserPosition
-                        // sort by price
-                        .sort((a, b) => {
-                          return !!invertedTokenOrder
-                            ? b.deposit.centerTickIndex.toNumber() -
-                                a.deposit.centerTickIndex.toNumber()
-                            : a.deposit.centerTickIndex.toNumber() -
-                                b.deposit.centerTickIndex.toNumber();
-                        })
-                        .map(
-                          (
-                            {
-                              tickDiff0,
-                              tickDiff1,
-                              deposit,
-                              token0Context,
-                              token1Context,
-                            },
-                            index
-                          ) => {
-                            const reserveA = !invertedTokenOrder
-                              ? tickDiff0.plus(token0Context?.userReserves || 0)
-                              : tickDiff1.plus(
-                                  token1Context?.userReserves || 0
-                                );
-                            const reserveB = !invertedTokenOrder
-                              ? tickDiff1.plus(token1Context?.userReserves || 0)
-                              : tickDiff0.plus(
-                                  token0Context?.userReserves || 0
-                                );
-
-                            const tickIndex = !invertedTokenOrder
-                              ? new BigNumber(
-                                  deposit.centerTickIndex.toNumber()
-                                )
-                              : new BigNumber(
-                                  deposit.centerTickIndex.toNumber()
-                                ).negated();
-
-                            const price = tickIndexToPrice(tickIndex.negated());
-                            // show only those ticks that are in the currently visible range
-                            return viewableMinIndex !== undefined &&
-                              viewableMaxIndex !== undefined &&
-                              tickIndex.isGreaterThanOrEqualTo(
-                                viewableMinIndex
-                              ) &&
-                              tickIndex.isLessThanOrEqualTo(
-                                viewableMaxIndex
-                              ) ? (
-                              <tr key={index} className="pt-2">
-                                <td>{index + 1}</td>
-                                <td>
-                                  {new BigNumber(1).div(price).toFixed(5)}
-                                </td>
-                                <td>
-                                  {userReserveATotal &&
-                                  reserveA.isGreaterThan(1e-5)
-                                    ? `${
-                                        userReserveATotal.isGreaterThan(0)
-                                          ? new BigNumber(
-                                              reserveA
-                                                .multipliedBy(100)
-                                                .dividedBy(userReserveATotal)
-                                            ).toFixed(1)
-                                          : 0
-                                      }%`
-                                    : ''}
-                                  {userReserveBTotal &&
-                                  reserveB.isGreaterThan(1e-5)
-                                    ? `${
-                                        userReserveBTotal.isGreaterThan(0)
-                                          ? new BigNumber(
-                                              reserveB
-                                                .multipliedBy(100)
-                                                .dividedBy(userReserveBTotal)
-                                            ).toFixed(1)
-                                          : 0
-                                      }%`
-                                    : ''}
-                                </td>
-                                <td>
-                                  {reserveA.isGreaterThan(1e-5)
-                                    ? getAmountInDenom(
-                                        tokenA,
-                                        reserveA,
-                                        tokenA.address,
-                                        tokenA.display,
-                                        {
-                                          fractionalDigits: 3,
-                                          significantDigits: 3,
-                                        }
-                                      )
-                                    : ''}
-                                </td>
-                                <td>
-                                  {reserveB.isGreaterThan(1e-5)
-                                    ? getAmountInDenom(
-                                        tokenB,
-                                        reserveB,
-                                        tokenB.address,
-                                        tokenB.display,
-                                        {
-                                          fractionalDigits: 3,
-                                          significantDigits: 3,
-                                        }
-                                      )
-                                    : ''}
-                                </td>
-                                <td className="row gap-2 ml-4">
-                                  {reserveA
-                                    ?.plus(reserveB || 0)
-                                    .isGreaterThan(0) &&
-                                    (reserveA.isZero() ||
-                                      reserveB.isZero()) && (
-                                      <button
-                                        type="button"
-                                        className="button button-light my-3"
-                                        onClick={() => {
-                                          setEditedUserPosition((ticks) => {
-                                            return ticks.map((tick) => {
-                                              return tick.deposit.centerTickIndex.toNumber() ===
-                                                deposit.centerTickIndex.toNumber() &&
-                                                tick.deposit.fee.toNumber() ===
-                                                  deposit.fee.toNumber()
-                                                ? {
-                                                    ...tick,
-                                                    tickDiff0:
-                                                      tick.token0Context?.userReserves.negated() ||
-                                                      new BigNumber(0),
-                                                    tickDiff1:
-                                                      tick.token1Context?.userReserves.negated() ||
-                                                      new BigNumber(0),
-                                                  }
-                                                : tick;
-                                            });
-                                          });
-                                        }}
-                                      >
-                                        Withdraw
-                                      </button>
-                                    )}
-                                  {(!tickDiff0.isZero() ||
-                                    !tickDiff1.isZero()) && (
-                                    <button
-                                      type="button"
-                                      className="button button-default my-3"
-                                      onClick={() => {
-                                        setEditedUserPosition((ticks) => {
-                                          return ticks.map((tick) => {
-                                            return tick.deposit.centerTickIndex.toNumber() ===
-                                              deposit.centerTickIndex.toNumber() &&
-                                              tick.deposit.fee.toNumber() ===
-                                                deposit.fee.toNumber()
-                                              ? {
-                                                  ...tick,
-                                                  tickDiff0: new BigNumber(0),
-                                                  tickDiff1: new BigNumber(0),
-                                                }
-                                              : tick;
-                                          });
-                                        });
-                                      }}
-                                    >
-                                      Reset
-                                    </button>
-                                  )}
-                                </td>
-                              </tr>
-                            ) : null;
-                          }
-                        )
-                    ) : (
-                      <tr>
-                        <td
-                          colSpan={5}
-                          className="p-5"
-                          style={{ textAlign: 'center' }}
-                        >
-                          No deposits made yet
-                        </td>
-                      </tr>
-                    )
-                  ) : (
-                    userTicks.map((tick, index) => {
-                      // note: fix these restrictions, they are a bit off
-                      return tick.price
-                        .multipliedBy(1.01)
-                        .isGreaterThanOrEqualTo(rangeMin) &&
-                        tick.price
-                          .dividedBy(1.01)
-                          .isLessThanOrEqualTo(rangeMax) ? (
-                        <tr key={index} className="pt-2">
-                          <td>{index + 1}</td>
-                          <td>
-                            {new BigNumber(tick.price.toFixed(5)).toFixed(5)}
-                          </td>
-                          <td>
-                            {tick.reserveA.isGreaterThan(1e-5)
-                              ? `${
-                                  newReserveATotal.isGreaterThan(0)
-                                    ? new BigNumber(
-                                        tick.reserveA
-                                          .multipliedBy(100)
-                                          .dividedBy(newReserveATotal)
-                                      ).toFixed(1)
-                                    : 0
-                                }%`
-                              : ''}
-                            {tick.reserveB.isGreaterThan(1e-5)
-                              ? `${
-                                  newReserveBTotal.isGreaterThan(0)
-                                    ? new BigNumber(
-                                        tick.reserveB
-                                          .multipliedBy(100)
-                                          .dividedBy(newReserveBTotal)
-                                      ).toFixed(1)
-                                    : 0
-                                }%`
-                              : ''}
-                          </td>
-                          <td>
-                            {tick.reserveA.isGreaterThan(1e-5)
-                              ? tick.reserveA.toFixed(3)
-                              : ''}
-                          </td>
-                          <td>
-                            {tick.reserveB.isGreaterThan(1e-5)
-                              ? tick.reserveB.toFixed(3)
-                              : ''}
-                          </td>
-                        </tr>
-                      ) : null;
-                    })
-                  )}
-                </tbody>
-              </table>
-            </div>
+            {editMode ? (
+              <MyEditedPositionTableCard
+                tokenA={tokenA}
+                tokenB={tokenB}
+                editedUserPosition={editedUserPosition}
+                setEditedUserPosition={setEditedUserPosition}
+                viewableMinIndex={viewableMinIndex}
+                viewableMaxIndex={viewableMaxIndex}
+              />
+            ) : (
+              <MyNewPositionTableCard
+                tokenA={tokenA}
+                tokenB={tokenB}
+                userTicks={userTicks}
+              />
+            )}
             <div className="col pt-lg col-lg-hide">
               {hasEdits ? editLiquidityForm : addLiquidityForm}
             </div>
