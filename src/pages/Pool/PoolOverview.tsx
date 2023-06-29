67a0dd71524d183337a4232cd06ae61c14100c2c
David Uhlmann <dib542@gmail.com>
2023-06-29 18:47:40 +1000

    fix: string usage to be able to use ESLint and Prettier together
        - ESLint has a rule "quotes" that will enforce single quotes
          due to our settings
        - but Prettier first converts the single quotes to double quotes
          as it looks "prettier" to not have single quote backslashes
          in a string: eg: 'message.module=\'dex\''
        - we use backticks and substitution to avoid both behaviors here

--- src/pages/Pool/PoolOverview.tsx
+++ src/pages/Pool/PoolOverview.tsx
@@ -183,7 +183,7 @@ function TransactionsTable({
 
       // create Query string (with all appropriate characters escaped)
       const queryParams = new URLSearchParams({
-        events: "message.module='dex'",
+        events: `message.module='${'dex'}'`,
         order_by: 'ORDER_BY_DESC',
         'pagination.limit': `${pageSize || 10}`,
         // add page key if it exists
