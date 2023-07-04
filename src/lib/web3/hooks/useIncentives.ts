758fbc5043f10cdc7e8f9bec42320a1c23500ede
David Uhlmann <dib542@gmail.com>
2023-07-04 11:09:20 +1000

    feat: add incentives indicator for each staked row

--- src/lib/web3/hooks/useIncentives.ts
+++ src/lib/web3/hooks/useIncentives.ts
@@ -5,6 +5,9 @@ import type {
   GetGaugesRequest,
   GetGaugesResponseSDKType,
 } from '@duality-labs/dualityjs/types/codegen/duality/incentives/query';
+import { GaugeSDKType } from '@duality-labs/dualityjs/types/codegen/duality/incentives/gauge';
+
+import { ValuedUserPositionDepositContext } from './useUserShareValues';
 
 const { REACT_APP__REST_API = '' } = process.env;
 
@@ -27,4 +30,43 @@ export default function useIncentiveGauges({
       return lcd.duality.incentives.getGauges({ status, denom: '' });
     },
   });
+}
+
+function isIncentiveMatch(
+  userPosition: ValuedUserPositionDepositContext,
+  incentiveGauge: GaugeSDKType
+): boolean {
+  const { pairID, startTick, endTick } = incentiveGauge.distribute_to || {};
+  if (pairID && startTick !== undefined && endTick !== undefined) {
+    return (
+      // is this the correct tick pair?
+      pairID.token0 === userPosition.token0.address &&
+      pairID.token1 === userPosition.token1.address &&
+      // are ticks within bounds?
+      userPosition.deposit.lowerTickIndex.greaterThanOrEqual(startTick) &&
+      userPosition.deposit.upperTickIndex.lessThanOrEqual(endTick)
+    );
+  }
+  return false;
+}
+
+export function useMatchIncentives(
+  userPositionOrUserPositions:
+    | ValuedUserPositionDepositContext
+    | ValuedUserPositionDepositContext[]
+): GaugeSDKType[] | undefined {
+  const userPositions = Array.isArray(userPositionOrUserPositions)
+    ? userPositionOrUserPositions
+    : [userPositionOrUserPositions];
+  const { data: incentives } = useIncentiveGauges();
+
+  if (incentives !== undefined) {
+    return incentives.gauges.filter((gauge) => {
+      return userPositions.some((userPosition) => {
+        return isIncentiveMatch(userPosition, gauge);
+      });
+    });
+  }
+
+  return undefined;
 }
