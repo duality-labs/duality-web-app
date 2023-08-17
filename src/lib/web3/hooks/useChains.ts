d63175a2716bba600c86267e4c7fedb49c2cde53
David Uhlmann <dib542@gmail.com>
2023-08-17 14:05:03 +1000

    feat: ensure only open channel tokens are shown in list

--- src/lib/web3/hooks/useChains.ts
+++ src/lib/web3/hooks/useChains.ts
@@ -2,6 +2,10 @@ import { Chain } from '@chain-registry/types';
 import { useMemo, useState } from 'react';
 import { ibc } from '@duality-labs/dualityjs';
 import { QueryClientStatesResponseSDKType } from '@duality-labs/dualityjs/types/codegen/ibc/core/client/v1/query';
+import { QueryConnectionsResponseSDKType } from '@duality-labs/dualityjs/types/codegen/ibc/core/connection/v1/query';
+import { QueryChannelsResponseSDKType } from '@duality-labs/dualityjs/types/codegen/ibc/core/channel/v1/query';
+import { State as ChannelState } from '@duality-labs/dualityjs/types/codegen/ibc/core/channel/v1/channel';
+import { State as ConnectionState } from '@duality-labs/dualityjs/types/codegen/ibc/core/connection/v1/connection';
 import { useQuery } from '@tanstack/react-query';
 
 import { getChainInfo } from '../wallets/keplr';
@@ -92,15 +96,99 @@ function useIbcClientStates(chain: Chain) {
   });
 }
 
-export function useConnectedChainIDs(chain: Chain = dualityChain) {
-  const { data: { client_states: ibcClientStates } = {} } =
-    useIbcClientStates(chain);
-  // list of connected chain IDs from IBC client list
-  // todo: ensure the chain connections are status: Active (not Expired)
+function useIbcConnections(chain: Chain) {
+  return useQuery({
+    queryKey: ['ibc-connections', chain.chain_id],
+    queryFn: async (): Promise<QueryConnectionsResponseSDKType | undefined> => {
+      // get IBC LCD client
+      const lcd = await getIbcLcdClient(chain);
+      return lcd?.ibc.core.connection.v1.connections();
+    },
+    refetchInterval: 5 * minutes,
+  });
+}
+
+function useIbcChannels(chain: Chain) {
+  return useQuery({
+    queryKey: ['ibc-channels', chain.chain_id],
+    queryFn: async (): Promise<QueryChannelsResponseSDKType | undefined> => {
+      // get IBC LCD client
+      const lcd = await getIbcLcdClient(chain);
+      // note: it appears that channels only appear in this list if they are of:
+      // - state: "STATE_OPEN" connections
+      // - status: "Active" clients
+      //   - maybe: confirm state with an Active client but not OPEN connection
+      return lcd?.ibc.core.channel.v1.channels();
+    },
+    refetchInterval: 5 * minutes,
+  });
+}
+
+function filterConnectionsOpen(
+  connection: QueryConnectionsResponseSDKType['connections'][number]
+): boolean {
+  // convert state Type to returned string representation of the enum
+  const state = connection.state as unknown as keyof typeof ConnectionState;
+  return state === 'STATE_OPEN';
+}
+
+function filterChannelsOpen(
+  channel: QueryChannelsResponseSDKType['channels'][number]
+): boolean {
+  // convert state Type to returned string representation of the enum
+  const state = channel.state as unknown as keyof typeof ChannelState;
+  return state === 'STATE_OPEN';
+}
+
+function useIbcOpenTransfers(chain: Chain = dualityChain) {
+  const { data: { client_states } = {} } = useIbcClientStates(chain);
+  const { data: { connections } = {} } = useIbcConnections(chain);
+  const { data: { channels } = {} } = useIbcChannels(chain);
+
   return useMemo(() => {
-    return (ibcClientStates || []).map((state) => {
-      // give correct clientState type
-      return (state.client_state as unknown as { chain_id: string }).chain_id;
+    // get openClients (all listed clients are assumed to be working)
+    const openClients = client_states || [];
+    // get open connections
+    const openConnections = (connections || []).filter(filterConnectionsOpen);
+    // get open channels
+    const openChannels = (channels || []).filter(filterChannelsOpen);
+
+    // note: we assume that if a client exists and its connections and channels
+    //       are open, then the same open resources exist on the counterparty.
+    //       this may not be true, but is good enough for some UI lists
+    return openClients.flatMap((clientState) => {
+      const chainID = (
+        clientState.client_state as unknown as { chain_id: string }
+      ).chain_id;
+      return (
+        openConnections
+          // filter to connections of the current client
+          .filter((c) => c.client_id === clientState.client_id)
+          .flatMap((connection) => {
+            return (
+              openChannels
+                // filter to transfer channels that end at the current connection
+                .filter((ch) => ch.port_id === 'transfer')
+                .filter((ch) => ch.connection_hops.at(-1) === connection.id)
+                .map((channel) => {
+                  return {
+                    chainID,
+                    client: clientState,
+                    connection,
+                    channel,
+                  };
+                })
+            );
+          })
+      );
     });
-  }, [ibcClientStates]);
+  }, [client_states, connections, channels]);
+}
+
+export function useConnectedChainIDs(chain: Chain = dualityChain) {
+  const openTransfers = useIbcOpenTransfers();
+  // return only chain IDs for easy comparison to different lists
+  return useMemo(() => {
+    return openTransfers.map((openTransfer) => openTransfer.chainID);
+  }, [openTransfers]);
 }
