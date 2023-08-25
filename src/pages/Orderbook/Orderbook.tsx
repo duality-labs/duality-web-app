1f06e8aeb30ad1b789b6c43e520b4304d031f7b2
David Uhlmann <dib542@gmail.com>
2023-08-25 19:07:19 +1000

    feat: add Orderbook page layout

--- src/pages/Orderbook/Orderbook.tsx
+++ src/pages/Orderbook/Orderbook.tsx
@@ -1,3 +1,7 @@
+import { useMatch } from 'react-router-dom';
+
+import { useTokenBySymbol } from '../../lib/web3/hooks/useTokens';
+
 export default function OrderbookPage() {
   return (
     <div className="container flex row">
@@ -9,5 +13,50 @@ export default function OrderbookPage() {
 }
 
 function Orderbook() {
-  return <div className="flex col"></div>;
+  // change tokens to match pathname
+  const match = useMatch('/orderbook/:tokenA/:tokenB');
+  const tokenA = useTokenBySymbol(match?.params['tokenA']);
+  const tokenB = useTokenBySymbol(match?.params['tokenB']);
+
+  return (
+    <div className="flex col gap-3">
+      <div className="row">
+        <div className="page-card flex">
+          <div className="row">
+            <div className="col">
+              Nav {tokenA?.symbol} {tokenB?.symbol}
+            </div>
+            <div className="col ml-auto">Nav right</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex row gap-3">
+        <div className="flex col">
+          <div className="flex page-card">left</div>
+        </div>
+        <div className="col">
+          <div className="flex page-card">center</div>
+        </div>
+        <div className="col">
+          <div className="flex page-card">right</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="page-card flex">
+          <div className="row flex-centered mb-lg">
+            <div className="col">
+              <div className="row flex-centered gap-lg">
+                <h4 className="h4">Orders</h4>
+                <div>Hide other pairs</div>
+              </div>
+            </div>
+            <div className="col ml-auto">Card Nav right</div>
+          </div>
+          <div className="row">
+            <div>Table</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
