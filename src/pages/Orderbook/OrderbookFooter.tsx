60e6741475d1304c11d07358aea9721550358ef1
David Uhlmann <dib542@gmail.com>
2023-08-26 05:15:03 +1000

    feat: add OrderbookFooter

---
+++ src/pages/Orderbook/OrderbookFooter.tsx
@@ -0,0 +1,46 @@
+import { Token } from '../../lib/web3/utils/tokens';
+import useTransactionTableData from '../Pool/hooks/useTransactionTableData';
+
+export default function OrderbookFooter({
+  tokenA,
+  tokenB,
+}: {
+  tokenA?: Token;
+  tokenB?: Token;
+}) {
+  return (
+    <div className="page-card flex">
+      <div className="row flex-centered mb-lg">
+        <div className="col">
+          <div className="row flex-centered gap-lg">
+            <h4 className="h4">Orders</h4>
+            <div>Hide other pairs</div>
+          </div>
+        </div>
+        <div className="col ml-auto">Card Nav right</div>
+      </div>
+      <div className="row">
+        {tokenA && tokenB && (
+          <OrderbookFooterTable tokenA={tokenA} tokenB={tokenB} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+const pageSize = 10;
+function OrderbookFooterTable({
+  tokenA,
+  tokenB,
+}: {
+  tokenA: Token;
+  tokenB: Token;
+}) {
+  useTransactionTableData({
+    tokenA,
+    tokenB,
+    action: 'PlaceLimitOrder',
+    pageSize,
+  });
+  return <div>Table</div>;
+}
