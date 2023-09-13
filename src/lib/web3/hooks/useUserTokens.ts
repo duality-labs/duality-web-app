5faf830159ced04b1562ceba1b20a1538afe90e8
David Uhlmann <dib542@gmail.com>
2023-09-14 07:41:56 +1000

    refactor: simplify userToken filtering

--- src/lib/web3/hooks/useUserTokens.ts
+++ src/lib/web3/hooks/useUserTokens.ts
@@ -1,18 +1,9 @@
 import { useMemo } from 'react';
-import useTokens, { matchTokens, useTokensWithIbcInfo } from './useTokens';
 import { useBankBalances } from '../indexerProvider';
 
 export default function useUserTokens() {
-  const tokenList = useTokensWithIbcInfo(useTokens());
-
   const { data: balances } = useBankBalances();
   return useMemo(() => {
-    return balances
-      ? tokenList.filter((token) => {
-          return balances.find((balance) => {
-            return matchTokens(balance.token, token);
-          });
-        })
-      : [];
-  }, [tokenList, balances]);
+    return balances?.map((balance) => balance.token) ?? [];
+  }, [balances]);
 }
