c5edb2b5e9686d7781cd7135947da138a2de36b7
David Uhlmann <dib542@gmail.com>
2023-09-14 16:21:57 +1000

    feat: add "Removes" heading to Transactions table for Withdraw events

--- src/pages/Pool/PoolOverview.tsx
+++ src/pages/Pool/PoolOverview.tsx
@@ -653,6 +653,7 @@ interface EventColumnProps<T> {
   heading: TransactionTableColumnKey;
 }
 interface GenericEventColumnProps<T> extends EventColumnProps<T> {
+  action: 'Deposit' | 'Withdraw';
   getToken0Reserves: (event: T) => string;
   getToken1Reserves: (event: T) => string;
 }
@@ -665,6 +666,7 @@ function EventColumn<
   heading,
   tokenA,
   tokenB,
+  action,
   getToken0Reserves,
   getToken1Reserves,
 }: GenericEventColumnProps<T>) {
@@ -684,7 +686,7 @@ function EventColumn<
       case 'Wallet':
         return formatAddress(Creator);
       case 'Type':
-        return `Add ${[
+        return `${action === 'Deposit' ? 'Add' : 'Remove'} ${[
           Number(getTokenAReserves()) > 0 && tokenA.symbol,
           Number(getTokenBReserves()) > 0 && tokenB.symbol,
         ]
@@ -772,6 +774,7 @@ function DepositColumn(props: EventColumnProps<DexDepositEvent>) {
   return (
     <EventColumn<DexDepositEvent>
       {...props}
+      action="Deposit"
       getToken0Reserves={getToken0Reserves}
       getToken1Reserves={getToken1Reserves}
     />
@@ -795,6 +798,7 @@ function WithdrawalColumn(props: EventColumnProps<DexWithdrawalEvent>) {
   return (
     <EventColumn<DexWithdrawalEvent>
       {...props}
+      action="Withdraw"
       getToken0Reserves={getToken0Reserves}
       getToken1Reserves={getToken1Reserves}
     />
