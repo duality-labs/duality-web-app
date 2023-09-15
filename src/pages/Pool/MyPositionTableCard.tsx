b2ded999073d5c1ae75f7acf9a3c74e7642911a6
David Uhlmann <dib542@gmail.com>
2023-09-15 20:34:54 +1000

    fix: make formatting of smaller than minimum numbers consistent part 2

--- src/pages/Pool/MyPositionTableCard.tsx
+++ src/pages/Pool/MyPositionTableCard.tsx
@@ -123,7 +123,9 @@ export function MyNewPositionTableCard({
           return (
             <tr key={index} className="pt-2">
               <td>{index + 1}</td>
-              <td>{new BigNumber(priceBToA.toFixed(5)).toFixed(5)}</td>
+              <td>
+                {priceBToA ? formatAmount(priceBToA.toNumber(), {}, 0) : '-'}
+              </td>
               <td>
                 {reserveA.isGreaterThan(0) && (
                   <div>{formatCurrency(valueA)}</div>
@@ -303,7 +305,7 @@ export function MyEditedPositionTableCard({
             ? new BigNumber(deposit.centerTickIndex1To0.toNumber())
             : new BigNumber(deposit.centerTickIndex1To0.toNumber()).negated();
 
-          const priceBToA = tickIndexToPrice(tickIndexBToA.negated());
+          const priceBToA = tickIndexToPrice(tickIndexBToA);
           // show only those ticks that are in the currently visible range
           return viewableMinIndex !== undefined &&
             viewableMaxIndex !== undefined &&
@@ -311,7 +313,9 @@ export function MyEditedPositionTableCard({
             tickIndexBToA.isLessThanOrEqualTo(viewableMaxIndex) ? (
             <tr key={index} className="pt-2">
               <td>{index + 1}</td>
-              <td>{new BigNumber(1).div(priceBToA).toFixed(5)}</td>
+              <td>
+                {priceBToA ? formatAmount(priceBToA.toNumber(), {}, 0) : '-'}
+              </td>
               <td>
                 {reserveA.isGreaterThan(0) && (
                   <div>{formatCurrency(valueA)}</div>
