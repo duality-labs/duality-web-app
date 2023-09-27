2a45d3216c6422460bfc6d9f720e286cedf14e2d
David Uhlmann <dib542@gmail.com>
2023-09-27 21:46:38 +1000

    feat: add NumericInput row component

--- src/components/cards/LimitOrderCard.tsx
+++ src/components/cards/LimitOrderCard.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 
 import TabsCard from './TabsCard';
 import Tabs from '../Tabs';
@@ -82,13 +82,97 @@ function LimitOrder({
 }) {
   const buyMode = !sellMode;
 
+  const [amount, setAmount] = useState('0');
+  const [total, setTotal] = useState('0');
   return (
     <div>
+      <div className="my-3">
+        <NumericInputRow
+          prefix="Amount"
+          value={amount}
+          onChange={setAmount}
+          suffix={tokenA?.symbol}
+        />
+      </div>
+      <div className="my-3">
+        <NumericInputRow
+          prefix="Total"
+          value={total}
+          onChange={setTotal}
+          suffix={tokenB?.symbol}
+          readOnly
+        />
+      </div>
       <div className="flex row">
         <button className="limit-order__confirm-button flex button-primary my-lg py-4">
           {buyMode ? 'Buy' : 'Sell'}
         </button>
       </div>
+    </div>
+  );
+}
+
+function NumericInputRow({
+  prefix = '',
+  value = '',
+  onInput,
+  onChange = onInput,
+  suffix = '',
+  min,
+  max,
+  format,
+  readOnly = false,
+}: {
+  prefix?: string;
+  value: string;
+  onInput?: (value: string) => void;
+  onChange?: (value: string) => void;
+  suffix?: string;
+  min?: number;
+  max?: number;
+  format?: (value: number) => string;
+  readOnly?: boolean;
+}) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [internalValue = value, setInternalValue] = useState<string>();
+  /**
+   * Makes sure the value is valid number within the proper range
+   * @param newValue the proposed new value to be checked
+   */
+  const maybeUpdate = useCallback(
+    (newValueString: string, onChange?: (value: string) => void) => {
+      if (onChange) {
+        const newValue = Number(newValueString);
+        if (min !== undefined && newValue < min) {
+          return onChange(format ? format(min) : min.toFixed());
+        }
+        if (max !== undefined && newValue > max) {
+          return onChange(format ? format(max) : max.toFixed());
+        }
+        if (!Number.isNaN(newValue)) {
+          return onChange(format ? format(newValue) : newValue.toFixed());
+        }
+      }
+    },
+    [min, max, format]
+  );
+
+  return (
+    <div className="token-amount-input flex row py-3 px-4">
+      <div className="token-amount-input__prefix">{prefix}</div>
+      <input
+        className="token-amount-input__input mx-3 flex"
+        value={internalValue}
+        onInput={() => maybeUpdate(inputRef.current?.value || '0', onInput)}
+        onChange={(e) => {
+          setInternalValue(e.target.value);
+          maybeUpdate(e.target.value || '0', onChange);
+        }}
+        onBlur={() => setInternalValue(undefined)}
+        readOnly={readOnly}
+      ></input>
+      <div className="token-amount-input__suffix">{suffix}</div>
     </div>
   );
 }
