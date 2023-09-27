145b87a658de000e78bb09c467d42b95d0c3df3d
David Uhlmann <dib542@gmail.com>
2023-09-27 23:44:59 +1000

    feat: add Swap logic to LimitOrderCard

--- src/components/cards/LimitOrderCard.tsx
+++ src/components/cards/LimitOrderCard.tsx
@@ -1,9 +1,14 @@
+import Long from 'long';
 import { useCallback, useMemo, useRef, useState } from 'react';
 
 import TabsCard from './TabsCard';
 import Tabs from '../Tabs';
 
-import { Token, getDisplayDenomAmount } from '../../lib/web3/utils/tokens';
+import {
+  Token,
+  getBaseDenomAmount,
+  getDisplayDenomAmount,
+} from '../../lib/web3/utils/tokens';
 import { dualityMainToken } from '../../lib/web3/hooks/useTokens';
 import {
   formatCurrency,
@@ -16,6 +21,11 @@ import './LimitOrderCard.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import Tooltip from '../Tooltip';
+import { useSwap } from '../../pages/Swap/hooks/useSwap';
+import { useRouterResult } from '../../pages/Swap/hooks/useRouter';
+import { useWeb3 } from '../../lib/web3/useWeb3';
+import { useOrderedTokenPair } from '../../lib/web3/hooks/useTokenPairs';
+import { useTokenPairTickLiquidity } from '../../lib/web3/hooks/useTickLiquidity';
 
 export default function LimitOrderCard({
   tokenA,
@@ -94,17 +104,137 @@ function LimitOrder({
   sell?: boolean;
 }) {
   const buyMode = !sellMode;
+  const [token0, token1] =
+    useOrderedTokenPair([tokenA?.address, tokenB?.address]) || [];
+  const {
+    data: [token0Ticks, token1Ticks],
+  } = useTokenPairTickLiquidity([token0, token1]);
 
   const [amount, setAmount] = useState('0');
   const [total, setTotal] = useState('0');
   const [sliderIndex, setSliderIndex] = useState<number>(0);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const sliderValue = sliderValues[sliderIndex] || 0;
+  const slippage = sliderValues[sliderIndex] || 0;
 
   const [fee] = useState('0');
+  const [{ isValidating: isValidatingSwap }, swapRequest] = useSwap();
+
+  const { data: routerResult } = useRouterResult({
+    tokenA: tokenA?.address,
+    tokenB: tokenB?.address,
+    valueA: amount,
+    valueB: undefined,
+  });
+  const { address, connectWallet } = useWeb3();
+
+  const onFormSubmit = useCallback(
+    function (event?: React.FormEvent<HTMLFormElement>) {
+      if (event) event.preventDefault();
+      // calculate tolerance from user slippage settings
+      // set tiny minimum of tolerance as the frontend calculations
+      // don't always exactly align with the backend calculations
+      const tolerance = Math.max(1e-12, slippage);
+      const tickIndexLimit = routerResult?.tickIndexOut?.toNumber();
+      if (
+        address &&
+        routerResult &&
+        tokenA &&
+        tokenB &&
+        !isNaN(tolerance) &&
+        tickIndexLimit &&
+        !isNaN(tickIndexLimit)
+      ) {
+        // convert to swap request format
+        const result = routerResult;
+        // Cosmos requires tokens in integer format of smallest denomination
+        // calculate gas estimate
+        const tickMin =
+          routerResult.tickIndexIn &&
+          routerResult.tickIndexOut &&
+          Math.min(
+            routerResult.tickIndexIn.toNumber(),
+            routerResult.tickIndexOut.toNumber()
+          );
+        const tickMax =
+          routerResult.tickIndexIn &&
+          routerResult.tickIndexOut &&
+          Math.max(
+            routerResult.tickIndexIn.toNumber(),
+            routerResult.tickIndexOut.toNumber()
+          );
+        const forward = result.tokenIn === token0;
+        const ticks = forward ? token1Ticks : token0Ticks;
+        const ticksPassed =
+          (tickMin !== undefined &&
+            tickMax !== undefined &&
+            ticks?.filter((tick) => {
+              return (
+                tick.tickIndex1To0.isGreaterThanOrEqualTo(tickMin) &&
+                tick.tickIndex1To0.isLessThanOrEqualTo(tickMax)
+              );
+            })) ||
+          [];
+        const ticksUsed =
+          ticksPassed?.filter(
+            forward
+              ? (tick) => !tick.reserve1.isZero()
+              : (tick) => !tick.reserve0.isZero()
+          ).length || 0;
+        const ticksUnused =
+          new Set<number>([
+            ...(ticksPassed?.map((tick) => tick.tickIndex1To0.toNumber()) ||
+              []),
+          ]).size - ticksUsed;
+        const gasEstimate = ticksUsed
+          ? // 120000 base
+            120000 +
+            // add 80000 if multiple ticks need to be traversed
+            (ticksUsed > 1 ? 80000 : 0) +
+            // add 1000000 for each tick that we need to remove liquidity from
+            1000000 * (ticksUsed - 1) +
+            // add 500000 for each tick we pass without drawing liquidity from
+            500000 * ticksUnused +
+            // add another 500000 for each reverse tick we pass without drawing liquidity from
+            (forward ? 0 : 500000 * ticksUnused)
+          : 0;
+
+        swapRequest(
+          {
+            amountIn: getBaseDenomAmount(tokenA, result.amountIn) || '0',
+            tokenIn: result.tokenIn,
+            tokenOut: result.tokenOut,
+            creator: address,
+            receiver: address,
+            // see LimitOrderType in types repo (cannot import at runtime)
+            // https://github.com/duality-labs/dualityjs/blob/2cf50a7af7bf7c6b1490a590a4e1756b848096dd/src/codegen/duality/dex/tx.ts#L6-L13
+            // using type IMMEDIATE_OR_CANCEL so that partially filled requests
+            // succeed (in testing when swapping 1e18 utokens, often the order
+            // would be filled with 1e18-2 utokens and FILL_OR_KILL would fail)
+            // todo: use type FILL_OR_KILL: order must be filled completely
+            orderType: 2,
+            // todo: set tickIndex to allow for a tolerance:
+            //   the below function is a tolerance of 0
+            tickIndex: Long.fromNumber(tickIndexLimit * (forward ? 1 : -1)),
+            maxAmountOut: getBaseDenomAmount(tokenB, result.amountOut) || '0',
+          },
+          gasEstimate
+        );
+      }
+    },
+    [
+      address,
+      routerResult,
+      tokenA,
+      tokenB,
+      token0,
+      token0Ticks,
+      token1Ticks,
+      slippage,
+      swapRequest,
+    ]
+  );
 
   return (
-    <div>
+    <form onSubmit={onFormSubmit}>
       <div className="mt-2 mb-4">
         <NumericInputRow
           prefix="Amount"
@@ -201,8 +331,12 @@ function LimitOrder({
         />
       </div>
       <div className="flex row">
-        <button className="limit-order__confirm-button flex button-primary my-lg py-4">
-          {buyMode ? 'Buy' : 'Sell'}
+        <button
+          className="limit-order__confirm-button flex button-primary my-lg py-4"
+          onClick={!address ? connectWallet : undefined}
+          disabled={isValidatingSwap}
+        >
+          {!address ? 'Connect Wallet' : buyMode ? 'Buy' : 'Sell'}
         </button>
       </div>
       <div className="flex row">
@@ -237,7 +371,7 @@ function LimitOrder({
           suffix={tokenB?.symbol}
         />
       </div>
-    </div>
+    </form>
   );
 }
 
