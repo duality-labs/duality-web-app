178973a8b22d080d750e6d7c8993f58e49bcb300
David Uhlmann <dib542@gmail.com>
2023-10-01 18:01:11 +1000

    feat: allow Drawer to act like a floating Dropdown

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -5,28 +5,41 @@ export default function Drawer({
   expanded = false,
   className,
   containerClassName,
+  floating = false,
   children,
 }: {
   expanded: boolean;
   className?: string;
   containerClassName?: string;
+  floating?: boolean;
   children: React.ReactNode;
 }) {
   const contentRef = useRef<HTMLDivElement>(null);
+  const dynamicHeight =
+    expanded && contentRef.current ? contentRef.current.offsetHeight : 0;
 
   return (
     <div
-      className={['drawer', containerClassName].filter(Boolean).join(' ')}
-      style={{
-        height:
-          expanded && contentRef.current ? contentRef.current.offsetHeight : 0,
-      }}
+      className={['drawer', containerClassName, floating && 'floating']
+        .filter(Boolean)
+        .join(' ')}
+      style={
+        !floating
+          ? {
+              height: dynamicHeight,
+            }
+          : undefined
+      }
     >
       <div
-        ref={contentRef}
         className={['drawer-panel', className].filter(Boolean).join(' ')}
+        style={{
+          height: dynamicHeight,
+        }}
       >
-        {children}
+        <div className="drawer-content" ref={contentRef}>
+          {children}
+        </div>
       </div>
     </div>
   );
