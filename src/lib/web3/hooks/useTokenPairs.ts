6e3547985e812cdafe508a804bc704604c8d513d
David Uhlmann <dib542@gmail.com>
2023-11-21 03:05:41 +1000

    feat: swap out useTokenPair logic from LCD to Indexer endpoint

--- src/lib/web3/hooks/useTokenPairs.ts
+++ src/lib/web3/hooks/useTokenPairs.ts
@@ -1,109 +1,37 @@
-import Long from 'long';
-import { useEffect, useMemo } from 'react';
-
-import { useLcdClientPromise } from '../lcdClient';
-
-import {
-  QueryTotalSupplyRequest,
-  QueryTotalSupplyResponse,
-} from '@duality-labs/dualityjs/types/codegen/cosmos/bank/v1beta1/query';
-import { getShareInfo } from '../utils/shares';
-import { getPairID } from '../utils/pairs';
 import { TokenAddress } from '../utils/tokens';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useIndexerStreamOfSingleDataSet } from './useIndexer';
 
-type QueryAllTokenMapState = {
-  data: [TokenAddress, TokenAddress][] | undefined;
+export type TokenPairReserves = [
+  token0: TokenAddress,
+  token1: TokenAddress,
+  reserve0: number,
+  reserve1: number
+];
+type DataRow = [index: number, values: TokenPairReserves];
+
+type TokenPairsState = {
+  data: TokenPairReserves[] | undefined;
   isValidating: boolean;
   error: Error | null;
 };
 
-// experimentally timed that 250 is faster than 100 or 1000 items per page
-//   - experiment list length: 7,220)
-const defaultPaginationLimit = Long.fromNumber(250);
+export default function useTokenPairs(): TokenPairsState {
+  const { data, isValidating, error } =
+    useIndexerStreamOfSingleDataSet<DataRow>('/liquidity/pairs');
 
-export default function useTokenPairs({
-  queryOptions,
-  query: queryConfig,
-  queryClient: queryClientConfig,
-}: {
-  // todo: pass entire useQuery options set here
-  queryOptions?: { refetchInterval: number | false };
-  query?: QueryTotalSupplyRequest;
-  queryClient?: string;
-} = {}): QueryAllTokenMapState {
-  const lcdClientPromise = useLcdClientPromise(queryClientConfig);
-
-  const {
-    data,
-    error,
-    isFetching: isValidating,
-    hasNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery({
-    ...queryOptions,
-    queryKey: ['cosmos.bank.v1beta1.totalSupply'],
-    queryFn: async ({
-      pageParam: nextKey,
-    }): Promise<QueryTotalSupplyResponse | undefined> => {
-      const client = await lcdClientPromise;
-      return await client.cosmos.bank.v1beta1.totalSupply({
-        ...queryConfig,
-        pagination: {
-          limit: defaultPaginationLimit,
-          ...queryConfig?.pagination,
-          ...(nextKey && {
-            key: nextKey,
-          }),
-        },
-      });
-    },
-    defaultPageParam: undefined,
-    getNextPageParam: (lastPage: QueryTotalSupplyResponse | undefined) => {
-      // don't pass an empty array as that will trigger another page to download
-      return lastPage?.pagination?.next_key?.length
-        ? lastPage?.pagination?.next_key
-        : undefined;
-    },
-  });
-
-  // fetch more data if data has changed but there are still more pages to get
-  useEffect(() => {
-    if (fetchNextPage && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [data, fetchNextPage, hasNextPage]);
-
-  // place pages of data into the same list
-  const tradingPairs = useMemo(() => {
-    const tradingPairMap = data?.pages?.reduce<
-      Map<string, [TokenAddress, TokenAddress]>
-    >((acc, page) => {
-      page?.supply.forEach((coin) => {
-        const match = getShareInfo(coin);
-        if (match) {
-          const { token0Address, token1Address } = match;
-          acc.set(getPairID(token0Address, token1Address), [
-            token0Address,
-            token1Address,
-          ]);
-        }
-      });
-
-      return acc;
-    }, new Map<string, [TokenAddress, TokenAddress]>());
-    return tradingPairMap && Array.from(tradingPairMap.values());
-  }, [data]);
-
+  if (data) {
+    const values = Array.from(data, (row) => row[1][1] as TokenPairReserves);
+    return { data: values, isValidating, error: error || null };
+  }
   // return state
-  return { data: tradingPairs, isValidating, error };
+  return { data: undefined, isValidating, error: error || null };
 }
 
 // add convenience method to fetch ticks in a pair
 export function useOrderedTokenPair([tokenA, tokenB]: [
   TokenAddress?,
   TokenAddress?
-]): [token0: TokenAddress, token1: TokenAddress] | undefined {
+]): TokenPairReserves | undefined {
   const { data: tokenPairs } = useTokenPairs();
   // search for ordered token pair in our token pair list
   return tokenA && tokenB
