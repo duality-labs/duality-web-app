c128b34fea7448d726136715e85d78a49791b894
David Uhlmann <dib542@gmail.com>
2023-11-29 22:45:01 +1000

    feat: add TradingView charts (#477)
    * feat: add TradingView charting library dependency
    
    * feat: add use-deep-compare-effect dependency
    
    * fix: ensure local TradingView dependencies can be found on server
    
    * fix: remove unneeded types dependency
    
    * feat: swap out ApexCharts for Trading view charts
    
    * feat: allow Indexer streams to pass on data block height info
    
    * feat: allow indexer stream requests to be made without SSE
    
        - useful for short known-length responses to be slightly quicker
    
    * feat: simplify TradingView data subscriptions with indexer streams
    
    * feat: simplify searchParams with getFetchURL type
    
    * feat: forward outer errors through to error handler:
    
        - otherwise these types of errors are never handled
    
    * feat: remove apexcharts dependency
    
    * feat: allow substring searching in token list
    
    * refactor: only create TradingView options until after conditions met
    
        - mostly a whitespace change
    
    * feat: add personal GitHub token into "npm ci" build step
    
    * feat: restrict access to personal token when not needed
    
    * feat: instead of providing NPM registry credentials, override git auth
    
        - this seems a bit hacky, but I haven't been able to find another
          method that allows adding then removing the access token
        - many online guides focus on persisting the token locally for
          a human user to re-use the token until it expires
    
    * fix: attempt to install tradingview charts with direct token
    
    * fix: test env vars
    
    * fix: attempt garbage
    
    * fix: attempt git config modification
    
    * fix: attempt to remove original default token from git
    
    * fix: add newly found fix to both workflows with comments
    
    * fix: incorrect URL parsing

--- src/pages/Orderbook/OrderbookChart.tsx
+++ src/pages/Orderbook/OrderbookChart.tsx
@@ -1,57 +1,66 @@
-import { useEffect, useMemo, useState } from 'react';
-import Chart from 'react-apexcharts';
-import { ApexOptions } from 'apexcharts';
+import BigNumber from 'bignumber.js';
+import { useEffect, useMemo, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDeepCompareMemoize } from 'use-deep-compare-effect';
+import {
+  widget,
+  IBasicDataFeed,
+  ChartingLibraryWidgetOptions,
+  DatafeedConfiguration,
+  ResolutionString,
+  LibrarySymbolInfo,
+  SearchSymbolResultItem,
+  Bar,
+} from '@tradingview/charting-library';
 
 import { Token } from '../../lib/web3/utils/tokens';
 
-const { REACT_APP__INDEXER_API = '' } = process.env;
+import useTokens, {
+  matchTokenByAddress,
+  useTokensWithIbcInfo,
+} from '../../lib/web3/hooks/useTokens';
+import useTokenPairs from '../../lib/web3/hooks/useTokenPairs';
+import { tickIndexToPrice } from '../../lib/web3/utils/ticks';
+import { TimeSeriesRow } from '../../components/stats/utils';
+import { IndexerStreamAccumulateSingleDataSet } from '../../lib/web3/hooks/useIndexer';
 
-const chartOptions: ApexOptions = {
-  title: {
-    text: '',
-    align: 'left',
-  },
-  xaxis: {
-    decimalsInFloat: 1,
-    type: 'datetime',
-    title: {
-      text: 'Time',
-    },
-  },
-  yaxis: {
-    decimalsInFloat: 1,
-    tooltip: {
-      enabled: true,
-    },
-    title: {
-      text: '',
-    },
-  },
-  theme: {
-    mode: 'dark',
-  },
-  chart: {
-    background: '#1f2b37',
-  },
-  plotOptions: {
-    candlestick: {
-      colors: {
-        upward: '#31C48D',
-        downward: '#F05252',
-      },
-      wick: {
-        useFillColor: true,
-      },
-    },
-  },
-  tooltip: {
-    z: {
-      formatter(val) {
-        return Number(val).toFixed(2);
-      },
-    },
-  },
+const { REACT_APP__INDEXER_API = '', NODE_ENV = 'production' } = process.env;
+
+const defaultWidgetOptions: Partial<ChartingLibraryWidgetOptions> = {
+  debug: NODE_ENV !== 'production',
+  autosize: true,
+  container: '',
+  locale: 'en',
+  disabled_features: ['use_localstorage_for_settings'],
+  enabled_features: ['study_templates'],
+  charts_storage_url: 'https://saveload.tradingview.com',
+  charts_storage_api_version: '1.1',
+  // path to static assets of the charting library
+  library_path: '/charting_library/',
 };
+
+interface RequestQuery {
+  stream?: 'true';
+}
+
+interface PaginationRequestQuery extends RequestQuery {
+  // replicated CosmosSDK keys
+  'pagination.key'?: string; // base64 string key
+  'pagination.offset'?: string; // integer
+  'pagination.limit'?: string; // integer
+  'pagination.count_total'?: 'true'; // boolean
+  // custom
+  'pagination.before'?: string; // unix timestamp
+  'pagination.after'?: string; // unix timestamp
+}
+
+interface BlockRangeRequestQuery extends RequestQuery {
+  // custom query parameters
+  'block_range.from_height'?: string; // integer
+  'block_range.to_height'?: string; // integer
+}
+
+type TimeSeriesResolution = 'second' | 'minute' | 'hour' | 'day' | 'month';
 
 export default function OrderBookChart({
   tokenA,
@@ -60,72 +69,294 @@ export default function OrderBookChart({
   tokenA: Token;
   tokenB: Token;
 }) {
-  const [chartSeries, setChartSeries] = useState<ApexAxisChartSeries>([]);
   const tokenAPath = tokenA.address;
   const tokenBPath = tokenB.address;
-  useEffect(() => {
-    const poller = {
-      poll: async () => undefined,
-    };
-    poller.poll = async function poll() {
-      // skip undefined token fetches
-      if (!tokenAPath || !tokenAPath) {
-        return;
-      }
-      let next = '';
-      const liquidity = [];
-      do {
-        const response = await fetch(
-          `${REACT_APP__INDEXER_API}/timeseries/price/${tokenAPath}/${tokenBPath}${
-            next ? `?pagination.key=${next}` : ''
-          }`
-        );
-        const { data = [], pagination = {} } = await response.json();
-        // add data into current context
-        liquidity.push(...data);
-        // fetch again if necessary
-        next = pagination['next_key'];
-      } while (next);
-      timeout = setTimeout(poller.poll, 25000);
 
-      setChartSeries([
-        {
-          name: `${tokenA.symbol}/${tokenB.symbol}`,
-          data: liquidity.map((row) => {
-            return [
-              row[0] * 1000,
-              row[1].map((n: number) =>
-                Number(Math.pow(1.0001, n).toFixed(2))
-              ) as number[],
-            ] as number[];
-          }),
-        },
-      ]);
-    } as () => Promise<undefined>;
+  const navigate = useNavigate();
 
-    let timeout = setTimeout(poller.poll, 0);
-    return () => clearTimeout(timeout);
-  }, [tokenA, tokenAPath, tokenB, tokenBPath]);
+  // find chart container to fit
+  const chartRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <div className="flex-centered">
-      <Chart
-        className="candlestick-chart"
-        type="candlestick"
-        options={useMemo(() => {
-          return {
-            ...chartOptions,
-            yaxis: {
-              ...chartOptions?.yaxis,
-              title: {
-                text: `${tokenA.symbol}/${tokenB.symbol}`,
-              },
-            },
-          };
-        }, [tokenA, tokenB])}
-        series={chartSeries}
-        height={440}
-      />
-    </div>
+  const tokenList = useTokensWithIbcInfo(useTokens());
+  const { data: tokenPairAddresses } = useTokenPairs();
+
+  // memoize tokenPairs so we don't trigger the graph re-rendering too often
+  const tokenPairs = useDeepCompareMemoize(
+    useMemo<Array<[Token, Token]>>(() => {
+      return tokenPairAddresses
+        ? tokenPairAddresses
+            // find the tokens that match our known pair token addresses
+            .map(([token0, token1]) => {
+              return [
+                tokenList.find(matchTokenByAddress(token0)),
+                tokenList.find(matchTokenByAddress(token1)),
+              ];
+            })
+            // remove pairs with unfound tokens
+            .filter<[Token, Token]>((tokenPair): tokenPair is [Token, Token] =>
+              tokenPair.every(Boolean)
+            )
+        : [];
+    }, [tokenList, tokenPairAddresses])
   );
+
+  // tokenPairID is made of symbols, which is different to token paths
+  const tokenPairID = useMemo(() => {
+    if (tokenA && tokenB) {
+      return `${tokenA.symbol}/${tokenB.symbol}`;
+    }
+  }, [tokenA, tokenB]);
+
+  useEffect(() => {
+    const supportedResolutions: ResolutionString[] = [
+      '1S', // second
+      '1', // minute
+      '60', // hour
+      '1D', // day
+    ] as ResolutionString[];
+
+    const resolutionMap: {
+      [tradingViewResolution: string]: TimeSeriesResolution;
+    } = {
+      '1S': 'second', // second
+      '1': 'minute', // minute
+      '60': 'hour', // hour
+      '1D': 'day', // day
+    };
+
+    // keep track of data subscription state here
+    type FetchID = string;
+    type SubscriberUUID = string;
+    const knownHeights: Map<FetchID, number> = new Map();
+    const streams: Map<
+      SubscriberUUID,
+      IndexerStreamAccumulateSingleDataSet<TimeSeriesRow>
+    > = new Map();
+
+    const getFetchID = (
+      symbolInfo: LibrarySymbolInfo,
+      resolution: ResolutionString
+    ): FetchID => {
+      return JSON.stringify({ name: symbolInfo.name, resolution });
+    };
+    const getFetchURL = (
+      symbolA: string,
+      symbolB: string,
+      resolution: ResolutionString,
+      searchParams?: PaginationRequestQuery & BlockRangeRequestQuery
+    ) => {
+      const url = new URL(
+        `${REACT_APP__INDEXER_API}/timeseries/price/${symbolA}/${symbolB}${
+          resolutionMap[`${resolution}`]
+            ? `/${resolutionMap[`${resolution}`]}`
+            : ''
+        }`
+      );
+
+      // add search params into constructed URL
+      Object.entries(searchParams || {}).forEach(([key, value]) => {
+        url.searchParams.append(key, value);
+      });
+
+      return url;
+    };
+
+    // don't create options unless ID requirements are satisfied
+    if (chartRef.current && tokenPairID && tokenAPath && tokenBPath) {
+      const datafeed: IBasicDataFeed = {
+        onReady: async (onReadyCallback) => {
+          await new Promise((resolve) => setTimeout(resolve, 1));
+          const datafeedConfiguration: DatafeedConfiguration = {
+            supports_marks: true,
+            supports_timescale_marks: true,
+            supports_time: true,
+            exchanges: [{ value: 'duality', name: 'Duality', desc: 'Duality' }],
+            // these are categories to be able to search on in the searchSymbols callback
+            symbols_types: [],
+            supported_resolutions: supportedResolutions,
+          };
+          onReadyCallback(datafeedConfiguration);
+        },
+        resolveSymbol: (
+          symbolName,
+          onSymbolResolvedCallback,
+          onResolveErrorCallback,
+          extension
+        ) => {
+          // if not on correct page, go to correct page
+          navigate(`/orderbook/${symbolName}`);
+
+          // find symbol information for this page
+          const symbolInfo: LibrarySymbolInfo = {
+            description: `Pair of ${symbolName}`,
+            format: 'price',
+            full_name: symbolName,
+            exchange: 'Duality',
+            listed_exchange: '',
+            minmov: 1,
+            name: symbolName,
+            pricescale: 1000,
+            session: '24x7',
+            has_intraday: true,
+            supported_resolutions: supportedResolutions,
+            timezone: 'Etc/UTC',
+            type: 'crypto',
+          };
+          setTimeout(() => {
+            onSymbolResolvedCallback(symbolInfo);
+          }, 0);
+        },
+        searchSymbols: (
+          userInput = '',
+          exchange, // will always be "Duality"
+          symbolType, // will always be "crypto"
+          onResultReadyCallback
+        ) => {
+          const tokens = Array.from(new Set(tokenPairs.flatMap((v) => v)));
+          const inputs = userInput.toLowerCase().split('/');
+          const filteredTokens = tokens.filter((token) => {
+            // return a match if any of the inputs given match a token
+            return inputs.some((input) => {
+              return (
+                token.chain.chain_name.toLowerCase().includes(input) ||
+                token.name.toLowerCase().includes(input) ||
+                token.base.toLowerCase().includes(input) ||
+                token.display.toLowerCase().includes(input) ||
+                token.symbol.toLowerCase().includes(input) ||
+                token.keywords?.map((v) => v.toLowerCase()).includes(input)
+              );
+            });
+          });
+
+          const items: SearchSymbolResultItem[] = tokenPairs
+            .filter(([tokenA, tokenB]) => {
+              return (
+                filteredTokens.includes(tokenA) ||
+                filteredTokens.includes(tokenB)
+              );
+            })
+            .map(([tokenA, tokenB]) => {
+              return {
+                exchange: 'Duality Dex',
+                symbol: `${tokenA.symbol}/${tokenB.symbol}`,
+                full_name: `${tokenA.symbol}/${tokenB.symbol}`,
+                description: `Duality Dex pair of ${tokenA.name} and ${tokenB.name}`,
+                type: 'crypto',
+              };
+            });
+          onResultReadyCallback(items);
+        },
+        getBars: async (
+          symbolInfo,
+          resolution,
+          periodParams,
+          onHistoryCallback,
+          onErrorCallback
+        ) => {
+          // construct fetch ID that corresponds to a unique known fetch height
+          const fetchID = getFetchID(symbolInfo, resolution);
+          const url = getFetchURL(tokenAPath, tokenBPath, resolution, {
+            'pagination.before': periodParams.to?.toFixed(0),
+            'pagination.after': periodParams.from?.toFixed(0),
+          });
+
+          const stream =
+            new IndexerStreamAccumulateSingleDataSet<TimeSeriesRow>(
+              url,
+              {
+                onCompleted: (data, height) => {
+                  stream.unsubscribe();
+                  knownHeights.set(fetchID, height);
+                  const bars: Bar[] = Array.from(data)
+                    // note: the data needs to be in chronological order
+                    // and our API delivers results in reverse-chronological order
+                    .reverse()
+                    .map(getBarFromTimeSeriesRow);
+                  onHistoryCallback(bars, { noData: !bars.length });
+                },
+                onError: (e) => {
+                  onErrorCallback(
+                    (e as Error)?.message || 'Unknown error occurred'
+                  );
+                },
+              },
+              { disableSSE: true }
+            );
+          streams.set(fetchID, stream);
+        },
+        subscribeBars: (
+          symbolInfo,
+          resolution,
+          onRealtimeCallback,
+          subscriberUID,
+          onResetCacheNeededCallback
+        ) => {
+          const fetchID = getFetchID(symbolInfo, resolution);
+          const url = getFetchURL(tokenAPath, tokenBPath, resolution, {
+            'block_range.from_height': knownHeights.get(fetchID)?.toFixed(0),
+          });
+
+          const stream =
+            new IndexerStreamAccumulateSingleDataSet<TimeSeriesRow>(url, {
+              onUpdate: (dataUpdates) => {
+                // note: the data needs to be in chronological order
+                // and our API delivers results in reverse-chronological order
+                const chronologicalUpdates = dataUpdates.reverse();
+                for (const row of chronologicalUpdates) {
+                  onRealtimeCallback(getBarFromTimeSeriesRow(row));
+                }
+              },
+              onError: (e) => {
+                // eslint-disable-next-line no-console
+                console.error('SSE error', e);
+              },
+            });
+          streams.set(subscriberUID, stream);
+        },
+        unsubscribeBars: (subscriberUID) => {
+          streams.get(subscriberUID)?.unsubscribe();
+          streams.delete(subscriberUID);
+        },
+      };
+
+      // create options if possible to do so
+      const widgetOptions: ChartingLibraryWidgetOptions = {
+        ...defaultWidgetOptions,
+        container: chartRef.current,
+        locale: 'en',
+        symbol: tokenPairID,
+        // start with minute resolution
+        interval: '1' as ResolutionString,
+        datafeed,
+      };
+
+      // create chart widget
+      const tvWidget = new widget(widgetOptions);
+
+      // return method to cleanup widget and data subscribers
+      return () => {
+        // remove widget
+        tvWidget.remove();
+        // unsubscribe all requests (both getBars and subscribeBars)
+        streams.forEach((stream) => {
+          stream.unsubscribe();
+        });
+      };
+    }
+  }, [navigate, tokenAPath, tokenBPath, tokenPairID, tokenPairs]);
+
+  return <div className="trading-view-chart flex" ref={chartRef}></div>;
+}
+
+function getBarFromTimeSeriesRow([
+  unixTimestamp,
+  [open, high, low, close],
+]: TimeSeriesRow) {
+  return {
+    time: unixTimestamp * 1000,
+    open: tickIndexToPrice(new BigNumber(open)).toNumber(),
+    high: tickIndexToPrice(new BigNumber(high)).toNumber(),
+    low: tickIndexToPrice(new BigNumber(low)).toNumber(),
+    close: tickIndexToPrice(new BigNumber(close)).toNumber(),
+  };
 }
