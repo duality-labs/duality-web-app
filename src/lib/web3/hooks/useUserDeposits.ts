6a471256118f7c1e7b0cc10c3983fa9f46f71552
David Uhlmann <dib542@gmail.com>
2023-12-02 15:20:19 +1000

    feat: add new base hook for querying user deposits of a token pair

--- src/lib/web3/hooks/useUserDeposits.ts
+++ src/lib/web3/hooks/useUserDeposits.ts
@@ -1,8 +1,11 @@
+import { useMemo } from 'react';
 import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import { dualitylabs } from '@duality-labs/dualityjs';
 import { QueryAllUserDepositsResponse } from '@duality-labs/dualityjs/types/codegen/dualitylabs/duality/dex/query';
+import { useDeepCompareMemoize } from 'use-deep-compare-effect';
 
 import { useWeb3 } from '../useWeb3';
+import { TokenIdPair, TokenPair, resolveTokenIdPair } from '../utils/tokens';
 import { minutes } from '../../utils/time';
 
 const { REACT_APP__REST_API = '' } = process.env;
@@ -31,4 +34,31 @@ export function useUserDeposits(): UseQueryResult<
   });
 
   return result;
+}
+
+export function useUserDepositsOfTokenPair(
+  tokenPair: TokenPair | TokenIdPair | undefined
+): UseQueryResult<QueryAllUserDepositsResponse | undefined> {
+  const [tokenIdA, tokenIdB] = resolveTokenIdPair(tokenPair);
+
+  const result = useUserDeposits();
+  const userDeposits = useDeepCompareMemoize(result.data?.Deposits);
+  const userDepositsOfTokenPair = useMemo(() => {
+    if (tokenIdA && tokenIdB) {
+      const tokenIDs = [tokenIdA, tokenIdB];
+      return userDeposits?.filter((deposit) => {
+        return (
+          tokenIDs.includes(deposit.pairID.token0) &&
+          tokenIDs.includes(deposit.pairID.token1)
+        );
+      });
+    }
+  }, [tokenIdA, tokenIdB, userDeposits]);
+
+  return {
+    ...result,
+    data: userDepositsOfTokenPair
+      ? { Deposits: userDepositsOfTokenPair }
+      : undefined,
+  } as UseQueryResult<QueryAllUserDepositsResponse | undefined>;
 }
