f4865b9f838af274a0f986af4c815d404ed63262
David Uhlmann <dib542@gmail.com>
2023-12-20 19:19:55 +1000

    fix: pool reserve data was not showing when pool ID is "0"

--- src/lib/web3/hooks/useUserReserves.ts
+++ src/lib/web3/hooks/useUserReserves.ts
@@ -246,7 +246,7 @@ function useUserDepositsTotalShares(
     for (const userPoolTotalShare of userPoolTotalShares || []) {
       // make map here
       const poolID = getDexSharePoolID(userPoolTotalShare.balance);
-      if (poolID) {
+      if (poolID !== undefined) {
         totalSharesByPoolID.set(poolID, userPoolTotalShare);
       }
     }
@@ -281,7 +281,9 @@ function useUserDepositsTotalShares(
     return userDepositsWithPoolID?.map(({ deposit, metadata }) => {
       const poolID = metadata?.ID.toNumber();
       const totalShares =
-        totalSharesByPoolID.get(poolID || -1)?.totalShares ?? '0';
+        poolID !== undefined
+          ? totalSharesByPoolID.get(poolID)?.totalShares ?? '0'
+          : '0';
       return {
         deposit,
         totalShares,
