2aec51c167195b6143a2e0b93e9fa093907fe142
David Uhlmann <dib542@gmail.com>
2023-12-22 19:32:43 +1000

    fix: rename neutron client from neutronjs

--- src/pages/Pool/useDeposit.ts
+++ src/pages/Pool/useDeposit.ts
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 import { DeliverTxResponse } from '@cosmjs/stargate';
 import BigNumber from 'bignumber.js';
 import Long from 'long';
-import { neutron as duality } from '@duality-labs/dualityjs';
+import { neutron } from '@duality-labs/dualityjs';
 
 import { useWeb3 } from '../../lib/web3/useWeb3';
 import rpcClient from '../../lib/web3/rpcMsgClient';
@@ -262,7 +262,7 @@ export function useDeposit([denomA, denomB]: [
           const res = await client.signAndBroadcast(
             web3.address,
             [
-              duality.dex.MessageComposer.withTypeUrl.deposit({
+              neutron.dex.MessageComposer.withTypeUrl.deposit({
                 creator: web3Address,
                 token_a: denomA,
                 token_b: denomB,
