2cf2b4320a150bc40aee9daaaf0580ab55e02c83
David Uhlmann <dib542@gmail.com>
2024-01-06 03:28:34 +1000

    feat: swap create-react-app for vite

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 
 // add Buffer.from support for '@duality-labs/dualityjs'
 import { Buffer } from 'buffer';
@@ -23,8 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
