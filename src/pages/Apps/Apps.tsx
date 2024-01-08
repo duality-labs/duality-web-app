af10a956c00eb0cfce7377ac7609ad4bc2c1e7fd
David Uhlmann <dib542@gmail.com>
2024-01-08 11:58:50 +1000

    feat: add second iframe example

--- src/pages/Apps/Apps.tsx
+++ src/pages/Apps/Apps.tsx
@@ -26,7 +26,47 @@ const marsStyle = {
   height: 100,
 };
 export default function Apps() {
-  if (useMatch('/apps/mars')) {
+  const marsSimple = useMatch('/apps/mars');
+  const marsAdvanced = useMatch('/apps/mars/advanced');
+  if (marsSimple) {
+    return (
+      <div className="container col flex gap-5 py-6">
+        <img
+          className="m-5 mr-auto"
+          src="https://raw.githubusercontent.com/cosmos/chain-registry/master/mars/images/mars-protocol.svg"
+          alt="Mars"
+          style={{
+            objectFit: 'cover',
+            height: '10em',
+            color: 'white',
+            filter: 'invert(100%)',
+          }}
+        ></img>
+        <p>Demo of dex data subscription using frame messaging</p>
+        <ul style={{ marginLeft: 30 }}>
+          <li>
+            wallet address is read from outer frame (but could be read from
+            inner frame)
+          </li>
+          <li>wallet address --&gt; inner frame</li>
+          <li>wallet address is shown in inner frame</li>
+          <li>
+            in inner frame: (but notifications could be sent to outer frame)
+            <ul style={{ marginLeft: 30 }}>
+              <li>transaction loading notification state</li>
+              <li>transaction request payload created</li>
+              <li>transaction request authorization asked</li>
+              <li>transaction request payload signed</li>
+              <li>transaction request made</li>
+              <li>transaction response notification state shown</li>
+            </ul>
+          </li>
+        </ul>
+        <AppIFrame src="https://65xs47-5173.csb.app" style={marsStyle} />
+      </div>
+    );
+  }
+  if (marsAdvanced) {
     return (
       <div className="container col flex gap-5 py-6">
         <img
@@ -60,51 +100,93 @@ export default function Apps() {
           <li>transaction response notification state --&gt; outer frame</li>
           <li>transaction response notification state shown in outer frame</li>
         </ul>
-        <AppIFrame src="https://tqljss-5173.csb.app" style={marsStyle} />
+        <AppIFrame src="https://2rfjdl-5173.csb.app/" style={marsStyle} />
       </div>
     );
   }
 
   return (
     <div className="container col flex gap-5 py-6">
-      <div style={{ width: '40em', height: '25em' }}>
-        <Link
-          className="logo"
-          // may be redirected by other logic from here
-          to="/apps/mars"
-        >
-          <div
-            style={{
-              height: '25em',
-              backgroundImage:
-                'url("https://neutron.marsprotocol.io/_next/static/media/bg.0851eeb4.svg")',
-              backgroundSize: 'cover',
-              borderRadius: '1rem',
-              border: '.4666666667rem solid #421f32',
-            }}
+      <div className="row flex gap-5">
+        <div style={{ width: '45em', height: '25em' }}>
+          <Link
+            className="logo"
+            // may be redirected by other logic from here
+            to="/apps/mars"
           >
-            <img
-              className="m-5"
-              src="https://raw.githubusercontent.com/cosmos/chain-registry/master/mars/images/mars-protocol.svg"
-              alt="Mars"
+            <div
               style={{
-                objectFit: 'cover',
-                height: '10em',
-                color: 'white',
-                filter: 'invert(100%)',
+                height: '25em',
+                backgroundImage:
+                  'url("https://neutron.marsprotocol.io/_next/static/media/bg.0851eeb4.svg")',
+                backgroundSize: 'cover',
+                borderRadius: '1rem',
+                border: '.4666666667rem solid #421f32',
               }}
-            ></img>
-            <div className="ml-5">
-              <h2 className="h2">The Advanced Orderbook</h2>
-              <p>Get the best trading experience</p>
-              <br />
-              <i>
-                Demo of dex transactions and data subscription using frame
-                messaging
-              </i>
+            >
+              <img
+                className="m-5"
+                src="https://raw.githubusercontent.com/cosmos/chain-registry/master/mars/images/mars-protocol.svg"
+                alt="Mars"
+                style={{
+                  objectFit: 'cover',
+                  height: '10em',
+                  color: 'white',
+                  filter: 'invert(100%)',
+                }}
+              ></img>
+              <div className="ml-5">
+                <h2 className="h2">The Advanced Orderbook</h2>
+                <p>Get the best trading experience</p>
+                <br />
+                <i>
+                  Demo of dex data subscription and inner frame wallet tx
+                  signing
+                </i>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
+
+        <div style={{ width: '45em', height: '25em' }}>
+          <Link
+            className="logo"
+            // may be redirected by other logic from here
+            to="/apps/mars/advanced"
+          >
+            <div
+              style={{
+                height: '25em',
+                backgroundImage:
+                  'url("https://neutron.marsprotocol.io/_next/static/media/bg.0851eeb4.svg")',
+                backgroundSize: 'cover',
+                borderRadius: '1rem',
+                border: '.4666666667rem solid #421f32',
+              }}
+            >
+              <img
+                className="m-5"
+                src="https://raw.githubusercontent.com/cosmos/chain-registry/master/mars/images/mars-protocol.svg"
+                alt="Mars"
+                style={{
+                  objectFit: 'cover',
+                  height: '10em',
+                  color: 'white',
+                  filter: 'invert(100%)',
+                }}
+              ></img>
+              <div className="ml-5">
+                <h2 className="h2">The Advanced Orderbook</h2>
+                <p>Get the best trading experience</p>
+                <br />
+                <i>
+                  Demo of data subscription and outer frame wallet tx signing
+                  using frame messaging
+                </i>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
