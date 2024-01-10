e8f52123c88dd7ee7e8846a2311ce2b0ff3b11f9
David Uhlmann <dib542@gmail.com>
2024-01-10 22:09:02 +1000

    fix: add other dependencies to get jest working with TypeScript again
        - these were probably handled by create-react-app before
        - we have to specify these in a jest config ourselves now

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.mjs'],
+  transform: {
+    // process `*.ts` and `*.tsx` files with `ts-jest`
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  //   extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.mjs',
+  },
+};
+
+export default config;
---
+++ src/tests/__mocks__/fileMock.mjs
@@ -0,0 +1,2 @@
+export const __esModule = true;
+export default 'test-file-stub';
