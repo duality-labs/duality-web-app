893db7f6bae207e8117768c461814b9ba9a7a989
David Uhlmann <dib542@gmail.com>
2024-01-18 19:13:27 +1000

    feat: add useTracedAsset to find deep asset detail for any denom

--- src/lib/web3/hooks/useDenomsFromRegistry.ts
+++ src/lib/web3/hooks/useDenomsFromRegistry.ts
@@ -1,64 +1,57 @@
 import useSWRImmutable from 'swr/immutable';
-import { useMemo } from 'react';
+import { SWRResponse } from 'swr';
+import { useEffect, useMemo, useState } from 'react';
 import {
   ChainRegistryClient,
+  ChainRegistryClientOptions,
   ChainRegistryChainUtil,
 } from '@chain-registry/client';
+import { Asset, AssetList, Chain } from '@chain-registry/types';
+import { useDeepCompareMemoize } from 'use-deep-compare-effect';
+import { useDenomTrace } from './useDenomsFromChain';
 
 const {
   REACT_APP__CHAIN_NAME = '',
   REACT_APP__CHAIN_IS_TESTNET = '',
-  REACT_APP__CHAIN_REGISTRY_CHAIN_NAMES = '',
-  REACT_APP__CHAIN_REGISTRY_ENDPOINTS = '["https://registry.ping.pub"]',
+  REACT_APP__CHAIN_REGISTRY_FILE_ENDPOINTS = '["https://registry.ping.pub"]',
+  REACT_APP__CHAIN_REGISTRY_PATH_ENDPOINTS = '["https://registry.ping.pub"]',
 } = import.meta.env;
 
 const isTestnet = REACT_APP__CHAIN_IS_TESTNET
   ? REACT_APP__CHAIN_IS_TESTNET === 'true'
   : REACT_APP__CHAIN_NAME.endsWith('testnet');
 
-// chain names are the other chains that we will get IBC connection data for
-// in the chain-registry, eg. "celestia" for "_IBC/celestia-neutron.json" in:
-// https://github.com/cosmos/chain-registry/pull/3100/files
-const chainRegistryChainNames: string[] = JSON.parse(
-  REACT_APP__CHAIN_REGISTRY_CHAIN_NAMES || '[]'
-);
-
 // chain registry endpoints are endpoints where we can get ~up-to-date (cached)
 // asset and IBC data. it is expected that this data will change infrequently
-const chainRegistryEndpoints: string[] = JSON.parse(
-  REACT_APP__CHAIN_REGISTRY_ENDPOINTS
+const chainRegistryFileEndpoints: string[] = JSON.parse(
+  REACT_APP__CHAIN_REGISTRY_FILE_ENDPOINTS
+).map((endpoint: string) => (isTestnet ? `${endpoint}/testnets` : endpoint));
+const chainRegistryDirectoryEndpoints: string[] = JSON.parse(
+  REACT_APP__CHAIN_REGISTRY_PATH_ENDPOINTS
 ).map((endpoint: string) => (isTestnet ? `${endpoint}/testnets` : endpoint));
 
-export function useChainUtil(chainName = REACT_APP__CHAIN_NAME) {
-  // abstract out chain client creation for any chain-registry data endpoint
-  async function createChainRegistryClient(chainRegistryEndpoint: string) {
-    const chainRegistryClient = new ChainRegistryClient({
-      baseUrl: chainRegistryEndpoint,
-      chainNames: Array.from(new Set([chainName, ...chainRegistryChainNames])),
-    });
+type ChainNamePair = [chainName1: string, chainName2: string];
 
-    // remove irrelevant IBC data from initial fetch
-    chainRegistryClient.urls = chainRegistryClient.urls.filter((url) => {
-      const ibcPair = url.split('/_IBC/').at(1)?.split('.').at(0)?.split('-');
-      // filter to non-IBC URLs and IBC URLs related to the current chain
-      return !ibcPair || ibcPair.includes(chainName);
-    });
-
-    // fetch all the URLs
-    await chainRegistryClient.fetchUrls();
-
-    // return just the chain utility instance
-    // it is possible to get the original fetcher at chainUtil.chainInfo.fetcher
-    return chainRegistryClient.getChainUtil(chainName);
-  }
-
+function useAllIbcNamePairs(): Array<[string, string]> | undefined {
   return useSWRImmutable(
-    ['chain-registry-client', chainName],
-    async (): Promise<ChainRegistryChainUtil> => {
-      for (const chainRegistryEndpoint of chainRegistryEndpoints) {
+    ['chain-registry-client-ibc-pair-list'],
+    async (): Promise<Array<ChainNamePair>> => {
+      for (const chainRegistryEndpoint of chainRegistryDirectoryEndpoints) {
         try {
-          return await createChainRegistryClient(chainRegistryEndpoint);
-        } catch {
+          const res = await fetch(`${chainRegistryEndpoint}/_IBC`);
+          const json: Array<{ name: string }> = await res.json();
+          return (
+            json.flatMap(({ name }: { name: string }) => {
+              const chainNames = name.split('.')?.at(0)?.split('-');
+              if (chainNames && chainNames[0] && chainNames[1]) {
+                return [[chainNames[0], chainNames[1]]];
+              }
+              return [];
+            }) || []
+          );
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.error(e);
           // try next endpoint if there is one
         }
       }
@@ -71,23 +64,228 @@ export function useChainUtil(chainName = REACT_APP__CHAIN_NAME) {
   ).data;
 }
 
-export function useNativeChainAssetList(chainName?: string) {
-  const chainUtil = useChainUtil(chainName);
+function useRelatedIbcNamePairs(
+  exploreChainName?: string
+): Array<[string, string]> | undefined {
+  const allIbcNamePairs = useAllIbcNamePairs();
+
+  return exploreChainName
+    ? allIbcNamePairs?.filter((ibcPairChainNames) =>
+        ibcPairChainNames.includes(exploreChainName)
+      )
+    : undefined;
+}
+
+export function useChain(chainName: string | undefined): SWRResponse<Chain> {
+  const swr = useChainUtil(chainName ? [chainName] : [], [], {
+    fetchRelatedAssets: false,
+    fetchRelatedPairs: false,
+  });
+  return {
+    ...swr,
+    data: swr.data?.chainInfo.fetcher.getChain(chainName || ''),
+  } as SWRResponse;
+}
+
+// get chain, defaulting to all related one-hop assets
+export function useChainUtil(
+  selectedChainNames: string[] = [REACT_APP__CHAIN_NAME],
+  selectedAssestListNames?: string[],
+  {
+    fetchRelatedPairs = true,
+    fetchRelatedAssets = true,
+  }: { fetchRelatedPairs?: boolean; fetchRelatedAssets?: boolean } = {}
+): SWRResponse<ChainRegistryChainUtil> {
+  const ibcNamePairs = useRelatedIbcNamePairs(
+    fetchRelatedPairs ? selectedChainNames.at(-1) : undefined
+  );
+  // ensure chain name is included in each (required for chain util of native chain)
+  const chainNames = Array.from(
+    new Set([REACT_APP__CHAIN_NAME, ...selectedChainNames])
+  );
+  // if selected asset lists aren't defined, find all related assets
+  const assetListNames = Array.from(
+    new Set([
+      REACT_APP__CHAIN_NAME,
+      ...(selectedAssestListNames ||
+        (fetchRelatedAssets && ibcNamePairs?.flat()) ||
+        []),
+    ])
+  );
+
+  // abstract out chain client creation for any chain-registry data endpoint
+  async function createChainRegistryClient(opts: ChainRegistryClientOptions) {
+    const chainRegistryClient = new ChainRegistryClient(opts);
+
+    // fetch all the URLs
+    try {
+      await chainRegistryClient.fetchUrls();
+    } catch {
+      // ignore if a resource throw errors
+    }
+
+    // return just the chain utility instance
+    // it is possible to get the original fetcher at chainUtil.chainInfo.fetcher
+    return chainRegistryClient.getChainUtil(REACT_APP__CHAIN_NAME);
+  }
+
+  return useSWRImmutable(
+    ['chain-util', chainNames, ibcNamePairs, assetListNames],
+    async (): Promise<ChainRegistryChainUtil> => {
+      // restrict asset collection to specific chains or
+      // check available endpoints for data
+      for (const endpoint of chainRegistryFileEndpoints) {
+        try {
+          return await createChainRegistryClient({
+            baseUrl: endpoint,
+            // ensure strings arrays have uniques strings
+            chainNames,
+            assetListNames,
+            ibcNamePairs,
+          });
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.error(e);
+          // try next endpoint if there is one
+        }
+      }
+      throw new Error('Could not get current chain-registry data');
+    },
+    {
+      // don't retry if we found an error we already tried all endpoints
+      shouldRetryOnError: false,
+    }
+  );
+}
+
+export function useChainNativeAssetList(): AssetList | undefined {
+  const { data: chainUtil } = useChainUtil();
   return useMemo(() => {
     // todo: use chainUtil?.chainInfo.nativeAssetList when types are fixed
-    return chainUtil?.chainInfo.fetcher.getChainAssetList(chainUtil.chainName);
+    return chainUtil?.chainInfo.nativeAssetLists;
   }, [chainUtil]);
 }
 
-export function useIBCChainAssetLists(chainName?: string) {
-  const chainUtil = useChainUtil(chainName);
+export function useChainIbcAssetList(): AssetList | undefined {
+  const { data: chainUtil } = useChainUtil();
   return useMemo(() => {
     // todo: use chainUtil?.chainInfo.assetLists when types are fixed
     return chainUtil?.chainInfo.fetcher.getChainAssetList(chainUtil.chainName);
   }, [chainUtil]);
 }
 
-export function useChainAssetLists(chainName?: string) {
-  const chainUtil = useChainUtil(chainName);
+export function useChainAssetLists() {
+  const { data: chainUtil } = useChainUtil();
   return useMemo(() => chainUtil?.chainInfo.fetcher.assetLists, [chainUtil]);
+}
+
+// define default options to start exploring without fetching too much data
+const defaultChainUtilOptions = {
+  chainNames: ['neutrontestnet'],
+  selectedAssestListNames: ['neutrontestnet'],
+};
+
+// this hook follows denom trace information along chain-registry IBC pair data
+// until a matching Asset is found or not
+export function useTracedAsset(
+  denom?: string
+): SWRResponse<{ asset?: Asset; chain?: Chain }> {
+  const { path = '' } = useDenomTrace(denom) || {};
+
+  const transferChannels: Array<[portId: string, channelId: string]> =
+    useDeepCompareMemoize(
+      path
+        .split('/')
+        .flatMap((path, index, paths) =>
+          index % 2 !== 0 ? [[paths[index - 1], path]] : []
+        )
+    );
+
+  const [{ chainNames, selectedAssestListNames }, setChainUtilOpts] = useState<{
+    chainNames: string[];
+    selectedAssestListNames: string[];
+  }>(defaultChainUtilOptions);
+
+  // get chain util that hopefully has the asset of the denom in question
+  const swr = useChainUtil(chainNames, selectedAssestListNames);
+  const chainUtil = swr.data;
+
+  // derive the found asset with current IBC data if it can be found
+  const asset = useMemo(() => {
+    try {
+      return denom ? chainUtil?.getAssetByDenom(denom) : undefined;
+    } catch {
+      // ignore error that asset cannot be found in current IBC data
+    }
+  }, [chainUtil, denom]);
+
+  // when switching denoms, start path exploration again
+  useEffect(() => {
+    // reset the chain names
+    setChainUtilOpts(defaultChainUtilOptions);
+  }, [path]);
+
+  // make chain util fetch deeper into the trace if needed
+  useEffect(() => {
+    const lastChainName = chainNames.at(-1) || '';
+    const ibcData = chainUtil?.chainInfo.fetcher.getChainIbcData(lastChainName);
+    const nextTransferChannelIndex = chainNames.length - 1;
+    const nextTransferChannel = transferChannels.at(nextTransferChannelIndex);
+
+    // if we don't have all the transfer channel hops covered then fetch more IBC data
+    if (!asset && ibcData && lastChainName && nextTransferChannel) {
+      const [portId, channelId] = nextTransferChannel;
+      for (const ibcDataRow of ibcData) {
+        // look up chain 1
+        if (ibcDataRow.chain_1.chain_name === lastChainName) {
+          const foundChannel = ibcDataRow.channels.find((channel) => {
+            return (
+              channel.chain_1.channel_id === channelId &&
+              channel.chain_1.port_id === portId
+            );
+          });
+          if (foundChannel) {
+            setNextChainName(ibcDataRow.chain_2.chain_name);
+            break;
+          }
+        }
+        // look up chain 2
+        else if (ibcDataRow.chain_2.chain_name === lastChainName) {
+          const foundChannel = ibcDataRow.channels.find((channel) => {
+            return (
+              channel.chain_2.channel_id === channelId &&
+              channel.chain_2.port_id === portId
+            );
+          });
+          if (foundChannel) {
+            setNextChainName(ibcDataRow.chain_1.chain_name);
+            break;
+          }
+        }
+      }
+    }
+
+    function setNextChainName(chainName: string) {
+      setChainUtilOpts(({ chainNames }) => {
+        // if this would be the last step then find the assetlist of this chain
+        // otherwise we should keep searching IBC data
+        if (chainNames.length >= transferChannels.length) {
+          return {
+            chainNames,
+            selectedAssestListNames: [chainName],
+          };
+        } else {
+          return {
+            chainNames: [...chainNames, chainName],
+            selectedAssestListNames: [],
+          };
+        }
+      });
+    }
+  }, [asset, chainNames, chainUtil, transferChannels]);
+
+  // get chain of the asset
+  const { data: chain } = useChain(asset && selectedAssestListNames.at(-1));
+
+  return { ...swr, data: { asset, chain } } as SWRResponse;
 }
