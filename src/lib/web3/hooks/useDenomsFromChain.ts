33c8122617ac7bfffa5623d1f5b1ed5f34f13bd5
David Uhlmann <dib542@gmail.com>
2024-01-19 16:59:26 +1000

    fix: useDenomsFromChain needs size to be set and updated

--- src/lib/web3/hooks/useDenomsFromChain.ts
+++ src/lib/web3/hooks/useDenomsFromChain.ts
@@ -1,69 +1,90 @@
 import useSWRInfinite, { SWRInfiniteKeyLoader } from 'swr/infinite';
 import { immutable } from 'swr/immutable';
-import { useCallback, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useDeepCompareMemoize } from 'use-deep-compare-effect';
 import { DenomTrace } from '@duality-labs/dualityjs/types/codegen/ibc/applications/transfer/v1/transfer';
 
-import { IbcRestClient, useIbcRestClient } from '../clients/lcdClients';
+import { useIbcRestClient } from '../clients/lcdClients';
 
 type DenomTraceByDenom = Map<string, DenomTrace>;
 
-function useUniqueDenoms(denoms: string[]) {
-  return useDeepCompareMemoize(Array.from(new Set(denoms)));
-}
+type SWRCommon<Data = unknown, Error = unknown> = {
+  isValidating: boolean;
+  isLoading: boolean;
+  error: Error;
+  data: Data | undefined;
+};
 
-function useDenomTraceByDenom(denoms: string[]): DenomTraceByDenom | undefined {
-  const uniqueDenoms = useUniqueDenoms(denoms);
+const concurrentItemCount = 3;
+
+export function useDenomTraceByDenom(
+  denoms: string[]
+): SWRCommon<DenomTraceByDenom> {
+  // use only unique IBC denoms
+  const ibcDenoms = useDeepCompareMemoize(
+    Array.from(new Set(denoms)).filter((denom) => denom.startsWith('ibc/'))
+  );
 
   const restClient = useIbcRestClient();
-  const getKey = useCallback<
-    SWRInfiniteKeyLoader<
-      DenomTrace,
-      [client: IbcRestClient, denom: string, key: string] | null
-    >
+  const swr = useSWRInfinite<
+    [string, DenomTrace?],
+    Error,
+    SWRInfiniteKeyLoader<DenomTrace, [denom: string, namespace: string]>
   >(
-    (index: number) => {
-      // don't start without the client
-      if (!restClient) {
-        return null;
-      }
-
-      // fetch page by pageKey
-      return [restClient, uniqueDenoms.at(index) || '', 'denom-trace'];
-    },
-    [restClient, uniqueDenoms]
-  );
-  const pages = useSWRInfinite<[string, DenomTrace?], Error, typeof getKey>(
-    getKey,
-    async ([restClient, denom]) => {
-      const hash = denom.split('ibc/').at(1);
-      return [
-        denom,
-        // fetch denom trace only if the denom has an IBC hash
-        hash
-          ? await restClient?.applications.transfer.v1
-              .denomTrace({ hash })
-              .then((response) => response.denom_trace)
-          : undefined,
-      ];
-    },
+    // allow hash to be an empty string
+    (index: number) => [ibcDenoms.at(index) || '', 'denom-trace'],
+    // handle cases of undefined client and empty hash string
+    restClient
+      ? async ([denom]) => {
+          const hash = denom.split('ibc/').at(1);
+          return [
+            denom,
+            // fetch denom trace only if the denom has an IBC hash
+            hash
+              ? await restClient.applications.transfer.v1
+                  .denomTrace({ hash })
+                  .then((response) => response.denom_trace)
+              : undefined,
+          ];
+        }
+      : null,
     // these endpoint responses never change
-    { use: [immutable] }
-  ).data;
+    {
+      parallel: true,
+      use: [immutable],
+      initialSize: concurrentItemCount,
+      revalidateFirstPage: false,
+      revalidateAll: false,
+    }
+  );
 
-  // combine maps into one
-  return useMemo<DenomTraceByDenom | undefined>(() => {
-    return pages?.reduce<DenomTraceByDenom>((map, [denom, denomTrace]) => {
-      if (denom && denomTrace) {
-        return map.set(denom, denomTrace);
-      }
-      return map;
-    }, new Map());
+  // get all pages, concurrentItemCount at a time
+  const { size, setSize } = swr;
+  useEffect(() => {
+    if (size < ibcDenoms.length) {
+      setSize((size) => Math.min(ibcDenoms.length, size + concurrentItemCount));
+    }
+  }, [size, setSize, ibcDenoms]);
+
+  // combine pages into one
+  const { data: pages } = swr;
+  const data = useMemo<DenomTraceByDenom | undefined>(() => {
+    return pages?.reduce<DenomTraceByDenom>(
+      (map, [denom, denomTrace] = ['']) => {
+        if (denom && denomTrace) {
+          return map.set(denom, denomTrace);
+        }
+        return map;
+      },
+      new Map()
+    );
   }, [pages]);
+
+  const { isValidating, isLoading, error } = swr;
+  return { isValidating, isLoading, error, data };
 }
 
-export function useDenomTrace(denom?: string): DenomTrace | undefined {
-  const denoms = useDeepCompareMemoize(denom ? [denom] : []);
-  const denomTraceByDenom = useDenomTraceByDenom(denoms);
-  return denomTraceByDenom?.get(denom || '');
+export function useDenomTrace(denom = ''): SWRCommon<DenomTrace> {
+  const { data: denomTraceByDenom, ...swr } = useDenomTraceByDenom([denom]);
+  return { ...swr, data: denomTraceByDenom?.get(denom) };
 }
