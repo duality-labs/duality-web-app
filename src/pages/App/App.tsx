7b1ec842b84b799e32eaff861ab97aec9e2441d7
David Uhlmann <dib542@gmail.com>
2024-02-06 04:18:08 +1000

    feat: add ErrorBoundary to catch unexpected errors

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -21,35 +21,84 @@ import MyLiquidity from '../MyLiquidity';
 // aren't overridden by page-card styles :(
 
 import './App.scss';
+import { Component, ErrorInfo, ReactNode } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Web3Provider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <BrowserRouter>
-            <Header />
-            <Stars />
-            <Planets />
-            <main>
-              <Routes>
-                <Route index element={<Navigate to={defaultPage} />} />
-                <Route path="swap/*" element={<Swap />} />
-                <Route path="pools/*" element={<Pool />} />
-                <Route path="orderbook/*" element={<Orderbook />} />
-                <Route path="portfolio/*" element={<MyLiquidity />} />
-                <Route path="bridge" element={<Bridge />} />
-                <Route path="*" element={<div>Not found</div>} />
-              </Routes>
-              <Notifications />
-            </main>
-          </BrowserRouter>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </Web3Provider>
+    <ErrorBoundary fallback={Fallback}>
+      <Web3Provider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <BrowserRouter>
+              <Header />
+              <Stars />
+              <Planets />
+              <main>
+                <Routes>
+                  <Route index element={<Navigate to={defaultPage} />} />
+                  <Route path="swap/*" element={<Swap />} />
+                  <Route path="pools/*" element={<Pool />} />
+                  <Route path="orderbook/*" element={<Orderbook />} />
+                  <Route path="portfolio/*" element={<MyLiquidity />} />
+                  <Route path="bridge" element={<Bridge />} />
+                  <Route path="*" element={<div>Not found</div>} />
+                </Routes>
+                <Notifications />
+              </main>
+            </BrowserRouter>
+          </ThemeProvider>
+        </QueryClientProvider>
+      </Web3Provider>
+    </ErrorBoundary>
   );
 }
 
 export default App;
+
+function Fallback({ error }: { error?: Error }) {
+  return (
+    <div>
+      error ({typeof error}): {JSON.stringify(error)}
+    </div>
+  );
+}
+
+class ErrorBoundary extends Component<{
+  fallback: (props: { error?: Error }) => ReactNode;
+  children: ReactNode;
+}> {
+  state: { didCatch: boolean; error?: Error } = { didCatch: false };
+
+  constructor(props: {
+    fallback: (props: { error?: Error }) => ReactNode;
+    children: ReactNode;
+  }) {
+    super(props);
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { didCatch: true, error };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    // Example "componentStack":
+    //   in ComponentThatThrows (created by App)
+    //   in ErrorBoundary (created by App)
+    //   in div (created by App)
+    //   in App
+    // eslint-disable-next-line no-console
+    console.error(error, info.componentStack);
+  }
+
+  render() {
+    if (this.state.didCatch) {
+      // You can render any custom fallback UI
+      return this.props.fallback({ error: this.state.error });
+    }
+
+    return this.props.children;
+  }
+}
