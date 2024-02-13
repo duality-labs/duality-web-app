f1ed2e7fc5a551295d173edd53a9cd07cb47682e
David Uhlmann <dib542@gmail.com>
2024-02-14 03:34:58 +1000

    Revert "feat: temporarily remove the Orderbook page"
    This reverts commit d71c4ad284b1261a5fd6055094fdb657e299cef6.

--- src/components/Header/routes.ts
+++ src/components/Header/routes.ts
@@ -3,6 +3,7 @@ const { REACT_APP__DEFAULT_PAIR = '' } = import.meta.env;
 export const pageLinkMap = {
   [['/swap', REACT_APP__DEFAULT_PAIR].join('/')]: 'Swap',
   '/pools': 'Pools',
+  [['/orderbook', REACT_APP__DEFAULT_PAIR].join('/')]: 'Orderbook',
   '/portfolio': 'Portfolio',
   '/bridge': 'Bridge',
 };
