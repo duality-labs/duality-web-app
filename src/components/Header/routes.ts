f2bc4a3e91987265961ec60a62b4506612fa1227
David Uhlmann <dib542@gmail.com>
2024-03-13 21:29:18 +1000

    feat: unhide Orderbook from beta

--- src/components/Header/routes.ts
+++ src/components/Header/routes.ts
@@ -1,13 +1,9 @@
-const { REACT_APP__DEFAULT_PAIR = '', REACT_APP__HIDE_ORDERBOOK = '' } =
-  import.meta.env;
+const { REACT_APP__DEFAULT_PAIR = '' } = import.meta.env;
 
 export const pageLinkMap = {
   [['/swap', REACT_APP__DEFAULT_PAIR].join('/')]: 'Swap',
   '/pools': 'Pools',
-  // conditionally add the orderbook in
-  ...(!REACT_APP__HIDE_ORDERBOOK && {
-    [['/orderbook', REACT_APP__DEFAULT_PAIR].join('/')]: 'Orderbook',
-  }),
+  [['/orderbook', REACT_APP__DEFAULT_PAIR].join('/')]: 'Orderbook',
   '/portfolio': 'Portfolio',
   '/bridge': 'Bridge',
 };
